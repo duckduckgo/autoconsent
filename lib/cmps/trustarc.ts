6064cafb64900af0c8bd15c42697574d49d009d7
Sam Macbeth <smacbeth@duckduckgo.com>
2021-11-18 13:04:08 +0100

    TrustArc fixes and hiding.

--- lib/cmps/trustarc.ts
+++ lib/cmps/trustarc.ts
@@ -1,61 +1,101 @@
-import AutoConsentBase, { waitFor } from './base';
-import { TabActor } from '../types';
+import AutoConsentBase, { waitFor } from "./base";
+import { TabActor } from "../types";
 
 export default class TrustArc extends AutoConsentBase {
   constructor() {
-    super('TrustArc');
+    super("TrustArc");
   }
 
   detectFrame(_: TabActor, frame: { url: string }) {
-    return frame.url.startsWith('https://consent-pref.trustarc.com/?');
+    return frame.url.startsWith("https://consent-pref.trustarc.com/?");
   }
 
   async detectCmp(tab: TabActor) {
-    if (tab.frame && tab.frame.url.startsWith('https://consent-pref.trustarc.com/?')) {
+    if (
+      tab.frame &&
+      tab.frame.url.startsWith("https://consent-pref.trustarc.com/?")
+    ) {
       return true;
     }
-    return tab.elementExists('#truste-show-consent');
+    return tab.elementExists("#truste-show-consent");
   }
 
   async detectPopup(tab: TabActor) {
-    if (tab.frame) {
-      return tab.waitForElement('#defaultpreferencemanager', 5000, tab.frame.id);
-    }
-    return tab.elementExists('#truste-show-consent');
+    return (
+      (await tab.elementsAreVisible("#truste-consent-content")) ||
+      (tab.frame &&
+        (await tab.waitForElement(
+          "#defaultpreferencemanager",
+          5000,
+          tab.frame.id
+        )))
+    );
   }
 
   async openFrame(tab: TabActor) {
-    if (await tab.elementExists('#truste-show-consent')) {
-      await tab.clickElement('#truste-show-consent');
+    if (await tab.elementExists("#truste-show-consent")) {
+      await tab.clickElement("#truste-show-consent");
     }
   }
 
   async navigateToSettings(tab: TabActor, frameId: number) {
     // wait for it to load
-    await waitFor(async () => {
-      return await tab.elementExists('.shp', frameId) ||
-        await tab.elementsAreVisible('.advance', 'any', frameId) ||
-        tab.elementExists('.switch span:first-child', frameId);
-    }, 10, 500);
+    await waitFor(
+      async () => {
+        return (
+          (await tab.elementExists(".shp", frameId)) ||
+          (await tab.elementsAreVisible(".advance", "any", frameId)) ||
+          tab.elementExists(".switch span:first-child", frameId)
+        );
+      },
+      10,
+      500
+    );
     // splash screen -> hit more information
-    if (await tab.elementExists('.shp', frameId)) {
-      await tab.clickElement('.shp', frameId);
+    if (await tab.elementExists(".shp", frameId)) {
+      await tab.clickElement(".shp", frameId);
     }
-    // wait for consent options
-    if (!await tab.waitForElement('.switch span:first-child', 2000, frameId)) {
-      return false;
-    }
-
+    await tab.waitForElement(".prefPanel", 5000, frameId);
     // go to advanced settings if not yet shown
-    if (await tab.elementsAreVisible('.advance', 'any', frameId)) {
-      await tab.clickElement('.advance', frameId);
+    if (await tab.elementsAreVisible(".advance", "any", frameId)) {
+      await tab.clickElement(".advance", frameId);
     }
     // takes a while to load the opt-in/opt-out buttons
-    return await waitFor(() => tab.elementsAreVisible('.switch span:first-child', 'any', frameId), 5, 1000);
+    return await waitFor(
+      () => tab.elementsAreVisible(".switch span:first-child", "any", frameId),
+      5,
+      1000
+    );
   }
 
   async optOut(tab: TabActor) {
-    await tab.hideElements(['.truste_overlay', '.truste_box_overlay', '.trustarc-banner', '.truste-banner']);
+    // await tab.hideElements(['.truste_overlay', '.truste_box_overlay', '.trustarc-banner', '.truste-banner']);
+    if (await tab.elementExists("#truste-consent-required")) {
+      return tab.clickElement("#truste-consent-required");
+    }
+    if (!tab.frame) {
+      await tab.clickElement("#truste-show-consent");
+      await waitFor(
+        async () =>
+          !!tab.frame &&
+          (await tab.elementsAreVisible(".mainContent", "any", tab.frame.id)),
+        50,
+        100
+      );
+    }
+    const frameId = tab.frame!.id;
+    tab.hideElements([".truste_popframe", ".truste_overlay", ".truste_box_overlay", "#truste-consent-track"]);
+    if (await tab.elementExists(".required", frameId)) {
+      await tab.clickElement(".required", frameId);
+    } else {
+      await this.navigateToSettings(tab, frameId);
+      await tab.clickElements(
+        ".switch span:nth-child(1):not(.active)",
+        frameId
+      );
+      await tab.clickElement(".submit", frameId);
+    }
+    await tab.waitForThenClick("#gwt-debug-close_id", 20000, tab.frame.id);
     return true;
   }
 
@@ -66,15 +106,19 @@ export default class TrustArc extends AutoConsentBase {
     }
     const frameId = tab.frame!.id;
     await this.navigateToSettings(tab, frameId);
-    await tab.clickElements('.switch span:nth-child(2)', frameId);
-    await tab.clickElement('.submit', frameId);
-    await waitFor(() => tab.elementExists('#gwt-debug-close_id', frameId), 300, 1000);
-    await tab.clickElement('#gwt-debug-close_id', frameId);
+    await tab.clickElements(".switch span:nth-child(2)", frameId);
+    await tab.clickElement(".submit", frameId);
+    await waitFor(
+      () => tab.elementExists("#gwt-debug-close_id", frameId),
+      300,
+      1000
+    );
+    await tab.clickElement("#gwt-debug-close_id", frameId);
     return true;
   }
 
   async openCmp(tab: TabActor) {
-    await tab.eval('truste.eu.clickListener()');
+    await tab.eval("truste.eu.clickListener()");
     return true;
   }
 
