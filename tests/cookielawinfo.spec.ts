fe6d6af0d358cc09c25f482061ad1c50de9e052b
Sam Macbeth <smacbeth@duckduckgo.com>
2022-01-31 16:23:25 +0100

    Cookie law info fix

--- tests/cookielawinfo.spec.ts
+++ tests/cookielawinfo.spec.ts
@@ -3,6 +3,7 @@ import generateCMPTests from "./runner";
 generateCMPTests('cookie-law-info', [
     'https://www.yubico.com/',
     'https://nytcrosswordanswers.org/',
+    'https://www.drohnen.de/'
 ], {
     skipRegions: ['US', 'GB']
 });
