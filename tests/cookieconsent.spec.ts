93576eff55368a2a3e0dc31c95985af44286922b
Sam Macbeth <smacbeth@duckduckgo.com>
2022-02-08 12:22:59 +0100

    expatica.com changed CMP.

--- tests/cookieconsent.spec.ts
+++ tests/cookieconsent.spec.ts
@@ -2,7 +2,8 @@ import generateCMPTests from "./runner";
 
 generateCMPTests('cookieconsent', [
     'https://yrc.com/',
-    'https://www.worldometers.info/'
+    'https://www.worldometers.info/',
+    'https://www.expatica.com/',
 ], {
     skipRegions: ['GB']
 });
