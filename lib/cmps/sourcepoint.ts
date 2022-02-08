41957683e143f1abb5acf9eaf82e53e1bbe51919
Sam Macbeth <smacbeth@duckduckgo.com>
2022-02-08 16:19:54 +0100

    Sourcepoint: unhide elements after rule completes

--- lib/cmps/sourcepoint.ts
+++ lib/cmps/sourcepoint.ts
@@ -40,45 +40,49 @@ export default class SourcePoint extends AutoConsentBase {
   }
 
   async optOut(tab: TabActor) {
-    tab.hideElements(["div[id^='sp_message_container_']"])
-    if (!this.isManagerOpen(tab)) {
-      if (!await waitFor(() => !!tab.frame, 30, 100)) {
-        throw "Frame never opened";
-      }
-      if (!await tab.elementExists("button.sp_choice_type_12", tab.frame.id)) {
-        // do not sell button
-        return tab.clickElement('button.sp_choice_type_13', tab.frame.id);
-      }
-      await success(tab.clickElement("button.sp_choice_type_12", tab.frame.id));
-      await waitFor(
-        () =>
-          new URL(tab.frame.url).pathname === "/privacy-manager/index.html",
-        200,
-        100
-      );
-    }
-    await tab.waitForElement('.type-modal', 20000, tab.frame.id);
-    // reject all button is offered by some sites
     try {
-      const path = await Promise.race([
-        tab.waitForElement('.sp_choice_type_REJECT_ALL', 2000, tab.frame.id).then(r => 0),
-        tab.waitForElement('.reject-toggle', 2000, tab.frame.id).then(() => 1),
-        tab.waitForElement('.pm-features', 2000, tab.frame.id).then(r => 2),
-      ]);
-      if (path === 0) {
-        await tab.wait(1000);
-        return await success(tab.clickElement('.sp_choice_type_REJECT_ALL', tab.frame.id))
-      } else if (path === 1) {
-        await tab.clickElement('.reject-toggle', tab.frame.id)
-      } else {
-        await tab.waitForElement('.pm-features', 10000, tab.frame.id);
-        await tab.clickElements('.checked > span', tab.frame.id);
-        if (await tab.elementExists('.chevron',  tab.frame.id)) {
-          await tab.clickElement('.chevron', tab.frame.id)
+      tab.hideElements(["div[id^='sp_message_container_']"])
+      if (!this.isManagerOpen(tab)) {
+        if (!await waitFor(() => !!tab.frame, 30, 100)) {
+          throw "Frame never opened";
         }
+        if (!await tab.elementExists("button.sp_choice_type_12", tab.frame.id)) {
+          // do not sell button
+          return tab.clickElement('button.sp_choice_type_13', tab.frame.id);
+        }
+        await success(tab.clickElement("button.sp_choice_type_12", tab.frame.id));
+        await waitFor(
+          () =>
+            new URL(tab.frame.url).pathname === "/privacy-manager/index.html",
+          200,
+          100
+        );
       }
-    } catch(e) {}
-    return await tab.clickElement('.sp_choice_type_SAVE_AND_EXIT', tab.frame.id);
+      await tab.waitForElement('.type-modal', 20000, tab.frame.id);
+      // reject all button is offered by some sites
+      try {
+        const path = await Promise.race([
+          tab.waitForElement('.sp_choice_type_REJECT_ALL', 2000, tab.frame.id).then(r => 0),
+          tab.waitForElement('.reject-toggle', 2000, tab.frame.id).then(() => 1),
+          tab.waitForElement('.pm-features', 2000, tab.frame.id).then(r => 2),
+        ]);
+        if (path === 0) {
+          await tab.wait(1000);
+          return await success(tab.clickElement('.sp_choice_type_REJECT_ALL', tab.frame.id))
+        } else if (path === 1) {
+          await tab.clickElement('.reject-toggle', tab.frame.id)
+        } else {
+          await tab.waitForElement('.pm-features', 10000, tab.frame.id);
+          await tab.clickElements('.checked > span', tab.frame.id);
+          if (await tab.elementExists('.chevron',  tab.frame.id)) {
+            await tab.clickElement('.chevron', tab.frame.id)
+          }
+        }
+      } catch(e) {}
+      return await tab.clickElement('.sp_choice_type_SAVE_AND_EXIT', tab.frame.id);
+    } finally {
+      tab.undoHideElements();
+    }
   }
 
   async test(tab: TabActor) {
