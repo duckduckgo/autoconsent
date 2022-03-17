b322e57b2d1c3f860dc57a7b93178833f60c2101
jdorweiler <jason@duckduckgo.com>
2022-03-17 14:52:18 -0600

    remove unused cmps

--- lib/cmps/all.ts
+++ lib/cmps/all.ts
@@ -4,8 +4,6 @@ import CookieBot from './cookiebot';
 import SourcePoint from './sourcepoint';
 import ContentManager from './consentmanager';
 import Evidon from './evidon';
-import Facebook from './facebook';
-import Instagram from './instagram';
 import { AutoConsentCMPRule } from '../rules';
 
 const rules = [
@@ -14,8 +12,6 @@ const rules = [
   new SourcePoint(),
   new ContentManager(),
   new Evidon(),
-  new Facebook(),
-  new Instagram(),
 ];
 
 export function createAutoCMP(config: AutoConsentCMPRule): AutoConsent {
