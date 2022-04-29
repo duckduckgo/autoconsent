e227ef66dca599a0fe4fd36afa191351db443c47
Sam Macbeth <smacbeth@duckduckgo.com>
2022-04-29 11:38:31 +0200

    Update tests where the site has removed the CMP

--- tests/cookiebot.spec.ts
+++ tests/cookiebot.spec.ts
@@ -1,9 +1,7 @@
 import generateCMPTests from "./runner";
 
 generateCMPTests('Cybotcookiebot', [
-    'https://www.zentrum-der-gesundheit.de/',
     'https://www.wohnen.de/',
-    'https://www.history.de/',
     'https://www.zwilling.com/de/',
     'https://forums.cpanel.net/',
 ]);
--- tests/optanon.spec.ts
+++ tests/optanon.spec.ts
@@ -1,7 +1,6 @@
 import generateCMPTests from "./runner";
 
 generateCMPTests('com_optanon', [
-    'https://www.myfonts.com/',
     'https://developer.atlassian.com/',
 ], {
     testOptOut: false,
