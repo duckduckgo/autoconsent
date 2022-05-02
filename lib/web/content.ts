b03229d582d70137bc47c34f8b4ef5860e671f53
jdorweiler <jason@duckduckgo.com>
2022-05-02 12:19:00 -0600

    revet visible change

--- lib/web/content.ts
+++ lib/web/content.ts
@@ -25,7 +25,7 @@ export default function handleMessage(message: ContentScriptMessage, debug = fal
     const elem = document.querySelectorAll<HTMLElement>(message.selector);
     const results = new Array(elem.length);
     elem.forEach((e, i) => {
-      results[i] = e.offsetParent !== null || window.getComputedStyle(e).display !== "none";
+      results[i] = e.offsetParent !== null || window.getComputedStyle(e).display !== "none" || e.style?.display !== "none";
     });
     if (results.length === 0) {
       return false;
