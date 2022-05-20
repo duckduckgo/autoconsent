342e38053e44fbc947290f54b391acb6296e3d08
Maxim <maks.tsoy@gmail.com>
2022-05-20 14:47:20 +0200

    Set up playwright tests

--- tests/runner.ts
+++ tests/runner.ts
@@ -1,61 +1,84 @@
-import fs from 'fs/promises';
+import fs from "fs";
 import path from 'path';
-import { test, expect } from '@playwright/test';
-import * as autoconsent from '../dist/autoconsent.puppet';
-import * as extraRules from '../rules/rules.json';
+import { test, expect, Page, Frame } from "@playwright/test";
+import { waitFor } from "../lib/web/content-utils";
 
-const consentomatic = extraRules.consentomatic;
-const rules = [
-    ...autoconsent.rules,
-    ...Object.keys(consentomatic).map(name => new autoconsent.ConsentOMaticCMP(`com_${name}`, consentomatic[name])),
-    ...extraRules.autoconsent.map(spec => autoconsent.createAutoCMP(spec)),
-];
-const screenshotDir = `./screenshots`;
-const testRegion = (process.env.REGION || 'NA').trim();
-
-async function ensureScreenshotDir() {
-    try {
-        await fs.stat(screenshotDir);
-    } catch(e) {
-        await fs.mkdir(screenshotDir);
-    }
-}
+const testRegion = (process.env.REGION || "NA").trim();
 
 type TestOptions = {
-    testOptOut: boolean;
-    testSelfTest: boolean;
-    skipRegions?: string[];
-}
+  testOptOut: boolean;
+  testSelfTest: boolean;
+  skipRegions?: string[];
+};
 const defaultOptions: TestOptions = {
-    testOptOut: true,
-    testSelfTest: true,
-    skipRegions: [],
+  testOptOut: true,
+  testSelfTest: true,
+  skipRegions: [],
+};
+
+const contentScript = fs.readFileSync(
+  path.join(__dirname, "../dist/autoconsent.puppet.js"),
+  "utf8"
+);
+
+export async function injectContentScript(page: Page | Frame) {
+  try {
+    await page.evaluate(contentScript);
+  } catch (e) {
+    // frame was detached
+    console.log(e);
+  }
 }
 
-export function generateTest(url: string, expectedCmp: string, options: TestOptions = { testOptOut: true, testSelfTest: true }) {
-    test(`${url.split('://')[1]} .${testRegion}`, async ({ page }) => {
-        if (options.skipRegions && options.skipRegions.indexOf(testRegion) !== -1) {
-            test.skip();
-        }
-        await page.goto(url, { waitUntil: 'commit' });
+export function generateTest(
+  url: string,
+  expectedCmp: string,
+  options: TestOptions = { testOptOut: true, testSelfTest: true }
+) {
+  test(`${url.split("://")[1]} .${testRegion}`, async ({ page }) => {
+    if (options.skipRegions && options.skipRegions.indexOf(testRegion) !== -1) {
+      test.skip();
+    }
+    await page.goto(url, { waitUntil: "commit" });
 
-        const tab = autoconsent.attachToPage(page, url, rules, 20, true);
-        await tab.checked;
-        expect(tab.getCMPName()).toBe(expectedCmp);
-        expect(await tab.isPopupOpen(50, 100)).toBeTruthy();
-        if (options.testOptOut) {
-            expect(await tab.doOptOut()).toBeTruthy();
-        }
-        if (options.testSelfTest) {
-            expect(await tab.testOptOutWorked()).toBeTruthy();
-        }
+    // set up a messaging function
+    const received = [];
+    async function messageCallback({ frame }, msg) {
+      received.push(msg);
+      console.log('Received', msg, 'from', await frame.url());
+      if (msg.type === 'optOutResult' && options.testSelfTest) {
+        await frame.evaluate(`autoconsentReceiveMessage({ type: "selfTest" })`);
+      }
+    }
+    await page.exposeBinding("autoconsentSendMessage", messageCallback);
+
+    // inject content scripts into every frame
+    await injectContentScript(page);
+    page.frames().forEach(injectContentScript);
+    page.on("framenavigated", injectContentScript);
+
+    // wait for all messages and assertions
+    await waitFor(() => received.length > 0, 20, 300);
+    expect(received[0]).toEqual({ type: "popupFound", cmp: expectedCmp });
+    if (options.testOptOut) {
+      await waitFor(() => received.length > 1, 20, 300);
+      expect(received[1]).toEqual({ type: "optOutResult", result: true });
+    }
+    if (options.testSelfTest) {
+      await waitFor(() => received.length > 2, 20, 300);
+      expect(received[2]).toEqual({ type: "selfTestResult", result: true });
+    }
+  });
+}
+
+export default function generateCMPTests(
+  cmp: string,
+  sites: string[],
+  options: Partial<TestOptions> = {}
+) {
+  test.describe(cmp, () => {
+    sites.forEach((url) => {
+      generateTest(url, cmp, Object.assign({}, defaultOptions, options));
     });
-}
-
-export default function generateCMPTests(cmp: string, sites: string[], options: Partial<TestOptions> = {}) {
-    test.describe(cmp, () => {
-        sites.forEach((url) => {
-            generateTest(url, cmp, Object.assign({}, defaultOptions, options));
-        });
-    })
+  });
 }
