7ecc47ac45d3b2f47aa370e30a30a98598bed856
Maxim <maks.tsoy@gmail.com>
2022-06-27 17:43:13 +0200

    Fix trustarc

--- lib/cmps/trustarc-frame.ts
+++ lib/cmps/trustarc-frame.ts
@@ -60,6 +60,7 @@ export default class TrustArcFrame extends AutoConsentCMPBase {
   async optOut() {
     await waitFor(() => document.readyState === 'complete', 20, 100);
     await waitForElement(".mainContent[aria-hidden=false]", 5000);
+    await waitForElement(".prefPanel", 1000);
 
     if (click(".rejectAll")) {
       return true;
