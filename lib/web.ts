c799c8eeb415d91b46ddec66822c4ae746da260a
Maxim <maks.tsoy@gmail.com>
2022-07-06 11:19:28 +0200

    Do not request self-test for opt-in

--- lib/web.ts
+++ lib/web.ts
@@ -241,7 +241,7 @@ export default class AutoConsent {
       type: 'optInResult',
       cmp: this.foundCmp ? this.foundCmp.name : 'none',
       result: optInResult,
-      scheduleSelfTest: this.foundCmp && this.foundCmp.hasSelfTest,
+      scheduleSelfTest: false, // self-tests are only for opt-out at the moment
       url: location.href,
     });
 
