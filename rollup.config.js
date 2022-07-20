123add211b128eee26239ac5c5d4501d53ad7783
Sam Macbeth <sammacbeth@users.noreply.github.com>
2022-07-20 12:49:22 +0200

    Support for MV2 on Firefox. (#16)
    * Support for MV2 on Firefox.
    
    * Remove debug code
    
    * Fix quote escaping when running window.eval
    
    * Revert change of onInstalled listener
    
    * Update readme with Firefox instructions
    
    * Put firefox and mv3 addons in separate dirs
    
    * Updated readme instructions for running the extension
    
    * Fix firefox manifest
    
    * Put the addons into dist
    
    * Flatten addon dir
    
    * Update readme.md
    
    Co-authored-by: Maxim Tsoy <maks.tsoy@gmail.com>
    
    Co-authored-by: Maxim Tsoy <maks.tsoy@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import json from '@rollup/plugin-json';
 import typescript from '@rollup/plugin-typescript';
+import copy from 'rollup-plugin-copy';
 import { terser } from "rollup-plugin-terser";
 import pkg from './package.json';
 
@@ -31,17 +32,33 @@ export default [{
 }, {
   input: './addon/background.ts',
   output: [{
-    file: './addon/background.bundle.js',
+    file: './dist/addon-mv3/background.bundle.js',
+    format: 'iife',
+  }, {
+    file: './dist/addon-firefox/background.bundle.js',
     format: 'iife',
   }],
   plugins: [
     typescript(),
     terser(),
+    copy({
+      targets: [
+        {
+          src: ['./addon/icons',  './rules/rules.json'],
+          dest: ['./dist/addon-firefox/', './dist/addon-mv3/']
+        },
+        { src: './addon/mv3/manifest.mv3.json', dest: './dist/addon-mv3/manifest.json' },
+        { src: './addon/firefox/manifest.firefox.json', dest: './dist/addon-firefox/manifest.json' },
+      ]
+    })
   ]
 }, {
   input: './addon/content.ts',
   output: [{
-    file: './addon/content.bundle.js',
+    file: './dist/addon-mv3/content.bundle.js',
+    format: 'iife',
+  }, {
+    file: './dist/addon-firefox/content.bundle.js',
     format: 'iife',
   }],
   plugins: [
