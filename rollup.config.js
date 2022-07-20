125863b52e2214ed69a54fa47e15dab1f2cd7dea
Sam Macbeth <smacbeth@duckduckgo.com>
2022-07-20 11:59:47 +0200

    Flatten addon dir

--- rollup.config.js
+++ rollup.config.js
@@ -55,11 +55,11 @@ export default [{
     copy({
       targets: [
         {
-          src: ['./addon/mv3/icons',  './rules/rules.json'],
+          src: ['./addon/icons',  './rules/rules.json'],
           dest: ['./dist/addon-firefox/', './dist/addon-mv3/']
         },
-        { src: './addon/mv3/manifest.json', dest: './dist/addon-mv3' },
-        { src: './addon/firefox/manifest.json', dest: './dist/addon-firefox' },
+        { src: './addon/mv3/manifest.mv3.json', dest: './dist/addon-mv3/manifest.json' },
+        { src: './addon/firefox/manifest.firefox.json', dest: './dist/addon-firefox/manifest.json' },
       ]
     })
   ]
