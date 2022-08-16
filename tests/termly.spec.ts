5b5548894d85f5c46bfbbaef277dc90e2e7ed2c7
Maxim <maks.tsoy@gmail.com>
2022-08-16 15:39:24 +0200

    Add opt-out rules for Termly

---
+++ tests/termly.spec.ts
@@ -0,0 +1,7 @@
+import generateCMPTests from "../playwright/runner";
+
+generateCMPTests('Termly', [
+    'https://itsalovelylife.com/',
+    'https://visualsbyimpulse.com/',
+    'https://www.iccsafe.org/',
+], {});
