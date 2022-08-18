a8ad838e3ad5320942f49e7114b0635b4fcdb1a1
Maxim <maks.tsoy@gmail.com>
2022-08-18 17:36:19 +0200

    Fix complianz test

--- tests/complianz-categories.spec.ts
+++ tests/complianz-categories.spec.ts
@@ -3,7 +3,12 @@ import generateCMPTests from "../playwright/runner";
 generateCMPTests('Complianz categories', [
     'https://www.expatica.com/de',
     'https://carnavaldecadiztv.com/',
-    'https://hypotheses.org/',
 ], {
     skipRegions: ['GB']
+});
+
+generateCMPTests('Complianz categories', [
+    'https://hypotheses.org/',
+], {
+    skipRegions: ['US', 'GB']
 });
