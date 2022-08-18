157496fbc84020630a40ac6478308588a537e2cb
euw-arasolofotsara1 <arasolofotsara@duckduckgo.com>
2022-08-18 18:35:08 +0200

    removed axeptio.eu from the test for its slow loading

--- tests/axeptio.spec.ts
+++ tests/axeptio.spec.ts
@@ -3,7 +3,6 @@ import generateCMPTests from "../playwright/runner";
 
 generateCMPTests('axeptio', [
     'https://www.welcometothejungle.com/',
-    'https://www.axeptio.eu/',
     'https://frichti.co/landing?redirectTo=/',
     'https://www.dorcel.com/'
 ]
