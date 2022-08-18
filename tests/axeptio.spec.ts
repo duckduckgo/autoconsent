6ba3b2baa826f452bd1058b9335d9fc715a91138
euw-arasolofotsara1 <arasolofotsara@duckduckgo.com>
2022-08-18 13:13:11 +0200

    all working to be sent as PR

--- tests/axeptio.spec.ts
+++ tests/axeptio.spec.ts
@@ -4,7 +4,7 @@ import generateCMPTests from "../playwright/runner";
 generateCMPTests('axeptio', [
     'https://www.welcometothejungle.com/',
     'https://www.axeptio.eu/',
-    'https://frichti.co/',
+    'https://frichti.co/landing?redirectTo=/',
     'https://www.dorcel.com/'
 ]
 );
