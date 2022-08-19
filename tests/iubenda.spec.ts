dc3e48b4945fdec0471a1c443cbbd323bed0a852
Maxim <maks.tsoy@gmail.com>
2022-08-19 12:53:23 +0200

    Add iubenda

---
+++ tests/iubenda.spec.ts
@@ -0,0 +1,8 @@
+import generateCMPTests from "../playwright/runner";
+
+generateCMPTests('iubenda', [
+    'https://www.rossignol.com/us/',
+    'https://www.lofficielusa.com/',
+    'https://www.3bmeteo.com/',
+],{
+});
