ec4fce820b12156468dd8929c65ace2f41ace18c
euw-arasolofotsara1 <arasolofotsara@duckduckgo.com>
2022-08-21 18:15:19 +0200

    pubtech selftest added & localtest passed

--- tests/pubtech.spec.ts
+++ tests/pubtech.spec.ts
@@ -2,6 +2,8 @@ import generateCMPTests from "../playwright/runner";
 
 generateCMPTests('pubtech', [
     'https://www.liberoquotidiano.it/',
+    'https://www.liberoquotidiano.it/?refresh_ce',
     'https://www.telefonino.net/',
+    'https://www.telefonino.net/?refresh-ce',
     'https://www.fattoincasadabenedetta.it/'
 ]);
