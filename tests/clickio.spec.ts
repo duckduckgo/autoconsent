25ae56763197932f13f2cb116aa45fe16cc51941
Maxim <maks.tsoy@gmail.com>
2022-08-24 17:34:15 +0200

    Add clickio

---
+++ tests/clickio.spec.ts
@@ -0,0 +1,8 @@
+import generateCMPTests from "../playwright/runner";
+
+generateCMPTests('click.io', [
+    'https://www.propertywire.com/',
+    'https://pedsovet.su/',
+    'https://golftoday.co.uk/',
+    'https://www.golfshake.com/',
+], {});
