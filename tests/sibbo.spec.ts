532cdf4a39de972283adaeca0373769f74bb2c11
euw-arasolofotsara1 <arasolofotsara@duckduckgo.com>
2022-09-01 20:06:29 +0200

    to try on jenkins

--- tests/sibbo.spec.ts
+++ tests/sibbo.spec.ts
@@ -3,8 +3,8 @@ import generateCMPTests from "../playwright/runner";
 generateCMPTests('sibbo', [
     'https://www.europafm.com/',
     'https://sibboventures.com/',
-    'https://www.cmmedia.es/',
     'https://www.todopapas.com/',
-    'https://www.parador.es/'
+    'https://www.parador.es/',
+    'https://www.estadiodeportivo.com/'
 ]
 );
