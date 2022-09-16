b9b5b7b8f110f30fb8dde69e0861996fcc149484
Maxim <maks.tsoy@gmail.com>
2022-09-16 13:33:30 +0200

    Don't use Promise.any to prevent issues in polyfilled contexts

--- tests/privacymanager.spec.ts
+++ tests/privacymanager.spec.ts
@@ -1,9 +1,8 @@
 import generateCMPTests from "../playwright/runner";
 
-// FIXME: the popup closes too fast, before sending the success report
 generateCMPTests('privacymanager.io', [
-    // 'https://linuxhint.com/',
-    // 'https://www.immmo.at/',
-    // 'https://eckart.net/gb/en/',
-    // 'https://www.actega.com/emea/en/',
+    'https://linuxhint.com/',
+    'https://www.immmo.at/',
+    'https://eckart.net/gb/en/',
+    'https://www.actega.com/emea/en/',
 ]);
