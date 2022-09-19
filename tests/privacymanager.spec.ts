2eded423a43cf469a3dc5172d1febb98b5e58495
Maxim <maks.tsoy@gmail.com>
2022-09-19 11:48:37 +0200

    tweak test regions for privacymanager.io

--- tests/privacymanager.spec.ts
+++ tests/privacymanager.spec.ts
@@ -3,8 +3,6 @@ import generateCMPTests from "../playwright/runner";
 generateCMPTests('privacymanager.io', [
     'https://linuxhint.com/',
     'https://www.immmo.at/',
-    'https://eckart.net/gb/en/',
-    'https://www.actega.com/emea/en/',
 ], {
     skipRegions: ['US']
 });
