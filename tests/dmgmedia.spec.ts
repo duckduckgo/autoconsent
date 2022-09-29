4a2e0dd42ad4ce707c7d998b8612725d19cca172
Maxim Tsoy <maks.tsoy@gmail.com>
2022-09-29 17:38:13 +0200

    Add rules for DMG Media

---
+++ tests/dmgmedia.spec.ts
@@ -0,0 +1,16 @@
+import generateCMPTests from "../playwright/runner";
+
+generateCMPTests('dmgmedia', [
+    'https://www.dailymail.co.uk/',
+    'https://metro.co.uk/',
+], {
+    skipRegions: ['US']
+});
+
+generateCMPTests('dmgmedia', [
+    'https://www.dailymail.co.uk/',
+    'https://metro.co.uk/',
+], {
+    skipRegions: ['US'],
+    mobile: true
+});
