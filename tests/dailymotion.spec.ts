007f62662247e2056be3200202e51b25f46ee7c2
Maxim Tsoy <maks.tsoy@gmail.com>
2022-10-03 17:34:35 +0200

    Add rule for dailymotion

---
+++ tests/dailymotion.spec.ts
@@ -0,0 +1,14 @@
+import generateCMPTests from "../playwright/runner";
+
+generateCMPTests('dailymotion.com', [
+    'https://www.dailymotion.com/us',
+],{
+    skipRegions: ['US']
+});
+
+generateCMPTests('dailymotion.com', [
+    'https://www.dailymotion.com/us',
+],{
+    skipRegions: ['US'],
+    mobile: true,
+});
