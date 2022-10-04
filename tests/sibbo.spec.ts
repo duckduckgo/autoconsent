e4671fe1ffaebbc0a3adf39ff069245bbbc6105a
Maxim Tsoy <maks.tsoy@gmail.com>
2022-10-04 10:43:30 +0200

    Fix some tests

--- tests/sibbo.spec.ts
+++ tests/sibbo.spec.ts
@@ -3,7 +3,6 @@ import generateCMPTests from "../playwright/runner";
 generateCMPTests('sibbo', [
     'https://www.europafm.com/',
     'https://sibboventures.com/',
-    'https://www.todopapas.com/',
     'https://www.estadiodeportivo.com/',
     'https://www.atresmediapublicidad.com/',
     'https://www.eitb.eus/',
