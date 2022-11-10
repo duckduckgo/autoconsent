1ca169ba4f396b3865e15b003482744d195b0954
Sam Macbeth <smacbeth@duckduckgo.com>
2022-11-10 14:34:17 +0100

    Reduce triggering of affinity.serif.com rule

--- tests/affinity-serif-com.spec.ts
+++ tests/affinity-serif-com.spec.ts
@@ -1,5 +1,5 @@
 import generateCMPTests from "../playwright/runner";
 
 generateCMPTests('affinity.serif.com', [
-    'https://affinity.serif.com/en-us/photo/'
+    'https://affinity.serif.com/en-us/photo/',
 ]);
