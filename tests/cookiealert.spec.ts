7bad39904a76c997a4e92968f29650a45675fa16
Sam Macbeth <smacbeth@duckduckgo.com>
2022-11-10 14:06:17 +0100

    Add cookiealert CMP (cookiebot variant)

---
+++ tests/cookiealert.spec.ts
@@ -0,0 +1,9 @@
+import generateCMPTests from "../playwright/runner";
+
+generateCMPTests('cookiealert', [
+  'https://www.kaufland.de/',
+], {
+  skipRegions: ["US", "GB", "FR"],
+  testSelfTest: false,
+}
+);
