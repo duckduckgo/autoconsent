88c89d7b83c9402d153641c82b72ba1a567b0466
Maxim Tsoy <maks.tsoy@gmail.com>
2023-04-12 11:29:03 +0200

    Tweak the Civic rule (#131)

---
+++ tests/civic-cookie-control.spec.ts
@@ -0,0 +1,8 @@
+import generateCMPTests from "../playwright/runner";
+
+generateCMPTests('civic-cookie-control', [
+  'https://www.birmingham.gov.uk/',
+  'https://planning.org.uk/',
+], {
+  skipRegions: ["US", "DE"]
+});
