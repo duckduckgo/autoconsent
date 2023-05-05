77792497dcab7044e73e1d1e7d92e0df526e46e2
Sam Macbeth <smacbeth@duckduckgo.com>
2023-05-05 17:38:59 +0200

    Add basic devtools.recorder plugin

--- addon/devtools/loader.js
+++ addon/devtools/loader.js
@@ -1,2 +1,38 @@
 /* global chrome */
-chrome.devtools.panels.create('Autoconsent', '/icons/cookie.png', "/devtools/panel.html")
+chrome.devtools.panels.create(
+  "Autoconsent",
+  "/icons/cookie.png",
+  "/devtools/panel.html"
+);
+
+if (chrome.devtools?.recorder) {
+  class MyPlugin {
+    stringify(recording) {
+      const autoconsentSteps = recording.steps
+        .filter((step) => ["click"].includes(step.type))
+        .map(({ selectors }) => {
+          return {
+            waitForThenClick: selectors,
+          };
+        });
+      return Promise.resolve(JSON.stringify(autoconsentSteps, undefined, 4));
+    }
+    stringifyStep(step) {
+      return Promise.resolve(
+        JSON.stringify(
+          {
+            waitForThenClick: step.selectors,
+          },
+          undefined,
+          4
+        )
+      );
+    }
+  }
+
+  chrome.devtools.recorder.registerRecorderExtensionPlugin(
+    new MyPlugin(),
+    /*name=*/ "Autoconsent",
+    /*mediaType=*/ "application/json"
+  );
+}
