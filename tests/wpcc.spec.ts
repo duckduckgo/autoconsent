793b65333770ce9eb138b1c622c6b20ec5ca0a49
euw-arasolofotsara1 <arasolofotsara@duckduckgo.com>
2023-05-12 16:45:29 +0200

    Add cmp wpcc notice banner rule and test

---
+++ tests/wpcc.spec.ts
@@ -0,0 +1,10 @@
+import generateCMPTests from "../playwright/runner";
+
+generateCMPTests('wpcc', [
+  'https://www.aznude.com/',
+  'https://www.iihs.org/',
+  'https://www.e-grammar.org/',
+  'http://thisvid.com/',
+  'https://www.fintrux.com/',
+  'https://hdpornohub.com/',
+], {});
