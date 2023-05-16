0d9c084310402579aa42be8c8b8c7851cd9e8998
euw-arasolofotsara1 <arasolofotsara@duckduckgo.com>
2023-05-16 18:35:55 +0200

    added urlPattern and tests sites

--- tests/xhamster-us.spec.ts
+++ tests/xhamster-us.spec.ts
@@ -2,4 +2,10 @@ import generateCMPTests from "../playwright/runner";
 
 generateCMPTests('xhamster-us', [
   'https://xhamster.com/',
-], {});
+  'https://xhamster2.com/',
+  'https://xhamster3.com/',
+  'https://www.xhamster.com/',
+  'https://www.xhamster2.com/'
+], {
+  onlyRegions: ['US'],
+});
