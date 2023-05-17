a88120466c3c037c4f92bcc49f12ee9f1f5211aa
euw-arasolofotsara1 <arasolofotsara@duckduckgo.com>
2023-05-18 00:28:06 +0200

    added 4 notice banner rules and tests top sites

---
+++ tests/bbb.spec.ts
@@ -0,0 +1,5 @@
+import generateCMPTests from "../playwright/runner";
+
+generateCMPTests('bbb.org', [
+  'https://www.bbb.org/'
+], {});
---
+++ tests/clustrmaps-com.spec.ts
@@ -0,0 +1,5 @@
+import generateCMPTests from "../playwright/runner";
+
+generateCMPTests('clustrmaps.com', [
+  'https://clustrmaps.com/'
+], {});
