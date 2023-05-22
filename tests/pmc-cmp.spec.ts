d0fe99e2dc537554f0fd3b2dccddda8742b5cb72
euw-arasolofotsara1 <arasolofotsara@duckduckgo.com>
2023-05-22 12:57:34 +0200

    lint errors fixed to PMC

--- tests/pmc-cmp.spec.ts
+++ tests/pmc-cmp.spec.ts
@@ -1,7 +1,7 @@
 import generateCMPTests from "../playwright/runner";
 
 generateCMPTests('pmc', [
-    'https://www.rollingstone.com/',
-    'https://www.hollywoodreporter.com/',
+  'https://www.rollingstone.com/',
+  'https://www.hollywoodreporter.com/',
 ],{
 });
