06a59c16bf4721f1ca9b0bbe0a6197ae8f35860c
Amanda RASOLOFOTSARA <50955126+icodebyamanda@users.noreply.github.com>
2023-05-26 11:01:39 +0200

    Round 2: added 5 notice banner rules and tests top US sites (#167)
    * added 4 notice banner rules and tests top sites
    
    * added pmc a cmp notice banner rule and test
    
    * lint errors fixed to PMC
    
    * Added 5 notice banners rules and tests top US site
    
    ---------
    
    Co-authored-by: euw-arasolofotsara1 <arasolofotsara@duckduckgo.com>

---
+++ tests/geeks-for-geeks.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from "../playwright/runner";
+
+generateCMPTests('geeks-for-geeks', [
+  'https://www.geeksforgeeks.org/',
+],{
+});
---
+++ tests/tractor-supply.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from "../playwright/runner";
+
+generateCMPTests('tractor-supply', [
+  'https://www.tractorsupply.com/',
+],{
+});
