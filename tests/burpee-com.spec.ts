04874f81451f231e10f4a81fb5e26a35f0390481
Sam Macbeth <sammacbeth@users.noreply.github.com>
2023-07-18 14:15:12 +0200

    Fix burpee rule matching and add new rule (#211)
    * Fix burpee on granit.com
    
    * Add cookie-manager-popup rule to cover some burpee.com matches.
    
    * Make selector more specific.

--- tests/burpee-com.spec.ts
+++ tests/burpee-com.spec.ts
@@ -1,5 +1,6 @@
 import generateCMPTests from "../playwright/runner";
 
 generateCMPTests('burpee.com', [
-  'https://www.burpee.com/'
+  'https://www.burpee.com/',
+  'https://granit.com/se'
 ], {});
