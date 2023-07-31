e07dee9ede82601707d1659a82d4d70743019735
Maxim Tsoy <maks.tsoy@gmail.com>
2023-07-31 13:04:04 +0200

    Fix ucercentrics-api (#214)

--- tests/usercentrics-api.spec.ts
+++ tests/usercentrics-api.spec.ts
@@ -2,9 +2,11 @@ import generateCMPTests from "../playwright/runner";
 
 generateCMPTests('usercentrics-api', [
   'https://hornbach.de',
-  'https://dm.de',
+  'https://dm.de', // unsafe-eval CSP
   'https://usercentrics.com/',
   'https://shopbetreiber-blog.de/',
+  'https://www.kia.com/us/en',
+  'https://www.sportscheck.com/filialen/dortmund/',
 ], {
   skipRegions: ["US", "GB", "FR"]
 }
