c3d1ed9c1794b2446b2396c194269bf9de70b9be
Maxim Tsoy <maks.tsoy@gmail.com>
2023-08-16 15:02:24 +0200

    Move loose functions into the class (no functional changes here)

--- lib/cmps/base.ts
+++ lib/cmps/base.ts
@@ -88,86 +88,6 @@ export default class AutoConsentCMPBase implements AutoCMP {
   }
 }
 
-async function evaluateRuleStep(rule: AutoConsentRuleStep) {
-  const results = [];
-  if (rule.exists) {
-    results.push(elementExists(rule.exists));
-  }
-  if (rule.visible) {
-    results.push(elementVisible(rule.visible, rule.check));
-  }
-  if (rule.eval) {
-    const res = doEval(rule.eval)
-    results.push(res);
-  }
-  if (rule.waitFor) {
-    results.push(waitForElement(rule.waitFor, rule.timeout));
-  }
-  if (rule.waitForVisible) {
-    results.push(waitForVisible(rule.waitForVisible, rule.timeout, rule.check));
-  }
-  if (rule.click) {
-    results.push(click(rule.click, rule.all));
-  }
-  if (rule.waitForThenClick) {
-    results.push(waitForThenClick(rule.waitForThenClick, rule.timeout, rule.all));
-  }
-  if (rule.wait) {
-    results.push(wait(rule.wait));
-  }
-  if (rule.hide) {
-    results.push(hide(rule.hide, rule.method));
-  }
-  if (rule.if) {
-    if (!rule.if.exists && !rule.if.visible) {
-      console.error('invalid conditional rule', rule.if);
-      return false;
-    }
-    const condition = await evaluateRuleStep(rule.if);
-    enableLogs && console.log('Condition is', condition);
-    if (condition) {
-      results.push(_runRulesSequentially(rule.then));
-    } else if (rule.else) {
-      results.push(_runRulesSequentially(rule.else));
-    }
-  }
-  if (rule.any) {
-    for (const step of rule.any) {
-      if (await evaluateRuleStep(step)) {
-        return true
-      }
-    }
-    return false
-  }
-
-  if (results.length === 0) {
-    enableLogs && console.warn('Unrecognized rule', rule);
-    return false;
-  }
-
-  // boolean and of results
-  const all = await Promise.all(results);
-  return all.reduce((a, b) => a && b, true);
-}
-
-async function _runRulesParallel(rules: AutoConsentRuleStep[]): Promise<boolean> {
-  const results = rules.map(rule => evaluateRuleStep(rule));
-  const detections = await Promise.all(results);
-  return detections.every(r => !!r);
-}
-
-async function _runRulesSequentially(rules: AutoConsentRuleStep[]): Promise<boolean> {
-  for (const rule of rules) {
-    enableLogs && console.log('Running rule...', rule);
-    const result = await evaluateRuleStep(rule);
-    enableLogs && console.log('...rule result', result);
-    if (!result && !rule.optional) {
-      return false;
-    }
-  }
-  return true;
-}
-
 export class AutoConsentCMP extends AutoConsentCMPBase {
 
   constructor(public config: AutoConsentCMPRule) {
@@ -193,14 +113,14 @@ export class AutoConsentCMP extends AutoConsentCMPBase {
 
   async detectCmp() {
     if (this.config.detectCmp) {
-      return _runRulesParallel(this.config.detectCmp);
+      return this._runRulesParallel(this.config.detectCmp);
     }
     return false;
   }
 
   async detectPopup() {
     if (this.config.detectPopup) {
-      return _runRulesSequentially(this.config.detectPopup);
+      return this._runRulesSequentially(this.config.detectPopup);
     }
     return false;
   }
@@ -208,7 +128,7 @@ export class AutoConsentCMP extends AutoConsentCMPBase {
   async optOut() {
     if (this.config.optOut) {
       enableLogs && console.log('Initiated optOut()', this.config.optOut);
-      return _runRulesSequentially(this.config.optOut);
+      return this._runRulesSequentially(this.config.optOut);
     }
     return false;
   }
@@ -216,22 +136,102 @@ export class AutoConsentCMP extends AutoConsentCMPBase {
   async optIn() {
     if (this.config.optIn) {
       enableLogs && console.log('Initiated optIn()', this.config.optIn);
-      return _runRulesSequentially(this.config.optIn);
+      return this._runRulesSequentially(this.config.optIn);
     }
     return false;
   }
 
   async openCmp() {
     if (this.config.openCmp) {
-      return _runRulesSequentially(this.config.openCmp);
+      return this._runRulesSequentially(this.config.openCmp);
     }
     return false;
   }
 
   async test() {
     if (this.hasSelfTest) {
-      return _runRulesSequentially(this.config.test);
+      return this._runRulesSequentially(this.config.test);
     }
     return super.test();
+  }
+
+  async evaluateRuleStep(rule: AutoConsentRuleStep) {
+    const results = [];
+    if (rule.exists) {
+      results.push(elementExists(rule.exists));
+    }
+    if (rule.visible) {
+      results.push(elementVisible(rule.visible, rule.check));
+    }
+    if (rule.eval) {
+      const res = doEval(rule.eval)
+      results.push(res);
+    }
+    if (rule.waitFor) {
+      results.push(waitForElement(rule.waitFor, rule.timeout));
+    }
+    if (rule.waitForVisible) {
+      results.push(waitForVisible(rule.waitForVisible, rule.timeout, rule.check));
+    }
+    if (rule.click) {
+      results.push(click(rule.click, rule.all));
+    }
+    if (rule.waitForThenClick) {
+      results.push(waitForThenClick(rule.waitForThenClick, rule.timeout, rule.all));
+    }
+    if (rule.wait) {
+      results.push(wait(rule.wait));
+    }
+    if (rule.hide) {
+      results.push(hide(rule.hide, rule.method));
+    }
+    if (rule.if) {
+      if (!rule.if.exists && !rule.if.visible) {
+        console.error('invalid conditional rule', rule.if);
+        return false;
+      }
+      const condition = await this.evaluateRuleStep(rule.if);
+      enableLogs && console.log('Condition is', condition);
+      if (condition) {
+        results.push(this._runRulesSequentially(rule.then));
+      } else if (rule.else) {
+        results.push(this._runRulesSequentially(rule.else));
+      }
+    }
+    if (rule.any) {
+      for (const step of rule.any) {
+        if (await this.evaluateRuleStep(step)) {
+          return true
+        }
+      }
+      return false
+    }
+  
+    if (results.length === 0) {
+      enableLogs && console.warn('Unrecognized rule', rule);
+      return false;
+    }
+  
+    // boolean and of results
+    const all = await Promise.all(results);
+    return all.reduce((a, b) => a && b, true);
+  }
+
+  async _runRulesParallel(rules: AutoConsentRuleStep[]): Promise<boolean> {
+    const results = rules.map(rule => this.evaluateRuleStep(rule));
+    const detections = await Promise.all(results);
+    return detections.every(r => !!r);
+  }
+  
+  async _runRulesSequentially(rules: AutoConsentRuleStep[]): Promise<boolean> {
+    for (const rule of rules) {
+      enableLogs && console.log('Running rule...', rule);
+      const result = await this.evaluateRuleStep(rule);
+      enableLogs && console.log('...rule result', result);
+      if (!result && !rule.optional) {
+        return false;
+      }
+    }
+    return true;
   }
 }
