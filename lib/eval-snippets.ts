458bc513b935fce2d6d74d76ca2223849123d40c
Maxim Tsoy <maks.tsoy@gmail.com>
2023-11-17 17:09:40 +0100

    Takealot fix (#292)
    * Fix tekealot.com on mobile
    
    * Enable mobile tests for takealot

--- lib/eval-snippets.ts
+++ lib/eval-snippets.ts
@@ -70,6 +70,7 @@ export const snippets = {
   EVAL_SIRDATA_0: () => document.cookie.includes('euconsent-v2'),
   EVAL_SNIGEL_0: () => !!document.cookie.match('snconsent'),
   EVAL_STEAMPOWERED_0: () => JSON.parse(decodeURIComponent(document.cookie.split(';').find(s => s.trim().startsWith('cookieSettings')).split('=')[1])).preference_state === 2,
+  EVAL_TAKEALOT_0: () => document.body.classList.remove('freeze') || (document.body.style = '') || true,
   EVAL_TARTEAUCITRON_0: () => tarteaucitron.userInterface.respondAll(false) || true,
   EVAL_TARTEAUCITRON_1: () => tarteaucitron.userInterface.respondAll(true) || true,
   EVAL_TARTEAUCITRON_2: () => document.cookie.match(/tarteaucitron=[^;]*/)[0].includes('false'),
--- tests/takealot-com.spec.ts
+++ tests/takealot-com.spec.ts
@@ -2,4 +2,12 @@ import generateCMPTests from "../playwright/runner";
 
 generateCMPTests('takealot.com', [
   'https://www.takealot.com/',
-], {});
+], {
+  mobile: false,
+});
+
+generateCMPTests('takealot.com', [
+  'https://www.takealot.com/',
+], {
+  mobile: true,
+});
