2cf6336d2a22f8e744c0932fcfb0d7fc48b28b76
Maxim Tsoy <maks.tsoy@gmail.com>
2023-12-19 11:03:07 +0100

    Extend the create-rule script

--- rules/create-rule.mjs
+++ rules/create-rule.mjs
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'url';
 import readline from 'readline'
 
 const rulesDir = path.dirname(fileURLToPath(import.meta.url))
+const testsDir = path.join(rulesDir, '../tests')
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
 
@@ -13,23 +14,32 @@ const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
   const filename = `autoconsent/${name}.json`
   const rule = {
     name,
+    vendorUrl: 'https://unknown',
     cosmetic: false,
     runContext: {
       main: true,
       frame: false,
     },
     intermediate: false,
+    prehideSelectors: [],
     detectCmp: [{ exists: '#TODO'}],
     detectPopup: [{ visible: '#TODO'}],
-    optIn: [{ click: '#Accept'}],
-    optOut: [{ click: '#Reject'}],
-    prehideSelectors: [],
+    optIn: [{ waitForThenClick: '#Accept'}],
+    optOut: [{ waitForThenClick: '#Reject'}],
     test: []
   }
   const ruleString = JSON.stringify(rule, undefined, 2)
+  const testString = `import generateCMPTests from "../playwright/runner";
+
+generateCMPTests('${name}', [
+  // TODO: Add test URLs here
+]);
+`
   console.log('Writing rule to', filename)
   console.log(ruleString)
   await fs.promises.writeFile(path.join(rulesDir, filename), ruleString)
+  console.log('Writing test to', `tests/${name}.spec.ts`)
+  await fs.promises.writeFile(path.join(testsDir, `${name}.spec.ts`), testString)
   rl.close()
 })()
 
