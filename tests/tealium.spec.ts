18714ad4aaf32de09b4796db04d82196cbf7d068
Maxim Tsoy <maks.tsoy@gmail.com>
2023-12-20 22:02:43 +0100

    Add test cases for tealium

--- tests/tealium.spec.ts
+++ tests/tealium.spec.ts
@@ -2,5 +2,11 @@ import generateCMPTests from "../playwright/runner";
 
 generateCMPTests('Tealium', [
   // 'https://www.bahn.de/', // uses shadow DOM, see https://app.asana.com/0/1201844467387842/1202635343225979/f
+  'https://www.forcepoint.com/',
   'https://www.lufthansa.com/de/en/homepage',
+  "http://www.tui.com/",
+  "http://www.minted.com/",
+  "http://help.brother-usa.com/",
+  "http://www.americangreetings.com/",
+  "http://www.hagerty.com/"
 ]);
