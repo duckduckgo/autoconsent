178c7b8952433610e024ad954e6398160b5365b4
Sam Macbeth <sammacbeth@users.noreply.github.com>
2024-01-09 13:35:09 +0100

    Test a subset of sites found in the coverage crawl in CI (#323)
    * Add a test to test a subset of sites from the coverage data
    
    * Run subset test on Jenkins
    
    * Use webkit for CI
    
    * Make number of sites configurable
    
    * Allow other branches to be checked out.
    
    * Run tests together in a single step
    
    * Add browser to description
    
    * Increase the test timeout
    
    * Update test result paths
    
    * Don't upload screenshots
    
    * Improve env block
    
    * Revert test root

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,6 @@ const config: PlaywrightTestConfig = {
       use: {
         ...devices['Desktop Safari'],
         proxy,
-        screenshot: 'only-on-failure',
         userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15',
       },
     },
@@ -24,7 +23,6 @@ const config: PlaywrightTestConfig = {
       use: {
         ...devices['iPhone SE'],
         proxy,
-        screenshot: 'only-on-failure',
         userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Mobile/15E148 Safari/604.1',
       },
     },
