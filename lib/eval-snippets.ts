ea8ddaf5612aa3c17e5dd859dd92c3ef0aa8380b
Sam Macbeth <sammacbeth@users.noreply.github.com>
2024-01-11 09:36:51 +0100

    Klaro: fix toggle selectors (#339)
    * Klaro: use "disable all" button when available.
    
    * Exclude 'required-only'
    
    * Support multiline eval snippets and update klaro to test all possible APIs
    
    * Improve Klaro self test snippet
    
    * Tidy spacing
    
    * Handle some more design variants

--- lib/eval-snippets.ts
+++ lib/eval-snippets.ts
@@ -14,7 +14,22 @@ export const snippets = {
   EVAL_COOKIEBOT_3: () => window.Cookiebot.withdraw() || true,
   EVAL_COOKIEBOT_4: () => window.Cookiebot.hide() || true,
   EVAL_COOKIEBOT_5: () => window.Cookiebot.declined === true,
-  EVAL_KLARO_1: () => klaro.getManager().config.services.every(c => c.required || !klaro.getManager().consents[c.name]),
+  EVAL_KLARO_1: () => {
+    const config = globalThis.klaroConfig || (globalThis.klaro?.getManager && globalThis.klaro.getManager().config)
+    if (!config) {
+      // with no klaro globals, we can't test on this page
+      return true
+    }
+    const optionalServices = (config.services || config.apps).filter(s => !s.required).map(s => s.name)
+    if (klaro && klaro.getManager) {
+      const manager = klaro.getManager()
+      return optionalServices.every(name => !manager.consents[name])  
+    } else if (klaroConfig && klaroConfig.storageMethod === 'cookie') {
+      const cookieName = klaroConfig.cookieName || klaroConfig.storageName;
+      const consents = JSON.parse(decodeURIComponent(document.cookie.split(';').find(c => c.trim().startsWith(cookieName)).split('=')[1]))
+      return Object.keys(consents).filter(k => optionalServices.includes(k)).every(k => consents[k] === false)
+    }
+  },
   EVAL_ONETRUST_1: () => window.OnetrustActiveGroups.split(',').filter(s => s.length > 0).length <= 1,
   EVAL_TRUSTARC_TOP: () => window && window.truste && window.truste.eu.bindMap.prefCookie === '0',
 
@@ -112,5 +127,5 @@ export const snippets = {
 
 export function getFunctionBody(snippetFunc: () => any) {
   const snippetStr = snippetFunc.toString();
-  return snippetStr.substring(snippetStr.indexOf("=>") + 2);
+  return `(${snippetStr})()`
 }
--- tests/klaro.spec.ts
+++ tests/klaro.spec.ts
@@ -5,5 +5,6 @@ generateCMPTests('Klaro', [
   'https://www.zeitraum-moebel.de/',
   'https://repisalud.isciii.es/',
   'https://www.innogames.com/',
+  'https://www.lebenslauf.de/',
   // 'https://dspace.library.stonybrook.edu', // polyfills Promise so playwright doesn't work
 ]);
