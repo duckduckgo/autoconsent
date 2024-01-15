a880fe547f460e1e8a8bddc3daa1e50cca48bf29
HoJeong Go <seia@outlook.kr>
2024-01-15 10:30:01 +0900

    Add a rule for admiral CMP

---
+++ tests/admiral.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from "../playwright/runner";
+
+generateCMPTests('admiral', [
+  'https://www.pocket-lint.com/what-rabbit-r1-does-it-use-ai-and-how-does-it-work/',
+  'https://www.getadmiral.com/',
+]);
