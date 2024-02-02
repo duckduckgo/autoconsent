95fb94bc32f6f0dd380870522623e9ac2bc58000
HoJeong Go <seia@outlook.kr>
2024-02-02 14:30:21 +0900

    chore: update selectors

--- tests/admiral.spec.ts
+++ tests/admiral.spec.ts
@@ -1,6 +1,5 @@
 import generateCMPTests from "../playwright/runner";
 
 generateCMPTests('admiral', [
-  'https://www.pocket-lint.com/what-rabbit-r1-does-it-use-ai-and-how-does-it-work/',
   'https://www.getadmiral.com/',
 ]);
