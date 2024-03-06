3bd6ddb3b9d8d51802ae400a71bb15123e4a25b0
Maxim Tsoy <maks.tsoy@gmail.com>
2024-03-06 16:46:51 +0100

    Adapt ci scripts from autofill

---
+++ ci/asana-create-tasks.js
@@ -0,0 +1,169 @@
+/* eslint-disable no-undef */
+/* eslint-disable camelcase */
+const timersPromises = require('node:timers/promises')
+const Asana = require('asana')
+const MarkdownIt = require('markdown-it')
+const {getLink} = require('./release-utils.js')
+const md = new MarkdownIt()
+
+const ASANA_ACCESS_TOKEN = process.env.ASANA_ACCESS_TOKEN
+const commit = process.env.GITHUB_SHA
+const version = process.env.VERSION
+const releaseUrl = process.env.RELEASE_URL || ''
+const releaseNotesRaw = process.env.RELEASE_NOTES
+const releaseNotes = md.render(releaseNotesRaw)
+
+const templateTaskGid = '1202253736774463'
+const autoconsentProjectGid = '1201844467387842'
+const releaseSectionGid = '1202253736774466'
+const projectExtractorRegex = /\[\[project_gids=(.+)]]\s/
+
+/**
+ * @typedef {{taskGid: string, taskUrl: string, displayName: string}} platformData
+ *
+ * @typedef {{
+ *   android: platformData,
+ *   windows: platformData
+ * }} AsanaOutput
+ */
+
+/** @type {AsanaOutput} */
+const platforms = {
+  android: {
+    displayName: 'Android',
+    taskGid: '',
+    taskUrl: ''
+  },
+  windows: {
+    displayName: 'Windows',
+    taskGid: '',
+    taskUrl: ''
+  },
+  ios: {
+    displayName: 'iOS',
+    taskGid: '',
+    taskUrl: ''
+  },
+  macos: {
+    displayName: 'macOS',
+    taskGid: '',
+    taskUrl: ''
+  }
+}
+
+let asana
+
+const setupAsana = () => {
+  asana = Asana.Client.create({
+    'defaultHeaders': {
+      'Asana-Enable': 'new_project_templates,new_user_task_lists,new_goal_memberships'
+    }
+  }).useAccessToken(ASANA_ACCESS_TOKEN)
+}
+
+const duplicateTemplateTask = (templateTaskGid) => {
+  const duplicateOption = {
+    include: ['notes', 'assignee', 'subtasks', 'projects'],
+    name: `Autoconsent release ${version}`,
+    opt_fields: 'html_notes'
+  }
+
+  return asana.tasks.duplicateTask(templateTaskGid, duplicateOption)
+}
+
+const waitForJobSuccess = async (job_gid, attempts = 1) => {
+  const interval = 500
+  const maxAttempts = 20
+
+  return new Promise(async (resolve, reject) => {
+    const { status } = await asana.jobs.getJob(job_gid)
+    if (status === 'succeeded') {
+      return resolve(status)
+    }
+    attempts += 1
+
+    if (attempts > maxAttempts) {
+      return reject(new Error(`The job ${job_gid} took too long to execute`))
+    }
+
+    await timersPromises.setTimeout(interval)
+    return waitForJobSuccess(job_gid, attempts)
+  })
+}
+
+const asanaCreateTasks = async () => {
+  setupAsana()
+
+  // Duplicating template task...
+  const { new_task, gid: duplicateTaskJobGid } = await duplicateTemplateTask(templateTaskGid)
+
+  const { html_notes: notes } = await asana.tasks.getTask(new_task.gid, { opt_fields: 'html_notes' })
+
+  const updatedNotes =
+        notes.replace('[[version]]', version)
+          .replace('[[commit]]', commit)
+          .replace('[[release_url]]', getLink(releaseUrl, 'Autoconsent Release'))
+          .replace('[[notes]]', releaseNotes)
+          .replace(/<\/?p>/ig, '\n')
+
+  // Updating task and moving to Release section...
+  await asana.tasks.updateTask(new_task.gid, {html_notes: updatedNotes})
+
+  await asana.tasks.addProjectForTask(new_task.gid, { project: autoconsentProjectGid, section: releaseSectionGid })
+
+  // The duplicateTask job returns when the task itself has been duplicated, ignoring the subtasks.
+  // We want to wait that the job completes so that we can fetch all the subtasks correctly.
+  await waitForJobSuccess(duplicateTaskJobGid)
+
+  // Getting subtasks...
+  const { data: subtasks } = await asana.tasks.getSubtasksForTask(new_task.gid, {opt_fields: 'name,html_notes,permalink_url'})
+
+  // Updating subtasks and moving to appropriate projects...
+  for (const subtask of subtasks) {
+    const {gid, name, html_notes, permalink_url} = subtask
+
+    const platform = Object.keys(platforms).find(
+      (key) => name.includes(platforms[key].displayName)
+    )
+    if (!platform) throw new Error('Unexpected platform name: ' + name)
+
+    platforms[platform].taskGid = gid
+    platforms[platform].taskUrl = permalink_url
+
+    const newName = name.replace('[[version]]', version)
+    const projectGids = (html_notes.match(projectExtractorRegex)?.[1] || '').split(',')
+
+    const subtaskNotes =
+            html_notes.replace(projectExtractorRegex, '')
+              .replace('[[notes]]', updatedNotes)
+
+    await asana.tasks.updateTask(gid, { name: newName, html_notes: subtaskNotes })
+
+    for (const projectGid of projectGids) {
+      await asana.tasks.addProjectForTask(gid, { project: projectGid, insert_after: null })
+    }
+  }
+
+  const finalNotes =
+        updatedNotes
+          .replace('<li>[[pr_url]]</li>', version)
+          .replace('<li>[[extra_content]]</li>', version)
+
+  await asana.tasks.updateTask(new_task.gid, {html_notes: finalNotes})
+
+  const jsonString = JSON.stringify(platforms)
+  return {stdout: jsonString}
+}
+
+asanaCreateTasks()
+  .then((result) => {
+    // The log is needed to read the value from the bash context
+    console.log(result.stdout)
+  })
+  .catch((e) => {
+    // The Asana API returns errors in e.value.errors. If that's undefined log whatever else we got
+    console.error(e.value?.errors || e)
+    process.exit(1)
+  })
+
+module.exports = {asanaCreateTasks}
---
+++ ci/create-pr-template.js
@@ -0,0 +1,66 @@
+const {readFileSync} = require('fs')
+const {join} = require('path')
+const {replaceAllInString} = require('./release-utils.js')
+const cwd = join(__dirname, '..')
+const filepath = (...path) => join(cwd, ...path)
+
+/**
+ * @typedef {{
+ *   releaseNotesRaw: string,
+ *   asanaOutputRaw: string,
+ *   releaseUrl: string,
+ *   version: string
+ * }} CreatePRTemplateData
+ * @typedef {'android' | 'extensions' | 'bsk' | 'ios' | 'macos' | 'windows'} ReleasePlatform
+ */
+
+const platform = /** @type {ReleasePlatform} */ (process.argv[2])
+
+/** @type {CreatePRTemplateData} */
+const data = {
+  version: process.env.VERSION || '',
+  releaseUrl: process.env.RELEASE_URL || '',
+  releaseNotesRaw: process.env.RELEASE_NOTES || '',
+  asanaOutputRaw: process.env.ASANA_OUTPUT || '{}'
+}
+
+/**
+ * Outputs the PR template populated with data
+ * @param {ReleasePlatform} platform
+ * @param {CreatePRTemplateData} data
+ * @returns {string}
+ */
+function createPRTemplate (platform, data) {
+  const asanaOutput = JSON.parse(data.asanaOutputRaw)
+  const templatePath = filepath(`./release/clients_pr_template.md`)
+  const template = readFileSync(templatePath, 'utf8')
+
+  const asanaUrlRegex = /\[\[asana_url]]/
+  const autoconsentReleaseUrlRegex = /\[\[autoconsent_release_url]]/
+  const extraContentRegex = /\[\[extra_content]]/
+  const versionRegex = /\[\[version]]/
+  const descriptionRegex = /\[\[description]]/
+
+  let extraContent = ''
+
+  let asanaUrl = asanaOutput[platform]?.taskUrl
+
+  if (['ios', 'macos'].includes(platform)) {
+    asanaUrl = asanaOutput.bsk?.taskUrl
+    extraContent = `BSK PR: ${data.bskPrUrl}`
+  }
+
+  const updatedTemplate = replaceAllInString(template, [
+    [asanaUrlRegex, asanaUrl],
+    [autoconsentReleaseUrlRegex, data.releaseUrl],
+    [extraContentRegex, extraContent],
+    [versionRegex, data.version],
+    [descriptionRegex, data.releaseNotesRaw]
+  ])
+  return updatedTemplate
+}
+
+// The log is needed to read the value from the bash context
+console.log(createPRTemplate(platform, data))
+
+module.exports = {createPRTemplate}
