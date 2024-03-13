97d61540e4e2484adcb15b7a32e7153e66e6965b
Maxim Tsoy <maks.tsoy@gmail.com>
2024-03-13 09:30:29 +0100

    DDG release automation (#389)
    * Adapt ci scripts from autofill
    
    * trigger on release-automation push
    
    * Use another template task
    
    * Mock values
    
    * Update the github token
    
    * handle empty project references
    
    * Fix PR generation
    
    * mock release data for testing
    
    * test
    
    * Create PRs to windows, ios, and macos
    
    * test windows and macos only
    
    * Use the correct GH token for checkout
    
    * test all jobs together
    
    * mock release notes
    
    * debug output notes
    
    * massage release notes
    
    * Workaround Asana html parser
    
    * change PR title
    
    * Remove debug trigger

---
+++ ci/clients_pr_template.md
@@ -0,0 +1,12 @@
+Task/Issue URL: [[asana_url]]
+Autoconsent Release: [[autoconsent_release_url]]
+[[extra_content]]
+
+## Description
+Updates Autoconsent to version [[[version]]]([[autoconsent_release_url]]).
+
+### Autoconsent [[version]] release notes
+[[description]]
+
+## Steps to test
+This release has been tested during Autoconsent development. You can check the release notes for more information.
---
+++ ci/release-utils.js
@@ -0,0 +1,61 @@
+/**
+ * Execs a string.replace and ensures the searchRegex is found
+ * @param {string} string
+ * @param {RegExp} searchRegex - Use groupings for more complex operations
+ * @param {string} replace
+ * @returns {string}
+ * @throws {Error} - If the string does not contain the searchRegex
+ */
+function replaceInString (string, searchRegex, replace) {
+  if (!searchRegex.test(string)) {
+    const errorMsg = 'No match found in the string. Check both string and regex.'
+    console.log(errorMsg)
+    console.log(string)
+    throw new Error(errorMsg)
+  }
+  return string.replaceAll(
+    new RegExp(searchRegex.source, 'gi'),
+    replace
+  )
+}
+
+/**
+ * Execs multiple string.replace and ensures the searchRegexes are found
+ * @param {string} string
+ * @param {[searchRegex: RegExp, replace?: string][]} changes
+ * @returns {string}
+ * @throws {Error} - If the string does not contain the searchRegex
+ */
+function replaceAllInString (string, changes) {
+  let updatedFile = string
+  for (const [searchRegex, replace = ''] of changes) {
+    updatedFile = replaceInString(updatedFile, searchRegex, replace)
+  }
+  return updatedFile
+}
+
+/**
+ * Creates an html <a> tag from a url and an optional anchor text
+ * @param {string} url
+ * @param {string} [anchor]
+ * @returns {string}
+ */
+function getLink (url, anchor) {
+  return `<a href="${url}">${anchor || url}</a>`
+}
+
+/**
+ * Wraps the passed content string within a <li></li>
+ * @param {string} content
+ * @returns {string}
+ */
+function wrapInLi (content) {
+  return `<li>${content}</li>`
+}
+
+module.exports = {
+  replaceInString,
+  replaceAllInString,
+  getLink,
+  wrapInLi
+}
