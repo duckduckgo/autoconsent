75b4ca60fcdf23397b5a2fe5bd514a8a61fc5431
HoJeong Go <seia@outlook.kr>
2024-05-15 17:49:21 +0900

    Add travelonbags.com (#438)
    * Add travelonbags.com
    
    refs https://github.com/ghostery/broken-page-reports/issues/634
    
    * feat: improve targets on generic rule aquasana.com
    
    * Clean up aquasana.com
    
    ---------
    
    Co-authored-by: Maxim Tsoy <maks.tsoy@gmail.com>

--- tests/aquasana-com.spec.ts
+++ tests/aquasana-com.spec.ts
@@ -1,5 +1,8 @@
 import generateCMPTests from "../playwright/runner";
 
 generateCMPTests('aquasana.com', [
-  'https://www.aquasana.com/',
+  'https://www.travelonbags.com/',
+  'https://www.theworks.co.uk/',
+  'https://www.martinguitar.com/',
+  'https://www.hpb.com/',
 ], {});
