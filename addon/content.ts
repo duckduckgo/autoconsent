1e908bb9d86b2710376254bdeaaddbd4cf27b2c7
Maxim Tsoy <maks.tsoy@gmail.com>
2024-08-05 15:18:00 +0200

    remove the perf metrics code

--- addon/content.ts
+++ addon/content.ts
@@ -1,6 +1,5 @@
 import { BackgroundMessage } from "../lib/messages";
 import AutoConsent from "../lib/web";
-import { collectMetrics } from "../lib/web";
 
 const consent = new AutoConsent(chrome.runtime.sendMessage);
 chrome.runtime.onMessage.addListener((message: BackgroundMessage) => {
@@ -8,10 +7,3 @@ chrome.runtime.onMessage.addListener((message: BackgroundMessage) => {
 });
 
 chrome.runtime.connect({ name: `instance-${consent.id}` });
-
-collectMetrics().then((results) => {
-  // pass the results to the native code. ddgPerfMetrics is a custom JS interface
-  const resultsJson = JSON.stringify(results);
-  console.log(location.href + ' ' + resultsJson);
-  window.alert(`PERF METRICS: ` + resultsJson);
-});
