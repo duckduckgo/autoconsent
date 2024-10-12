76c20c73e8baa0cfa1c5c16185fc96eebd8db490
ondrej.frei <frei.ondrej@pm.me>
2024-10-12 17:05:14 +0200

    #333 CR: Align esbuild settings in web-test-runner.config.mjs with prod ones

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,9 +1,16 @@
 import {esbuildPlugin} from '@web/dev-server-esbuild';
+import {fileURLToPath} from "url";
 
 export default {
   files: [
     'tests-wtr/**/*.html',
   ],
   nodeResolve: true,
-  plugins: [esbuildPlugin({ts: true})],
+  plugins: [
+    esbuildPlugin({
+      ts: true,
+      tsconfig: fileURLToPath(new URL('./tsconfig.json', import.meta.url)),
+      target: 'es2021',
+    })
+  ],
 };
