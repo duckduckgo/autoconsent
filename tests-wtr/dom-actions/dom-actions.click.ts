6142ac68758d1a8dd6d1c5bbbe58d8d1e86656a3
ondrej.frei <frei.ondrej@pm.me>
2024-10-14 21:00:04 +0200

    Extract helper utils.instantiateDomActions()

--- tests-wtr/dom-actions/dom-actions.click.ts
+++ tests-wtr/dom-actions/dom-actions.click.ts
@@ -1,10 +1,5 @@
 import { expect } from '@esm-bundle/chai';
-import {DomActions} from "../../lib/dom-actions";
-
-function instantiateDomActions() {
-  // @ts-expect-error we don't need to add a full AutoConsent instance, DomActions only needs config.logs from it
-  return new DomActions({config: {logs: {rulesteps: false, lifecycle: false, evals: false, errors: false, messages: false}}})
-}
+import { utils } from "./utils";
 
 // must be run from dom-actions.click.html
 describe('click', () => {
@@ -27,7 +22,7 @@ describe('click', () => {
 
   it('clicks on a button', () => {
     // Given
-    const domActions = instantiateDomActions();
+    const domActions = utils.instantiateDomActions();
 
     // When
     const clickedSuccessfully = domActions.click('#test');
@@ -39,7 +34,7 @@ describe('click', () => {
 
   it('clicks all upon multiple matches when all=true', () => {
     // Given
-    const domActions = instantiateDomActions();
+    const domActions = utils.instantiateDomActions();
 
     // When
     const clickedSuccessfully = domActions.click('button', true);
@@ -52,7 +47,7 @@ describe('click', () => {
 
   it('clicks only first one upon multiple matches when all=false', () => {
     // Given
-    const domActions = instantiateDomActions();
+    const domActions = utils.instantiateDomActions();
 
     // When
     const clickedSuccessfully = domActions.click('button');
@@ -65,7 +60,7 @@ describe('click', () => {
 
   it('clicks by chained selector', () => {
     // Given
-    const domActions = instantiateDomActions();
+    const domActions = utils.instantiateDomActions();
 
     // When
     const clickedSuccessfully = domActions.click(['#second', 'button'])
@@ -78,7 +73,7 @@ describe('click', () => {
 
   it('clicks by xpath selector', () => {
     // Given
-    const domActions = instantiateDomActions();
+    const domActions = utils.instantiateDomActions();
 
     // When
     const clickedSuccessfully = domActions.click(['xpath///*[@id="second"]/button'])
@@ -91,7 +86,7 @@ describe('click', () => {
 
   it('clicks an open shadow dom element', () => {
     // Given
-    const domActions = instantiateDomActions();
+    const domActions = utils.instantiateDomActions();
 
     let clickCounterShadowRoot = 0
 
---
+++ tests-wtr/dom-actions/utils.ts
@@ -0,0 +1,8 @@
+import {DomActions} from "../../lib/dom-actions";
+
+export const utils = {
+  instantiateDomActions: () => {
+    // @ts-expect-error we don't need to add a full AutoConsent instance, DomActions only needs config.logs from it
+    return new DomActions({config: {logs: {rulesteps: false, lifecycle: false, evals: false, errors: false, messages: false}}})
+  }
+}
