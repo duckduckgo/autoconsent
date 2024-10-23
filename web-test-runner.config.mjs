b54b8be82279953a9faacbc4bac559a871da649c
HoJeong Go <seia@outlook.kr>
2024-10-23 12:28:02 +0200

    chore: apply eslint autofix

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,16 +1,16 @@
-import {esbuildPlugin} from '@web/dev-server-esbuild';
-import {fileURLToPath} from "url";
+import { esbuildPlugin } from '@web/dev-server-esbuild'
+import { fileURLToPath } from 'url'
 
 export default {
-  files: [
-    'tests-wtr/**/*.html',
-  ],
-  nodeResolve: true,
-  plugins: [
-    esbuildPlugin({
-      ts: true,
-      tsconfig: fileURLToPath(new URL('./tsconfig.json', import.meta.url)),
-      target: 'es2021',
-    })
-  ],
-};
+    files: [
+        'tests-wtr/**/*.html'
+    ],
+    nodeResolve: true,
+    plugins: [
+        esbuildPlugin({
+            ts: true,
+            tsconfig: fileURLToPath(new URL('./tsconfig.json', import.meta.url)),
+            target: 'es2021'
+        })
+    ]
+}
