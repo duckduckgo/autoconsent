dbbaa4d4c13db77cb2ee198079f6329b743f7225
HoJeong Go <seia@outlook.kr>
2024-10-23 12:33:28 +0900

    feat: check bigcommerce cmp with eval

--- lib/eval-snippets.ts
+++ lib/eval-snippets.ts
@@ -61,6 +61,7 @@ export const snippets = {
   EVAL_AXEPTIO_0: () => document.cookie.includes('axeptio_authorized_vendors=%2C%2C'),
   EVAL_BAHN_TEST: () => utag.gdpr.getSelectedCategories().length === 1,
   EVAL_BING_0: () => document.cookie.includes('AL=0') && document.cookie.includes('AD=0') && document.cookie.includes('SM=0'),
+  EVAL_BIGCOMMERCE_0: () => typeof window.consentManager !== 'undefined',
   EVAL_BLOCKSY_0: () => document.cookie.includes('blocksy_cookies_consent_accepted=no'),
   EVAL_BORLABS_0: () => !JSON.parse(decodeURIComponent(document.cookie.split(';').find(c => c.indexOf('borlabs-cookie') !== -1).split('=', 2)[1])).consents.statistics,
   EVAL_BUNDESREGIERUNG_DE_0: () => document.cookie.match('cookie-allow-tracking=0'),
