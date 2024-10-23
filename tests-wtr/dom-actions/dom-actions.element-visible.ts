c7fa5662fc3cb31b175148732f005b93a7ac4710
ondrej.frei <frei.ondrej@pm.me>
2024-10-23 21:02:07 +0200

    Reuse DomActions across dom-actions.element-visible.ts and combine When + Then phases

--- tests-wtr/dom-actions/dom-actions.element-visible.ts
+++ tests-wtr/dom-actions/dom-actions.element-visible.ts
@@ -1,132 +1,68 @@
 import {expect} from '@esm-bundle/chai';
 import {instantiateDomActions} from "./utils";
+import {DomActions} from "../../lib/dom-actions";
 
 // must be run from dom-actions.element-visible.html
 describe('elementVisible', () => {
+  let domActions: DomActions
+
+  beforeEach(() => {
+    // Given
+    domActions = instantiateDomActions();
+  })
+
   describe('mode: "none"', () => {
     it('should return true if no elements found by selector', () => {
-      // Given
-      const domActions = instantiateDomActions();
-
-      // When
-      const visible = domActions.elementVisible('#not-found', 'none');
-
-      // Then
-      expect(visible).to.be.true
+      // When + Then
+      expect(domActions.elementVisible('#not-found', 'none')).to.be.true
     })
     it('should return false if all elements are visible', () => {
-      // Given
-      const domActions = instantiateDomActions();
-
-      // When
-      const visible = domActions.elementVisible(['#all-visible', 'button'], 'none');
-
-      // Then
-      expect(visible).to.be.false
+      // When + Then
+      expect(domActions.elementVisible(['#all-visible', 'button'], 'none')).to.be.false
     })
     it('should return false if some elements are visible', () => {
-      // Given
-      const domActions = instantiateDomActions();
-
-      // When
-      const visible = domActions.elementVisible(['#some-visible', 'button'], 'none');
-
-      // Then
-      expect(visible).to.be.false
+      // When + Then
+      expect(domActions.elementVisible(['#some-visible', 'button'], 'none')).to.be.false
     })
     it('should return true if no elements are visible', () => {
-      // Given
-      const domActions = instantiateDomActions();
-
-      // When
-      const visible = domActions.elementVisible(['#none-visible', 'button'], 'none');
-
-      // Then
-      expect(visible).to.be.true
+      // When + Then
+      expect(domActions.elementVisible(['#none-visible', 'button'], 'none')).to.be.true
     })
   })
   describe('mode: "any"', () => {
     it('should return false if no elements found by selector', () => {
-      // Given
-      const domActions = instantiateDomActions();
-
-      // When
-      const visible = domActions.elementVisible('#not-found', 'any');
-
-      // Then
-      expect(visible).to.be.false
+      // When + Then
+      expect(domActions.elementVisible('#not-found', 'any')).to.be.false
     })
     it('should return false if no elements are visible', () => {
-      // Given
-      const domActions = instantiateDomActions();
-
-      // When
-      const visible = domActions.elementVisible(['#none-visible', 'button'], 'any');
-
-      // Then
-      expect(visible).to.be.false
+      // When + Then
+      expect(domActions.elementVisible(['#none-visible', 'button'], 'any')).to.be.false
     })
     it('should return true if some elements are visible', () => {
-      // Given
-      const domActions = instantiateDomActions();
-
-      // When
-      const visible = domActions.elementVisible(['#some-visible', 'button'], 'any');
-
-      // Then
-      expect(visible).to.be.true
+      // When + Then
+      expect(domActions.elementVisible(['#some-visible', 'button'], 'any')).to.be.true
     })
     it('should return true if all elements are visible', () => {
-      // Given
-      const domActions = instantiateDomActions();
-
-      // When
-      const visible = domActions.elementVisible(['#all-visible', 'button'], 'any');
-
-      // Then
-      expect(visible).to.be.true
+      // When + Then
+      expect(domActions.elementVisible(['#all-visible', 'button'], 'any')).to.be.true
     })
   })
   describe('mode: "all"', () => {
     it('should return false if no elements found by selector', () => {
-      // Given
-      const domActions = instantiateDomActions();
-
-      // When
-      const visible = domActions.elementVisible('#not-found', 'all');
-
-      // Then
-      expect(visible).to.be.false
+      // When + Then
+      expect(domActions.elementVisible('#not-found', 'all')).to.be.false
     })
     it('should return false if no elements are visible', () => {
-      // Given
-      const domActions = instantiateDomActions();
-
-      // When
-      const visible = domActions.elementVisible(['#none-visible', 'button'], 'all');
-
-      // Then
-      expect(visible).to.be.false
+      // When + Then
+      expect(domActions.elementVisible(['#none-visible', 'button'], 'all')).to.be.false
     })
     it('should return false if some elements are visible', () => {
-      // Given
-      const domActions = instantiateDomActions();
-
-      // When
-      const visible = domActions.elementVisible(['#some-visible', 'button'], 'all');
-
-      // Then
-      expect(visible).to.be.false
+      // When + Then
+      expect(domActions.elementVisible(['#some-visible', 'button'], 'all')).to.be.false
     })
     it('should return true if all elements are visible', () => {
-      // Given
-      const domActions = instantiateDomActions();
-
-      // When
-      const visible = domActions.elementVisible(['#all-visible', 'button'], 'all');
-
-      // Then
-      expect(visible).to.be.true
+      // When + Then
+      expect(domActions.elementVisible(['#all-visible', 'button'], 'all')).to.be.true
     })
   })
 })
