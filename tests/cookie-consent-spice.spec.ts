98efc823eacbbae4df9c0c22a5decc26a8e397e6
Sam Macbeth <sammacbeth@users.noreply.github.com>
2024-11-22 20:07:14 +0100

    Add cookie-consent-spice rule (#540)

---
+++ tests/cookie-consent-spice.spec.ts
@@ -0,0 +1,3 @@
+import generateCMPTests from '../playwright/runner';
+
+generateCMPTests('cookie-consent-spice', ['https://www.thomann.de/ie/risa_uke_solid_concert.htm', 'https://www.thomannmusic.com/']);
