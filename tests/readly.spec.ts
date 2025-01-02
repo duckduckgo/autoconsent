acc1d595f18e78e9552da9109ad74a7e2b2d9e8d
HoJeong Go <seia@outlook.kr>
2025-01-02 18:40:52 +0900

    fix(lint): run prettier

--- tests/readly.spec.ts
+++ tests/readly.spec.ts
@@ -1,5 +1,3 @@
-import generateCMPTests from "../playwright/runner";
+import generateCMPTests from '../playwright/runner';
 
-generateCMPTests('readly', [
-  'https://go.readly.com/discover'
-], {});
+generateCMPTests('readly', ['https://go.readly.com/discover'], {});
