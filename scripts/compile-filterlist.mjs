090e576e6f3e2fd548dac256da594a12ecf47aea
David Harbage <harbagio@gmail.com>
2025-01-09 17:11:08 -0500

    Tidy up scripts, add automatic retrieval of tranco list, remove filterlists

--- scripts/compile-filterlist.mjs
+++ scripts/compile-filterlist.mjs
@@ -14,8 +14,37 @@ const engine = FiltersEngine.parse(filterlistContent, {
     loadCSPFilters: false,
 });
 const serializedEngine = engine.serialize();
-const engineJson = JSON.stringify(Array.from(new Uint16Array(serializedEngine.buffer, 0, Math.floor(serializedEngine.buffer.byteLength / 2))));
-const engineLength = serializedEngine.length
+const properlySizedBuffer = ensure16BitAlignment(serializedEngine.buffer);
+const engineJson = JSON.stringify(Array.from(new Uint16Array(properlySizedBuffer)));
+const engineLength = serializedEngine.length;
+
+/**
+ * Ensures the buffer is aligned to a 16-bit boundary by padding if necessary
+ * @param {Buffer} originalBuffer
+ * @returns {Buffer} - The aligned buffer
+ */
+function ensure16BitAlignment(originalBuffer) {
+    // Check if the buffer's byte length is odd
+    if (originalBuffer.byteLength % 2 !== 0) {
+        // Create a new buffer with an additional byte for padding
+        const paddedBuffer = new ArrayBuffer(originalBuffer.byteLength + 1);
+
+        // Create a Uint8Array view over the original and new buffers
+        const originalView = new Uint8Array(originalBuffer);
+        const paddedView = new Uint8Array(paddedBuffer);
+
+        // Copy the original data into the new buffer
+        paddedView.set(originalView);
+
+        // Add padding byte to end of buffer
+        paddedView[originalBuffer.byteLength] = 0; // Padding byte
+
+        return paddedBuffer;
+    }
+
+    // If already aligned, return the original buffer
+    return originalBuffer;
+}
 
 fs.writeFile(
     path.join(rulesDir, '../lib/filterlist-engine.ts'),
--- scripts/rebuild-filterlist.mjs
+++ scripts/rebuild-filterlist.mjs
@@ -5,31 +5,39 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { parse } from 'csv-parse/sync';
 import { getDomain } from 'tldts-experimental';
+import { Readable } from 'stream';
+import { finished } from 'stream/promises';
 
 const rulesDir = path.join(path.dirname(fileURLToPath(import.meta.url)), '../rules');
 const dataDir = path.join(path.dirname(fileURLToPath(import.meta.url)), '../data');
 
 const easylistRevision = fs.readFileSync(path.join(rulesDir, 'filterlists', 'easylist_revision.txt'), 'utf-8');
 
-const trancoCSV = fs.readFileSync(path.join(dataDir, 'top-1m.csv'), 'utf-8');
 const MAX_DOMAIN_RANK = 100000;
 
-let domainMap
+let domainMap;
 
-async function processFilterList (listFileName) {
+/**
+ * Processes a list, filters out unsupported rules, and removes longtail domain rules
+ * @param {String} listFileName
+ */
+async function processFilterList(listFileName) {
     console.log(`Processing ${listFileName}`);
     const data = fs.readFileSync(path.join(rulesDir, 'filterlists', listFileName), 'utf-8');
     const lines = data.split('\n');
-    const filterlistJSON = {"rules": {}};
+    const filterlistJSON = { rules: {} };
 
-    // first remove irrelevant rules
-    const filteredLines = lines.filter(line => !line.startsWith('!') &&
-                                               !line.startsWith('##') &&
-                                               !line.startsWith('||') &&
-                                               !line.includes(':remove') &&
-                                               !line.includes(':upward') &&
-                                               !line.includes('redirect-rule'));
-    // dump rules into json structure for parsing
+    // Remove unsupported rule types
+    const filteredLines = lines.filter(
+        (line) =>
+            !line.startsWith('!') &&
+            !line.startsWith('##') &&
+            !line.startsWith('||') &&
+            !line.includes(':remove') &&
+            !line.includes(':upward') &&
+            !line.includes('redirect-rule'),
+    );
+    // Dump rules into json structure for parsing
     for (const line in filteredLines) {
         const rule = filteredLines[line];
         const splitRule = rule.split('##');
@@ -47,16 +55,15 @@ async function processFilterList (listFileName) {
         }
     }
 
-    // remove any targets that contain a +js rule. we do it this way because
+    // Remove any targets that contain a +js rule. we do it this way because
     // when a target has a :js rule, we must also remove any css rules
     for (const item in filterlistJSON.rules) {
-        const containsJS = filterlistJSON.rules[item].some(filter => filter.includes('+js'));
+        const containsJS = filterlistJSON.rules[item].some((filter) => filter.includes('+js'));
         if (containsJS) {
             delete filterlistJSON.rules[item];
         }
     }
 
-    // remove longtail domain rules using tranco data
     for (const item in filterlistJSON.rules) {
         let domains;
 
@@ -66,12 +73,12 @@ async function processFilterList (listFileName) {
             domains = [item];
         }
 
-        const filteredDomains = domains.filter(domain => {
+        const filteredDomains = domains.filter((domain) => {
             const tlDomain = getDomain(domain);
-            return isPopularDomain(tlDomain);
+            return domainMap.has(tlDomain);
         });
 
-        // if no domains meet cutoff, remove rule entirely. if one or more
+        // If no domains meet cutoff, remove rule entirely. If one or more
         // domains meet cutoff, remove those that don't
         if (filteredDomains.length === 0) {
             delete filterlistJSON.rules[item];
@@ -82,78 +89,98 @@ async function processFilterList (listFileName) {
         }
     }
 
-    await convertAndWriteADB(filterlistJSON, listFileName);
+    await convertAndWriteABP(filterlistJSON, listFileName);
 }
 
-function isPopularDomain (domain) {
-    // first load tranco csv into Map for easy querying
-    if (typeof domainMap !== 'object') {
-        loadTrancoList();
+/**
+ * Fetch domain popularity data from Tranco and load it into a Map for querying
+ */
+async function loadTrancoList() {
+    const csvLocation = path.resolve(path.join(dataDir, 'top-1m.csv'));
+    if (!fs.existsSync(csvLocation)) {
+        console.log('Fetching latest Tranco list..');
+        // Download most recent list from https://tranco-list.eu using API
+        const date = new Date();
+        let available;
+        let download;
+
+        do {
+            const formattedDate = date.toISOString().split('T')[0].replace('/', '');
+            const res = await (await fetch(`https://tranco-list.eu/api/lists/date/${formattedDate}`)).json();
+
+            available = res.available;
+            download = res.download;
+
+            // Decrement date until a daily list is available
+            date.setTime(date.getTime() - 7 * 24 * 3600000);
+        } while (!available);
+
+        const listData = await fetch(download);
+        await finished(Readable.fromWeb(listData.body).pipe(fs.createWriteStream(csvLocation)));
     }
 
-    const match = domainMap.has(domain);
+    if (typeof domainMap !== 'object') {
+        const trancoCSV = fs.readFileSync(path.join(dataDir, 'top-1m.csv'), 'utf-8');
+        const records = parse(trancoCSV, {
+            columns: false,
+            skip_empty_lines: true,
+        });
 
-    return match;
+        domainMap = new Map();
+
+        records.slice(0, MAX_DOMAIN_RANK).forEach(([key, value]) => {
+            domainMap.set(value, key);
+        });
+    }
 }
 
-function loadTrancoList () {
-    const records = parse(trancoCSV, {
-        columns: false,
-        skip_empty_lines: true
-    });
-
-    domainMap = new Map();
-    
-    // we want to query by domain, not rank
-    records.slice(0, MAX_DOMAIN_RANK).forEach(([key, value]) => {
-        domainMap.set(value, key);
-    });
-}
-
-function convertAndWriteADB(JSONList, fileName) {
+/**
+ * Convert filter list to ABP format and write to disk
+ * @param {Object} JSONList
+ * @param {String} fileName
+ */
+function convertAndWriteABP(JSONList, fileName) {
     return new Promise((resolve, reject) => {
-        const liteFileName = fileName.replace('.', '_lite.');
-        const liteFileLocation = path.join(rulesDir, 'filterlists', liteFileName);
-        const stream = fs.createWriteStream(liteFileLocation, { flags: 'w' });
+        const fileLocation = path.join(rulesDir, 'filterlists', fileName);
+        const stream = fs.createWriteStream(fileLocation, { flags: 'w' });
 
         stream.once('open', () => {
             for (const item in JSONList.rules) {
-                JSONList.rules[item].forEach(filter => {
+                JSONList.rules[item].forEach((filter) => {
                     const abpRule = item + '##' + filter;
                     stream.write(abpRule + '\n');
-                })
+                });
             }
             stream.end();
-        })
+        });
 
         stream.on('finish', () => {
-            console.log(`ABP format list written to ${liteFileLocation}`);
+            console.log(`ABP format list written to ${fileLocation}`);
             resolve();
-        })
+        });
 
         stream.on('error', (err) => {
-            console.error(`An error occurred: ${err.message}`);
+            console.error(`An error occurred while writing ${fileLocation}: ${err.message}`);
         });
     });
-
 }
 
-function combineFilterLists () {
-    console.log(`Combining filterlists`);
-
+/**
+ * Combine all filter lists into a single filterlist.txt
+ */
+function combineFilterLists() {
     // TODO: consider using python-abp (flrender) to generate filterlist properly
-    const filterlistContent =
-`[Adblock Plus 2.0]
+    const filterlistContent = `[Adblock Plus 2.0]
 ! Title: CPM Cosmetic Filter List
 ! Based on EasyList ${easylistRevision}
 ! Generated on ${new Date().toISOString()}
 !------------------------General element hiding rules-------------------------!
 ${fs.readFileSync(path.join(rulesDir, 'filterlists', 'easylist_cookie_general_hide.txt'), 'utf-8')}
 !------------------------Specific element hiding rules------------------------!
-${fs.readFileSync(path.join(rulesDir, 'filterlists', 'easylist_cookie_specific_hide_lite.txt'), 'utf-8')}
-${fs.readFileSync(path.join(rulesDir, 'filterlists', 'easylist_cookie_specific_uBO_lite.txt'), 'utf-8')}
+${fs.readFileSync(path.join(rulesDir, 'filterlists', 'easylist_cookie_specific_hide.txt'), 'utf-8')}
+${fs.readFileSync(path.join(rulesDir, 'filterlists', 'easylist_cookie_specific_uBO.txt'), 'utf-8')}
 !------------------------Rules for international sites------------------------!
-${fs.readFileSync(path.join(rulesDir, 'filterlists', 'easylist_cookie_international_specific_hide_lite.txt'), 'utf-8')}
+${fs.readFileSync(path.join(rulesDir, 'filterlists', 'easylist_cookie_international_specific_hide.txt'), 'utf-8')}
 !---------------------------------Allowlists----------------------------------!
 ${fs.readFileSync(path.join(rulesDir, 'filterlists', 'easylist_cookie_allowlist_general_hide.txt'), 'utf-8')}
 !--------------------------------DDG overrides--------------------------------!
@@ -163,7 +190,11 @@ ${fs.readFileSync(path.join(rulesDir, 'filterlists', 'overrides.txt'), 'utf-8')}
     fs.writeFile(path.join(rulesDir, 'filterlist.txt'), filterlistContent, () => console.log('Written filterlist.txt'));
 }
 
-async function rebuildFilterList () {
+/**
+ * Parse and refine lists using domain popularity, then combine them and write filterlist.txt
+ */
+async function rebuildFilterList() {
+    await loadTrancoList();
     await processFilterList('easylist_cookie_specific_uBO.txt');
     await processFilterList('easylist_cookie_specific_hide.txt');
     await processFilterList('easylist_cookie_international_specific_hide.txt');
