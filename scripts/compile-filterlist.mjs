e8b7f81a4c0198440fe770c4afd3cfd77fd01f0a
David Harbage <harbagio@gmail.com>
2025-01-15 10:07:26 -0500

    Revert "Reduce filter list size by using domain popularity, updating encoding…"
    This reverts commit 1db59c6161e310d3b1aa70f51a3038ca5d9b78d9.

--- scripts/compile-filterlist.mjs
+++ scripts/compile-filterlist.mjs
@@ -14,42 +14,7 @@ const engine = FiltersEngine.parse(filterlistContent, {
     loadCSPFilters: false,
 });
 const serializedEngine = engine.serialize();
-const properlySizedBuffer = ensure32BitAlignment(serializedEngine.buffer);
-const engineJson = JSON.stringify(Array.from(new Uint32Array(properlySizedBuffer)));
-const engineLength = serializedEngine.length;
-
-/**
- * Ensures the buffer is aligned to a 32-bit boundary by padding if necessary
- * @param {Buffer} originalBuffer
- * @returns {Buffer} - The aligned buffer
- */
-
-function ensure32BitAlignment(originalBuffer) {
-    // Calculate the necessary padding for 32-bit alignment
-    const padding = originalBuffer.byteLength % 4 === 0 ? 0 : 4 - (originalBuffer.byteLength % 4);
-
-    if (padding !== 0) {
-        // Create a new buffer with the required padding for 32-bit alignment
-        const paddedBuffer = new ArrayBuffer(originalBuffer.byteLength + padding);
-
-        // Create a Uint8Array view over the original and new buffers
-        const originalView = new Uint8Array(originalBuffer);
-        const paddedView = new Uint8Array(paddedBuffer);
-
-        // Copy the original data into the new buffer
-        paddedView.set(originalView);
-
-        // Set the padding bytes to zero
-        for (let i = originalBuffer.byteLength; i < paddedBuffer.byteLength; i++) {
-            paddedView[i] = 0;
-        }
-
-        return paddedBuffer;
-    }
-
-    // If already aligned, return the original buffer
-    return originalBuffer;
-}
+const engineJson = JSON.stringify(Array.from(serializedEngine));
 
 fs.writeFile(
     path.join(rulesDir, '../lib/filterlist-engine.ts'),
@@ -57,9 +22,9 @@ fs.writeFile(
 declare global {
 const BUNDLE_FILTERLIST: boolean;
 }
-const serializedEngine = /* @__PURE__ */ new Uint8Array(new Uint32Array(
+const serializedEngine = /* @__PURE__ */ new Uint8Array(
   ${engineJson}
-).buffer).slice(0, ${engineLength});
+);
 const emptyEngine = /* @__PURE__ */ new Uint8Array([]);
 export default BUNDLE_FILTERLIST ? serializedEngine : emptyEngine;
 `,
