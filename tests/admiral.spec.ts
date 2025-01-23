a6b32edd42d195d2936e1aef837e3842fb6946db
Sam Macbeth <smacbeth@duckduckgo.com>
2025-01-23 11:57:23 +0100

    Admiral: clear test sites, currently non are using admiral.

--- tests/admiral.spec.ts
+++ tests/admiral.spec.ts
@@ -1,11 +1,7 @@
 import generateCMPTests from '../playwright/runner';
 
 generateCMPTests(
-    'airbnb',
-    [
-        'https://twinfinite.net/features/best-geeky-video-game-places-new-york/',
-        'https://www.androidpolice.com/proton-mail-calendar-desktop-app-launched/',
-        'https://www.xda-developers.com/apps-every-mac-user-should-be-using/',
-    ],
+    'admiral',
+    [],
     {},
 );
