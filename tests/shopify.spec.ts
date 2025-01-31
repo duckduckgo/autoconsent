53a2516724ab7c3872cb1da6b2a3a6151fdca34c
Robert Anderson <randerson@duckduckgo.com>
2025-01-31 11:48:47 +1100

    Fix tests

--- tests/shopify.spec.ts
+++ tests/shopify.spec.ts
@@ -3,5 +3,5 @@ import generateCMPTests from '../playwright/runner';
 generateCMPTests('shopify', [
     'https://store.leviton.com/collections/smart-load-center',
     'https://snaprevise.co.uk/',
-    'https://hannun.com/',
+    'https://www.blacklimba.com/',
 ]);
