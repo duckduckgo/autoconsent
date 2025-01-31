0482c1ec03a98c27edc68a5ec9ca1b2113999dae
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-01-31 10:21:57 +0100

    Improve the Civic rule (#614)

--- tests/civic-cookie-control.spec.ts
+++ tests/civic-cookie-control.spec.ts
@@ -1,5 +1,9 @@
 import generateCMPTests from '../playwright/runner';
 
-generateCMPTests('civic-cookie-control', ['https://www.birmingham.gov.uk/', 'https://planning.org.uk/', 'https://www.jessops.com/'], {
-    skipRegions: ['US', 'DE'],
-});
+generateCMPTests(
+    'civic-cookie-control',
+    ['https://www.birmingham.gov.uk/', 'https://planning.org.uk/', 'https://www.jessops.com/', 'https://www.bcs.org/'],
+    {
+        skipRegions: ['US', 'DE'],
+    },
+);
