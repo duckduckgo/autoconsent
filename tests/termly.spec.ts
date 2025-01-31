8d7f1470ffe7268ed84a737814eacf2f8e17b1bd
Maxim Tsoy <maks.tsoy@gmail.com>
2025-01-31 10:48:17 +0100

    Improve Termly rule

--- tests/termly.spec.ts
+++ tests/termly.spec.ts
@@ -1,6 +1,16 @@
 import generateCMPTests from '../playwright/runner';
 
-generateCMPTests('Termly', ['https://itsalovelylife.com/', 'https://www.iccsafe.org/'], {});
+generateCMPTests(
+    'Termly',
+    [
+        'https://itsalovelylife.com/',
+        'https://www.iccsafe.org/',
+        'https://cooper.edu/',
+        'https://www.chateauelan.com/',
+        'https://www.bentley.edu/',
+    ],
+    {},
+);
 
 generateCMPTests('Termly', ['https://visualsbyimpulse.com/'], {
     skipRegions: ['DE'],
