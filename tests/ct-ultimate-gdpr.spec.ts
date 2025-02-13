0f776db7feb1056ec3e3446f6646dc5555b14067
Robert Anderson <randerson@duckduckgo.com>
2025-02-13 14:41:02 +1100

    Add rule for ct-ultimate-gdpr CMP

---
+++ tests/ct-ultimate-gdpr.spec.ts
@@ -0,0 +1,3 @@
+import generateCMPTests from '../playwright/runner';
+
+generateCMPTests('ct-ultimate-gdpr', ['https://e-cryptonews.com/', 'https://www.enka.com/', 'https://www.agatharuizdelaprada.com/en/']);
