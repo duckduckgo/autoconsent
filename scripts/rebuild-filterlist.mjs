afaa98b7d4f2f550cd8046befb0329bb72f4c87b
Maxim Tsoy <maks.tsoy@gmail.com>
2025-02-21 17:21:18 +0100

    automate easylist rules for coverage crawl

--- scripts/rebuild-filterlist.mjs
+++ scripts/rebuild-filterlist.mjs
@@ -10,6 +10,7 @@ import { finished } from 'stream/promises';
 
 const rulesDir = path.join(path.dirname(fileURLToPath(import.meta.url)), '../rules');
 const dataDir = path.join(path.dirname(fileURLToPath(import.meta.url)), '../data');
+const libDir = path.join(path.dirname(fileURLToPath(import.meta.url)), '../lib');
 
 const easylistRevision = fs.readFileSync(path.join(rulesDir, 'filterlists', 'easylist_revision.txt'), 'utf-8');
 const domainSpecificLists = [
@@ -18,12 +19,32 @@ const domainSpecificLists = [
     'easylist_cookie_international_specific_hide.txt',
 ];
 
+/** @type {string[]} */
+const domainsWithJsRules = [];
+
+/** @type {[string, string][]} */
+const filterRules = [];
+
 const MAX_DOMAIN_RANK = 100000;
 
-/** @typedef {{ rules: Record<string, string[]>}} FilterlistJSON */
+/** @typedef {{ rules: Record<string, string[]>, domainsWithJsRules: string[] }} FilterlistJSON */
 
 /** @type {Map<string, string>} */
 let domainMap;
+
+/**
+ * @param {String} listFileName
+ */
+function parseFilterlist(listFileName) {
+    const data = fs.readFileSync(path.join(rulesDir, 'filterlists', listFileName), 'utf-8');
+    const lines = data.split('\n');
+
+    // Remove comments and network rules, which take a different format:
+    // !  at start of line indicates comment
+    // || at start of line indicates network rule
+    const filteredLines = lines.filter((line) => !line.startsWith('!') && !line.startsWith('||'));
+    return filteredLines.map((line) => line.split('##'));
+}
 
 /**
  * Processes a list containing domain-specific cosmetic rules, filters
@@ -32,24 +53,16 @@ let domainMap;
  */
 async function processDomainSpecificFilterList(listFileName) {
     console.log(`Processing ${listFileName}`);
-    const data = fs.readFileSync(path.join(rulesDir, 'filterlists', listFileName), 'utf-8');
-    const lines = data.split('\n');
+
     /** @type {FilterlistJSON} */
-    const filterlistJSON = { rules: {} };
+    const filterlistJSON = { rules: {}, domainsWithJsRules: [] };
 
-    // Remove comments and network rules, which take a different format:
-    // !  at start of line indicates comment
-    // || at start of line indicates network rule
-    const filteredLines = lines.filter((line) => !line.startsWith('!') && !line.startsWith('||'));
-    // Dump rules into json structure for parsing
-    for (const rule of filteredLines) {
-        const splitRule = rule.split('##');
-        const target = splitRule[0];
-        const action = splitRule[1];
-
+    for (const [target, action] of parseFilterlist(listFileName)) {
         if (!action) {
             continue;
         }
+
+        filterRules.push([target, action]);
 
         if (!filterlistJSON.rules[target]) {
             filterlistJSON.rules[target] = [action];
@@ -63,40 +76,51 @@ async function processDomainSpecificFilterList(listFileName) {
     for (const item in filterlistJSON.rules) {
         const containsJS = filterlistJSON.rules[item].some((filter) => filter.includes('+js'));
         if (containsJS) {
-            delete filterlistJSON.rules[item];
+            filterlistJSON.rules[item] = filterlistJSON.rules[item].filter((filter) => !filter.includes('+js'));
+            filterlistJSON.domainsWithJsRules = filterlistJSON.domainsWithJsRules.concat(item.split(','));
         }
     }
 
-    for (const item in filterlistJSON.rules) {
-        let domains;
+    // Remove any targets that contain a +js rule. we do it this way because
+    // when a target has a :js rule, we must also remove any css rules
+    // for (const item in filterlistJSON.rules) {
+    //     const containsJS = filterlistJSON.rules[item].some((filter) => filter.includes('+js'));
+    //     if (containsJS) {
+    //         delete filterlistJSON.rules[item];
+    //     }
+    // }
 
-        if (item.includes(',')) {
-            domains = item.split(',');
-        } else {
-            domains = [item];
-        }
+    // for (const item in filterlistJSON.rules) {
+    //     let domains;
 
-        const filteredDomains = domains.filter((domain) => {
-            // Don't filter global rules which don't have a domain
-            if (domain === '') {
-                return true;
-            }
+    //     if (item.includes(',')) {
+    //         domains = item.split(',');
+    //     } else {
+    //         domains = [item];
+    //     }
 
-            const tlDomain = getDomain(domain);
-            return domainMap.has(tlDomain);
-        });
+    //     const filteredDomains = domains.filter((domain) => {
+    //         // Don't filter global rules which don't have a domain
+    //         if (domain === '') {
+    //             return true;
+    //         }
 
-        // If no domains meet cutoff, remove rule entirely. If one or more
-        // domains meet cutoff, remove those that don't
-        if (filteredDomains.length === 0) {
-            delete filterlistJSON.rules[item];
-        } else if (filteredDomains.length !== domains.length) {
-            const filteredDomainString = filteredDomains.join(',');
-            filterlistJSON.rules[filteredDomainString] = filterlistJSON.rules[item];
-            delete filterlistJSON.rules[item];
-        }
-    }
+    //         const tlDomain = getDomain(domain);
+    //         return domainMap.has(tlDomain);
+    //     });
 
+    //     // If no domains meet cutoff, remove rule entirely. If one or more
+    //     // domains meet cutoff, remove those that don't
+    //     if (filteredDomains.length === 0) {
+    //         delete filterlistJSON.rules[item];
+    //     } else if (filteredDomains.length !== domains.length) {
+    //         const filteredDomainString = filteredDomains.join(',');
+    //         filterlistJSON.rules[filteredDomainString] = filterlistJSON.rules[item];
+    //         delete filterlistJSON.rules[item];
+    //     }
+    // }
+
+    domainsWithJsRules.push(...filterlistJSON.domainsWithJsRules);
     await convertAndWriteABP(filterlistJSON, listFileName);
 }
 
@@ -206,9 +230,26 @@ ${fs.readFileSync(path.join(rulesDir, 'filterlists', 'overrides.txt'), 'utf-8')}
 async function rebuildFilterList() {
     await loadTrancoList();
 
+    for (const [target, action] of parseFilterlist('easylist_cookie_general_hide.txt')) {
+        if (!action) {
+            continue;
+        }
+
+        filterRules.push([target, action]);
+    }
+
     for (const list of domainSpecificLists) {
         await processDomainSpecificFilterList(list);
     }
+
+    // Write domains with JS rules to a file
+    const jsDomainsPath = path.join(libDir, 'domains-with-js-rules.ts');
+    fs.writeFileSync(jsDomainsPath, `export const domainsWithJsRules = ${JSON.stringify(domainsWithJsRules, null, 4)};\n`, 'utf-8');
+    console.log(`Saved domains with JS rules to ${jsDomainsPath}`);
+
+    const filterRulesPath = path.join(libDir, 'easylist-filters.ts');
+    fs.writeFileSync(filterRulesPath, `export const easylistFilters = ${JSON.stringify(filterRules, null, 4)};\n`, 'utf-8');
+    console.log(`Saved filter rules to ${filterRulesPath}`);
 
     combineFilterLists();
 }
