81db5781c48522d8203d6a4941c6f42f891f93a7
Maxim Tsoy <maks.tsoy@gmail.com>
2025-02-25 15:41:49 +0100

    add rule for a common cmp

---
+++ tests/ckies.spec.ts
@@ -0,0 +1,7 @@
+import generateCMPTests from '../playwright/runner';
+
+generateCMPTests('ckies', [
+    'https://www.francescovarvello.it/',
+    'https://www.ricamo20.com/company/',
+    'https://www.erichvonwernerverlag.de/',
+]);
