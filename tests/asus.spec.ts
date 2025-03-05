03d800709763a0f26675203334f8cb75e4024350
Maxim Tsoy <maks.tsoy@gmail.com>
2025-03-05 15:44:40 +0100

    Fix asus rule

--- tests/asus.spec.ts
+++ tests/asus.spec.ts
@@ -1,3 +1,3 @@
 import generateCMPTests from '../playwright/runner';
 
-generateCMPTests('asus', ['https://www.asus.com/us/', 'https://www.asus.com/']);
+generateCMPTests('asus', ['https://www.asus.com/us/', 'https://www.asus.com/', 'https://rog.asus.com/']);
