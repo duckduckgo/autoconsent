b88c8c81d001aebc2eeecbfe34df702d7d19c4ba
Maxim Tsoy <maks.tsoy@gmail.com>
2025-05-15 16:12:04 +0200

    Support iframe piercing and update the FastCMP rule

--- tests/fastcmp.spec.ts
+++ tests/fastcmp.spec.ts
@@ -1,3 +1,3 @@
 import generateCMPTests from '../playwright/runner';
 
-generateCMPTests('fastcmp', ['https://corporate.fastcmp.com/', 'https://www.nigella.com/', 'https://www.lse.co.uk/']);
+generateCMPTests('fastcmp', ['https://corporate.fastcmp.com/', 'https://www.nigella.com/']);
