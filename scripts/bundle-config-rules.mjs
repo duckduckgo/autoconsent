ee46d75af06df26190bf5a6dc80b0c656a9c8b5b
Sam Macbeth <smacbeth@duckduckgo.com>
2025-05-21 12:01:46 +0200

    Only write rules to the autoconsent feature file.

--- scripts/bundle-config-rules.mjs
+++ scripts/bundle-config-rules.mjs
@@ -10,8 +10,7 @@ if (process.argv.length !== 3) {
 const privacyConfigPath = process.argv[2];
 
 const bundleLocations = [
-    // join(privacyConfigPath, 'features', 'autoconsent.json'),
-    join(privacyConfigPath, 'overrides', 'macos-override.json'),
+    join(privacyConfigPath, 'features', 'autoconsent.json'),
 ];
 
 const compactRules = JSON.parse(fs.readFileSync(join(dirname(fileURLToPath(import.meta.url)), '../rules/compact-rules.json'), 'utf-8'));
