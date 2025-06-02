3ea20371b0dca6603ec0ee956de1a6e4c468dd58
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-06-02 08:01:02 -0400

    Add autoconsent rules for https://yandex.com/

---
+++ tests/auto_GB_yandex.com_0.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_GB_yandex.com_0', ["https://yandex.com/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["GB"]});
