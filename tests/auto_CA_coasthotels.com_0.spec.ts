e31582229289337b5b13d60966f71040b7df976d
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 16:10:17 +0200

    Add autoconsent rules for https://www.coasthotels.com/

---
+++ tests/auto_CA_coasthotels.com_0.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_CA_coasthotels.com_0', ["https://www.coasthotels.com/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["CA"]});
