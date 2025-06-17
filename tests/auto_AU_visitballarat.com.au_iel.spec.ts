3b4554560949a5ea73d431f6fd6ff96b69a49018
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 15:10:53 -0400

    Add autoconsent rules for https://www.visitballarat.com.au/

---
+++ tests/auto_AU_visitballarat.com.au_iel.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_AU_visitballarat.com.au_iel', ["https://www.visitballarat.com.au/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["AU"]});
