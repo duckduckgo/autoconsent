1b246b43fb4b91d538914826fcdee2cef4233eae
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 16:10:17 +0200

    Add autoconsent rules for https://www.bfmtv.com/

---
+++ tests/auto_CA_bfmtv.com_0.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_CA_bfmtv.com_0', ["https://www.bfmtv.com/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["CA"]});
