ca30d73be2389b39d9076dd629cf95071e11999a
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 16:10:18 +0200

    Add autoconsent rules for https://help.dropbox.com/

--- tests/auto_US_help.dropbox.com_0.spec.ts
+++ tests/auto_US_help.dropbox.com_0.spec.ts
@@ -1,3 +1,2 @@
-import generateCMPTests from '../playwright/runner';
-
-generateCMPTests('auto_US_help.dropbox.com_0', ['https://help.dropbox.com/']);
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_US_help.dropbox.com_0', ["https://help.dropbox.com/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["US"]});
