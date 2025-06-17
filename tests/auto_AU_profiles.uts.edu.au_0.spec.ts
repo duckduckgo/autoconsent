6574533c8505729642f67ee83d6579e9946a27a0
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 16:10:17 +0200

    Add autoconsent rules for https://profiles.uts.edu.au/

---
+++ tests/auto_AU_profiles.uts.edu.au_0.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_AU_profiles.uts.edu.au_0', ["https://profiles.uts.edu.au/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["AU"]});
