3edf6d6d10695c17b66e68db08537d89d2476962
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 16:10:17 +0200

    Add autoconsent rules for https://www.karenmillen.com/

---
+++ tests/auto_AU_karenmillen.com_0.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_AU_karenmillen.com_0', ["https://www.karenmillen.com/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["AU"]});
