bce3e4aed10d31e5bedaef6ce34940c76f2ff732
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 16:10:17 +0200

    Add autoconsent rules for https://wikifeetx.com/

---
+++ tests/auto_AU_wikifeetx.com_0.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_AU_wikifeetx.com_0', ["https://wikifeetx.com/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["AU"]});
