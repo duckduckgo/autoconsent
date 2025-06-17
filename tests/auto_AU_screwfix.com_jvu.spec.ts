e3ff917631ec4ac5de59d3ce5e5e78f066cda614
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 18:28:29 -0400

    Add autoconsent rules for https://www.screwfix.com/

---
+++ tests/auto_AU_screwfix.com_jvu.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_AU_screwfix.com_jvu', ["https://www.screwfix.com/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["AU"]});
