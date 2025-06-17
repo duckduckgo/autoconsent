6413e3ac8cabb2af2c92a29c930f6a8c2164d934
Maxim Tsoy <maks.tsoy@gmail.com>
2025-06-17 23:38:04 +0200

    Remove duplicate dropbox rules

--- tests/auto_AU_dropbox.com_4.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../playwright/runner';
-generateCMPTests('auto_AU_dropbox.com_4', ['https://www.dropbox.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['AU'] });
--- tests/auto_CA_help.dropbox.com_0.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../playwright/runner';
-generateCMPTests('auto_CA_help.dropbox.com_0', ['https://help.dropbox.com/'], {
-    testOptIn: false,
-    testSelfTest: false,
-    onlyRegions: ['CA'],
-});
