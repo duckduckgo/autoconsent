331f95300d1d5ee9724317bffa9043d07fdf1dba
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 18:28:30 -0400

    Add autoconsent rules for https://www.tsx.com/

---
+++ tests/auto_CA_tsx.com_lep.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_CA_tsx.com_lep', ["https://www.tsx.com/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["CA"]});
