5a3be75e025db432b719fe90f3899e886ad78592
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 18:28:30 -0400

    Add autoconsent rules for https://www.theguardian.com/uk

---
+++ tests/auto_GB_theguardian.com_z7g.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_GB_theguardian.com_z7g', ["https://www.theguardian.com/uk"], {testOptIn: false, testSelfTest: false, onlyRegions: ["GB"]});
