0a84cc7671ee9b5dde097f3e37a5b2eb955fd048
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 18:28:27 -0400

    Add autoconsent rules for https://evernote.com/

---
+++ tests/auto_AU_evernote.com_xha.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_AU_evernote.com_xha', ["https://evernote.com/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["AU"]});
