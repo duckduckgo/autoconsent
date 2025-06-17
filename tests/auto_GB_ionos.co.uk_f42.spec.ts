4ceed2ccddad9d6398fc0efc097de0c305785a03
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 18:28:29 -0400

    Add autoconsent rules for https://www.ionos.co.uk/

---
+++ tests/auto_GB_ionos.co.uk_f42.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_GB_ionos.co.uk_f42', ["https://www.ionos.co.uk/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["GB"]});
