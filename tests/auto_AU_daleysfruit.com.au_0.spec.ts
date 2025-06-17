2a7253f10a1d0772f4ad0efa1739506f9add49de
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 16:10:17 +0200

    Add autoconsent rules for https://www.daleysfruit.com.au/

---
+++ tests/auto_AU_daleysfruit.com.au_0.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_AU_daleysfruit.com.au_0', ["https://www.daleysfruit.com.au/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["AU"]});
