3cc770fe807c91453777d1fa78c40a869cef0b8b
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 16:10:17 +0200

    Add autoconsent rules for https://forums.frontier.co.uk/

---
+++ tests/auto_AU_forums.frontier.co.uk_0.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_AU_forums.frontier.co.uk_0', ["https://forums.frontier.co.uk/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["AU"]});
