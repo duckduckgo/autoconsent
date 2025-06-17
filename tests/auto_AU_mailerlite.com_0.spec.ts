1b787493712f26935dc2c3935c87ac2ac4337ceb
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 16:10:17 +0200

    Add autoconsent rules for https://www.mailerlite.com/

---
+++ tests/auto_AU_mailerlite.com_0.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_AU_mailerlite.com_0', ["https://www.mailerlite.com/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["AU"]});
