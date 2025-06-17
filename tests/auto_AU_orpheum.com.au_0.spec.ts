406944f9e6111f203bbb78c2e4eef4a2fc0e1005
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 16:10:17 +0200

    Add autoconsent rules for https://www.orpheum.com.au/

---
+++ tests/auto_AU_orpheum.com.au_0.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_AU_orpheum.com.au_0', ["https://www.orpheum.com.au/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["AU"]});
