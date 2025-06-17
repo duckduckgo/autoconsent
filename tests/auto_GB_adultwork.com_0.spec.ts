30a2c3ee2ec75be7867ded73a96f9e03a1b8863d
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 16:10:18 +0200

    Add autoconsent rules for https://www.adultwork.com/

---
+++ tests/auto_GB_adultwork.com_0.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_GB_adultwork.com_0', ["https://adultwork.com/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["GB"]});
