313b5e601f9e554985dca706e04054c8fc6ee49b
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 16:10:17 +0200

    Add autoconsent rules for $'https://redirection.ubisoft.com/site/entrance?entrance=www.ubisoft.com/'

---
+++ tests/auto_CA_redirection.ubisoft.com_0.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_CA_redirection.ubisoft.com_0', ["https://redirection.ubisoft.com/site/entrance?entrance=www.ubisoft.com/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["CA"]});
