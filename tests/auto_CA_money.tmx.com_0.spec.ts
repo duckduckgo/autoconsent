b1b7fc7472e7f9013e899507fc1a03981b8824c4
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 16:10:17 +0200

    Add autoconsent rules for https://money.tmx.com/

---
+++ tests/auto_CA_money.tmx.com_0.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_CA_money.tmx.com_0', ["https://money.tmx.com/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["CA"]});
