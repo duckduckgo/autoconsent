c0f5297e0c1c67b795e4e69140c7c47d77aa0cc8
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 16:10:18 +0200

    Add autoconsent rules for https://parliamentlive.tv/Commons

---
+++ tests/auto_GB_parliamentlive.tv_0.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_GB_parliamentlive.tv_0', ["https://parliamentlive.tv/Commons"], {testOptIn: false, testSelfTest: false, onlyRegions: ["GB"]});
