538c92730f44a230f0f6f61aa8fdd440747c3274
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 16:10:18 +0200

    Add autoconsent rules for https://www.creativebloq.com/

---
+++ tests/auto_GB_creativebloq.com_0.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_GB_creativebloq.com_0', ["https://www.creativebloq.com/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["GB"]});
