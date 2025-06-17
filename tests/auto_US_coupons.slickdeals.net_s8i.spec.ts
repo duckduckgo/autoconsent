a58d25309feb2b2256d1ae67c6caa5e78f3492de
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 15:10:56 -0400

    Add autoconsent rules for https://coupons.slickdeals.net/

---
+++ tests/auto_US_coupons.slickdeals.net_s8i.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_US_coupons.slickdeals.net_s8i', ["https://coupons.slickdeals.net/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["US"]});
