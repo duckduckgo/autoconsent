20e5e861b22ca4eb65ce2aef455871b2b27b5620
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 16:10:17 +0200

    Add autoconsent rules for https://www.cmpa-acpm.ca/en/home

---
+++ tests/auto_CA_cmpa-acpm.ca_0.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_CA_cmpa-acpm.ca_0', ["https://www.cmpa-acpm.ca/en/home"], {testOptIn: false, testSelfTest: false, onlyRegions: ["CA"]});
