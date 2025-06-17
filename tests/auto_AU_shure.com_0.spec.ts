e51c75e0f757928fb9b4ebe9ecfb0a275713c013
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 16:10:17 +0200

    Add autoconsent rules for https://www.shure.com/en-ASIA

---
+++ tests/auto_AU_shure.com_0.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_AU_shure.com_0', ["https://www.shure.com/en-ASIA"], {testOptIn: false, testSelfTest: false, onlyRegions: ["AU"]});
