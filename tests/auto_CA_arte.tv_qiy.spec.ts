f095903aec10f3f1716134fbe98bb9681d1fc776
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 18:28:28 -0400

    Add autoconsent rules for https://www.arte.tv/en/

---
+++ tests/auto_CA_arte.tv_qiy.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_CA_arte.tv_qiy', ["https://www.arte.tv/en/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["CA"]});
