bed86b561c770bb16736d08108944b9cdfe83c21
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 15:10:54 -0400

    Add autoconsent rules for https://www.bet365.com.au/

---
+++ tests/auto_AU_bet365.com.au_9zj.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_AU_bet365.com.au_9zj', ["https://www.bet365.com.au/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["AU"]});
