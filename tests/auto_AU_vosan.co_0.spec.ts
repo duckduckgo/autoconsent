a3823ab8b9d7b7d47510446a663108e473292c71
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 16:10:17 +0200

    Add autoconsent rules for https://www.vosan.co/

---
+++ tests/auto_AU_vosan.co_0.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_AU_vosan.co_0', ["https://www.vosan.co/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["AU"]});
