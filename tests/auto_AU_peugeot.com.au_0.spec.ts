5a74d45f8677815db40bb5b7dc2a7881ff6d0597
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 16:10:17 +0200

    Add autoconsent rules for https://www.peugeot.com.au/

---
+++ tests/auto_AU_peugeot.com.au_0.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_AU_peugeot.com.au_0', ["https://www.peugeot.com.au/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["AU"]});
