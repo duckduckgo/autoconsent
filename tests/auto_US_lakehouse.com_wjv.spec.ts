e6c17c6f6c659a9aa1fec03287d63dd2d9f106b2
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 15:10:56 -0400

    Add autoconsent rules for https://www.lakehouse.com/

---
+++ tests/auto_US_lakehouse.com_wjv.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_US_lakehouse.com_wjv', ["https://www.lakehouse.com/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["US"]});
