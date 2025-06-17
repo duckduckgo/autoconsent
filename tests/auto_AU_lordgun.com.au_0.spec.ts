7448165637a8dce08dd91d06f4b57b9236fe4781
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 16:10:17 +0200

    Add autoconsent rules for https://www.lordgun.com.au/

---
+++ tests/auto_AU_lordgun.com.au_0.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_AU_lordgun.com.au_0', ["https://www.lordgun.com.au/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["AU"]});
