a4e53425c24c7ba848eeee4e35cf168e51b72321
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 15:10:55 -0400

    Add autoconsent rules for https://arubanetworking.hpe.com/

---
+++ tests/auto_CA_arubanetworking.hpe.com_15g.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_CA_arubanetworking.hpe.com_15g', ["https://arubanetworking.hpe.com/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["CA"]});
