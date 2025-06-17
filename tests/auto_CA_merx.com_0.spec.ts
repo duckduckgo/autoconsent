52cd7843250317dae96acb467c63b856f4203d77
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 16:10:17 +0200

    Add autoconsent rules for https://www.merx.com/

---
+++ tests/auto_CA_merx.com_0.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_CA_merx.com_0', ["https://www.merx.com/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["CA"]});
