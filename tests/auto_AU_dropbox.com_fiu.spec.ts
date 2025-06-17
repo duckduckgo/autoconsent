a1dfcaba39aa41e00f340f299f8e12909874f449
Dax the Deployer <dax@duckduckgo.com>
2025-06-17 18:28:28 -0400

    Add autoconsent rules for https://www.dropbox.com/

---
+++ tests/auto_AU_dropbox.com_fiu.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from "../playwright/runner";
+generateCMPTests('auto_AU_dropbox.com_fiu', ["https://www.dropbox.com/"], {testOptIn: false, testSelfTest: false, onlyRegions: ["AU"]});
