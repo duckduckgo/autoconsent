02d7497cf24c1d0e954edb5fe7508f8a40a9239f
Dax the Deployer <dax@duckduckgo.com>
2025-06-19 18:56:42 -0400

    Add autoconsent rules for https://www.lighting.philips.com.au/

---
+++ tests/auto_AU_lighting.philips.com.au_134.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../playwright/runner';
+generateCMPTests('auto_AU_lighting.philips.com.au_134', ['https://www.lighting.philips.com.au/'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['AU'],
+});
