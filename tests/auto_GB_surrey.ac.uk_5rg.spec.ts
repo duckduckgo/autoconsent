3843f630ecbb6e592ab7267ebb730a1ebb37f8d1
Dax the Deployer <dax@duckduckgo.com>
2025-06-19 07:46:24 -0400

    Add autoconsent rules for https://www.surrey.ac.uk/

---
+++ tests/auto_GB_surrey.ac.uk_5rg.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../playwright/runner';
+generateCMPTests('auto_GB_surrey.ac.uk_5rg', ['https://www.surrey.ac.uk/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
