4d826401a24bcb79e49b76877633d83ae570e668
Dax the Deployer <dax@duckduckgo.com>
2025-06-19 18:56:38 -0400

    Add autoconsent rules for https://docs.oracle.com/en/

---
+++ tests/auto_GB_docs.oracle.com_a4m.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../playwright/runner';
+generateCMPTests('auto_GB_docs.oracle.com_a4m', ['https://docs.oracle.com/en/'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['GB'],
+});
