a066de8fdb0b99da5920a02a39719b8c581c9a72
Dax the Deployer <dax@duckduckgo.com>
2025-06-19 18:56:41 -0400

    Add autoconsent rules for https://www.everysaving.co.uk/

---
+++ tests/auto_GB_everysaving.co.uk_y0n.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../playwright/runner';
+generateCMPTests('auto_GB_everysaving.co.uk_y0n', ['https://www.everysaving.co.uk/'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['GB'],
+});
