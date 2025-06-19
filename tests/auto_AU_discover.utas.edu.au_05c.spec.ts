87e9e0cc8c716f807bf8b5f603087d4b69d8f5b3
Dax the Deployer <dax@duckduckgo.com>
2025-06-19 18:56:38 -0400

    Add autoconsent rules for https://discover.utas.edu.au/

---
+++ tests/auto_AU_discover.utas.edu.au_05c.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../playwright/runner';
+generateCMPTests('auto_AU_discover.utas.edu.au_05c', ['https://discover.utas.edu.au/'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['AU'],
+});
