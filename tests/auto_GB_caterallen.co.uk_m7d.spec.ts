01aed1641ad491f23b31a9ecf2475eb059a36718
Dax the Deployer <dax@duckduckgo.com>
2025-06-19 07:46:22 -0400

    Add autoconsent rules for https://www.caterallen.co.uk/

---
+++ tests/auto_GB_caterallen.co.uk_m7d.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../playwright/runner';
+generateCMPTests('auto_GB_caterallen.co.uk_m7d', ['https://www.caterallen.co.uk/'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['GB'],
+});
