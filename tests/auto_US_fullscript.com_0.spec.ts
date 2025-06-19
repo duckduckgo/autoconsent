083e78214a4b98cc964ffba6cfc12e95e5e82a55
Dax the Deployer <dax@duckduckgo.com>
2025-06-19 07:46:21 -0400

    Add autoconsent rules for https://fullscript.com/

--- tests/auto_US_fullscript.com_0.spec.ts
+++ tests/auto_US_fullscript.com_0.spec.ts
@@ -1,3 +1,2 @@
 import generateCMPTests from '../playwright/runner';
-
-generateCMPTests('auto_US_fullscript.com_0', ['https://fullscript.com/']);
+generateCMPTests('auto_US_fullscript.com_0', ['https://fullscript.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['US'] });
