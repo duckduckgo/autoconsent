b4dc52ebb4e559adf3000a3a317ec7702fbdcabd
Dax the Deployer <dax@duckduckgo.com>
2025-06-19 18:56:44 -0400

    Add autoconsent rules for https://www.theguardian.com/theobserverarchive

---
+++ tests/auto_GB_theguardian.com_3ph.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../playwright/runner';
+generateCMPTests('auto_GB_theguardian.com_3ph', ['https://www.theguardian.com/theobserverarchive'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['GB'],
+});
