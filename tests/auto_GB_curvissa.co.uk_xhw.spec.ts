79f991718a98a951cd74add4652a3f39d98a55d3
Dax the Deployer <dax@duckduckgo.com>
2025-06-19 18:56:41 -0400

    Add autoconsent rules for https://www.curvissa.co.uk/

---
+++ tests/auto_GB_curvissa.co.uk_xhw.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../playwright/runner';
+generateCMPTests('auto_GB_curvissa.co.uk_xhw', ['https://www.curvissa.co.uk/'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['GB'],
+});
