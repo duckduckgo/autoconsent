4e3da519526deabf8575b25e163552a1a105f393
Dax the Deployer <dax@duckduckgo.com>
2025-06-19 18:56:40 -0400

    Add autoconsent rules for https://www.buyspares.co.uk/

---
+++ tests/auto_GB_buyspares.co.uk_dc4.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../playwright/runner';
+generateCMPTests('auto_GB_buyspares.co.uk_dc4', ['https://www.buyspares.co.uk/'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['GB'],
+});
