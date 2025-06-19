8007790a717b776d9851527d6b861ab70bd2287e
Dax the Deployer <dax@duckduckgo.com>
2025-06-19 18:56:43 -0400

    Add autoconsent rules for https://www.salomon.com/en-gb

---
+++ tests/auto_GB_salomon.com_u2a.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../playwright/runner';
+generateCMPTests('auto_GB_salomon.com_u2a', ['https://www.salomon.com/en-gb'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['GB'],
+});
