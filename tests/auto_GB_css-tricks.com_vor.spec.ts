faca042b0fbdee3192d6a7d94c68a80d8738a474
Dax the Deployer <dax@duckduckgo.com>
2025-06-19 18:56:38 -0400

    Add autoconsent rules for https://css-tricks.com/

---
+++ tests/auto_GB_css-tricks.com_vor.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../playwright/runner';
+generateCMPTests('auto_GB_css-tricks.com_vor', ['https://css-tricks.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
