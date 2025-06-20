d3e905f871c6006a5e954d1e56a425d4ec518328
Dax the Deployer <dax@duckduckgo.com>
2025-06-20 04:50:30 -0400

    Add autoconsent rules for https://reolink.com/

---
+++ tests/auto_GB_reolink.com_tw6.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../playwright/runner';
+generateCMPTests('auto_GB_reolink.com_tw6', ['https://reolink.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
