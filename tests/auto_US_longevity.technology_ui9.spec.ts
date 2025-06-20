084effeb09d31c09927b32ce633593178750f5eb
Dax the Deployer <dax@duckduckgo.com>
2025-06-20 04:50:29 -0400

    Add autoconsent rules for https://longevity.technology/

---
+++ tests/auto_US_longevity.technology_ui9.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../playwright/runner';
+generateCMPTests('auto_US_longevity.technology_ui9', ['https://longevity.technology/'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['US'],
+});
