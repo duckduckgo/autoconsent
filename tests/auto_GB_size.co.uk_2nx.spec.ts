3b9865cc6d63300e1c69a4f0e69845918072ad6a
Dax the Deployer <dax@duckduckgo.com>
2025-06-20 04:50:34 -0400

    Add autoconsent rules for https://www.size.co.uk/

---
+++ tests/auto_GB_size.co.uk_2nx.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../playwright/runner';
+generateCMPTests('auto_GB_size.co.uk_2nx', ['https://www.size.co.uk/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
