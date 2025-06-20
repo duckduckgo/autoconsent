baf3f868d54ed9c61bf5c0bb9b118f7d6481a8f0
Dax the Deployer <dax@duckduckgo.com>
2025-06-20 04:50:28 -0400

    Add autoconsent rules for http://www.lawyer.com/

---
+++ tests/auto_CA_lawyer.com_glx.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../playwright/runner';
+generateCMPTests('auto_CA_lawyer.com_glx', ['http://www.lawyer.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['CA'] });
