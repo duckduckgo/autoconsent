5a43d3da0c8b10ffccd215a2d330b79af5ba9a2e
Dax the Deployer <dax@duckduckgo.com>
2025-06-20 04:50:33 -0400

    Add autoconsent rules for https://www.parchment.com/

---
+++ tests/auto_US_parchment.com_7r7.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../playwright/runner';
+generateCMPTests('auto_US_parchment.com_7r7', ['https://www.parchment.com/'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['US'],
+});
