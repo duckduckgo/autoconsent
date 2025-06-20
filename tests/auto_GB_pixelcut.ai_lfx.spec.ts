30ab381291ada477d56df753fbac9a44027f68a8
Dax the Deployer <dax@duckduckgo.com>
2025-06-20 04:50:34 -0400

    Add autoconsent rules for https://www.pixelcut.ai/

---
+++ tests/auto_GB_pixelcut.ai_lfx.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../playwright/runner';
+generateCMPTests('auto_GB_pixelcut.ai_lfx', ['https://www.pixelcut.ai/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
