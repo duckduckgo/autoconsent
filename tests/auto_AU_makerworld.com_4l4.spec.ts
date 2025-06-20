17499a7d4270cf9e0246667ab3ae9f101510fd9f
Dax the Deployer <dax@duckduckgo.com>
2025-06-20 04:50:29 -0400

    Add autoconsent rules for https://makerworld.com/en

---
+++ tests/auto_AU_makerworld.com_4l4.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../playwright/runner';
+generateCMPTests('auto_AU_makerworld.com_4l4', ['https://makerworld.com/en'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['AU'],
+});
