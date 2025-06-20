8cf20d9728d1504c4e76522cadabb386a14f5a9f
Dax the Deployer <dax@duckduckgo.com>
2025-06-20 04:50:30 -0400

    Add autoconsent rules for https://students.flinders.edu.au/

---
+++ tests/auto_AU_students.flinders.edu.au_eh4.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../playwright/runner';
+generateCMPTests('auto_AU_students.flinders.edu.au_eh4', ['https://students.flinders.edu.au/'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['AU'],
+});
