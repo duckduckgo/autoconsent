ef7a7ba76c8e3f6dd9f9639427e9c297a2191d5e
Dax the Deployer <dax@duckduckgo.com>
2025-06-20 04:50:35 -0400

    Add autoconsent rules for https://www.thorlabs.com/

---
+++ tests/auto_GB_thorlabs.com_87c.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../playwright/runner';
+generateCMPTests('auto_GB_thorlabs.com_87c', ['https://www.thorlabs.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
