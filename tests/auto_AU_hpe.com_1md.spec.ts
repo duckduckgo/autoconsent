41e55c73073d6496f774d3e5d56835114c425ad5
Dax the Deployer <dax@duckduckgo.com>
2025-06-20 04:50:32 -0400

    Add autoconsent rules for https://www.hpe.com/us/en/home.html

---
+++ tests/auto_AU_hpe.com_1md.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../playwright/runner';
+generateCMPTests('auto_AU_hpe.com_1md', ['https://www.hpe.com/us/en/home.html'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['AU'],
+});
