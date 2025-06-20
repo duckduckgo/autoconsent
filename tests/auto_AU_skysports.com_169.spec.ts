857c9078f1c1db64e9c674151a3e388caa72693e
Dax the Deployer <dax@duckduckgo.com>
2025-06-20 04:50:34 -0400

    Add autoconsent rules for https://www.skysports.com/

---
+++ tests/auto_AU_skysports.com_169.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../playwright/runner';
+generateCMPTests('auto_AU_skysports.com_169', ['https://www.skysports.com/'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['AU'],
+});
