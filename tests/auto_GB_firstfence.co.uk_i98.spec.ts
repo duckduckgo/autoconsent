dc0dbc6f9dae54af9673b21dd77ead5d8878463f
Dax the Deployer <dax@duckduckgo.com>
2025-06-20 04:50:29 -0400

    Add autoconsent rules for https://firstfence.co.uk/

---
+++ tests/auto_GB_firstfence.co.uk_i98.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../playwright/runner';
+generateCMPTests('auto_GB_firstfence.co.uk_i98', ['https://firstfence.co.uk/'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['GB'],
+});
