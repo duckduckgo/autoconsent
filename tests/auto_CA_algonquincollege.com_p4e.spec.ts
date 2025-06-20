8aacd9a48deaaf45d4ecae43503b534ba60b0101
Dax the Deployer <dax@duckduckgo.com>
2025-06-20 04:50:30 -0400

    Add autoconsent rules for https://www.algonquincollege.com/

---
+++ tests/auto_CA_algonquincollege.com_p4e.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../playwright/runner';
+generateCMPTests('auto_CA_algonquincollege.com_p4e', ['https://www.algonquincollege.com/'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['CA'],
+});
