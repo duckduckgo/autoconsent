f17bae8b09289e38f9d54d8744dc0234e2d0ac3e
Dax the Deployer <dax@duckduckgo.com>
2025-06-20 04:50:33 -0400

    Add autoconsent rules for https://www.peekyou.com/

---
+++ tests/auto_US_peekyou.com_32h.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../playwright/runner';
+generateCMPTests('auto_US_peekyou.com_32h', ['https://www.peekyou.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['US'] });
