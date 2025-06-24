50894619817aa01cdc0f606ff9ca4a0986f140e0
Maxim Tsoy <maks.tsoy@gmail.com>
2025-06-24 08:33:15 +0200

    Remove a rule clicking a paywall

--- tests/auto_GB_marca.com_iyp.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../playwright/runner';
-generateCMPTests('auto_GB_marca.com_iyp', ['https://www.marca.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
