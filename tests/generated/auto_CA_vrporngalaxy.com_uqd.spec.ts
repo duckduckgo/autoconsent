306b1a686c48b9e9c289dc92d81700d4177f31b4
Dax the Deployer <dax@duckduckgo.com>
2025-06-30 07:02:38 -0400

    Add autoconsent rules for https://www.vrporngalaxy.com/

---
+++ tests/generated/auto_CA_vrporngalaxy.com_uqd.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_CA_vrporngalaxy.com_uqd', ['https://www.vrporngalaxy.com/'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['CA'],
+});
