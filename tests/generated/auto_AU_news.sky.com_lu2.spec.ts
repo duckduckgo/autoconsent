c222eac8bd9657b7e3d98ceff7d3aa5387a733a1
Dax the Deployer <dax@duckduckgo.com>
2025-06-30 07:02:34 -0400

    Add autoconsent rules for https://news.sky.com/

---
+++ tests/generated/auto_AU_news.sky.com_lu2.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_AU_news.sky.com_lu2', ['https://news.sky.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['AU'] });
