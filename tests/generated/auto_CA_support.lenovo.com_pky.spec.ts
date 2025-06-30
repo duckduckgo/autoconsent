d0fac5c973d8cea6ee751ecfcc8caeb4bf110648
Maxim Tsoy <maks.tsoy@gmail.com>
2025-06-30 18:31:04 +0200

    Remove duplicate lenovo rules

--- tests/generated/auto_CA_support.lenovo.com_pky.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CA_support.lenovo.com_pky', ['https://support.lenovo.com/ca/en/'], {
-    testOptIn: false,
-    testSelfTest: false,
-    onlyRegions: ['CA'],
-});
--- tests/generated/auto_GB_support.lenovo.com_707.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_support.lenovo.com_707', ['https://support.lenovo.com/gb/en/'], {
-    testOptIn: false,
-    testSelfTest: false,
-    onlyRegions: ['GB'],
-});
