65ad194d395b7f458fda980c80dc0bea6a2fc0cf
Maxim Tsoy <maks.tsoy@gmail.com>
2025-06-30 19:07:11 +0200

    Fix cookieyes rule

--- tests/generated/auto_GB_whatprice.co.uk_vey.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_whatprice.co.uk_vey', ['https://www.whatprice.co.uk/'], {
-    testOptIn: false,
-    testSelfTest: false,
-    onlyRegions: ['GB'],
-});
