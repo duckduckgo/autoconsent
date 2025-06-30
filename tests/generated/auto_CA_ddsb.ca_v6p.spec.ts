dad8d996ee83a0a56c23a7ffbbb949f3e55dd1c0
Maxim Tsoy <maks.tsoy@gmail.com>
2025-06-30 18:02:26 +0200

    Fix the ultimate-gdpr rule

--- tests/generated/auto_CA_ddsb.ca_v6p.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CA_ddsb.ca_v6p', ['https://www.ddsb.ca/en/index.aspx'], {
-    testOptIn: false,
-    testSelfTest: false,
-    onlyRegions: ['CA'],
-});
