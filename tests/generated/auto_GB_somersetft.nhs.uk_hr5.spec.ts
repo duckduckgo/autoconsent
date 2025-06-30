1f13bff2a9043eb499e196b8d4a8416074fc8234
Dax the Deployer <dax@duckduckgo.com>
2025-06-30 07:02:37 -0400

    Add autoconsent rules for https://www.somersetft.nhs.uk/

---
+++ tests/generated/auto_GB_somersetft.nhs.uk_hr5.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_GB_somersetft.nhs.uk_hr5', ['https://www.somersetft.nhs.uk/'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['GB'],
+});
