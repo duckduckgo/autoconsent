4423f9668c543dfa246e2f5f30d69fc812d873a4
Dax the Deployer <dax@duckduckgo.com>
2025-06-30 07:02:36 -0400

    Add autoconsent rules for https://www.keepersecurity.com/

---
+++ tests/generated/auto_CA_keepersecurity.com_npf.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_CA_keepersecurity.com_npf', ['https://www.keepersecurity.com/'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['CA'],
+});
