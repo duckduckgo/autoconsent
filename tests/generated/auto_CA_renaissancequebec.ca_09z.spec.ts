3c4ae7e73c8437b260c7ac1f5a2bed910635d4c8
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-07-02 09:12:06 +0200

    Fix rule issues found in the last crawl (#808)
    * Fix the ultimate-gdpr rule
    
    * Remove outdated ultimate-gdpr test
    
    * Fix cookieyes rule
    
    * Remove more rules handled by cookieyes

--- tests/generated/auto_CA_renaissancequebec.ca_09z.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CA_renaissancequebec.ca_09z', ['https://renaissancequebec.ca/en/'], {
-    testOptIn: false,
-    testSelfTest: false,
-    onlyRegions: ['CA'],
-});
--- tests/generated/auto_CA_tsx.com_g0i.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CA_tsx.com_g0i', ['https://www.tsx.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['CA'] });
