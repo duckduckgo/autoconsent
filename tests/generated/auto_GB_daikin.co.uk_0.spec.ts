4bab98d2fabf844973ec1abf1e16c2b9b2af9062
Dax the Deployer <dax@duckduckgo.com>
2025-07-09 21:44:00 -0400

    Add autoconsent rules for https://www.daikin.co.uk/en_gb/residential.html

---
+++ tests/generated/auto_GB_daikin.co.uk_0.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_GB_daikin.co.uk_0', ['https://www.daikin.co.uk/en_gb/residential.html'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['GB'],
+});
