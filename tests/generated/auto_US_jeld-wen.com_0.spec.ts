b89e739c7e4ea989cf537c65e8e97e9ef1ec1430
Dax the Deployer <dax@duckduckgo.com>
2025-07-10 17:31:38 -0400

    Add autoconsent rules for https://www.jeld-wen.com/en-us

--- tests/generated/auto_US_jeld-wen.com_0.spec.ts
+++ tests/generated/auto_US_jeld-wen.com_0.spec.ts
@@ -1,3 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
-
-generateCMPTests('auto_US_jeld-wen.com_0', ['https://www.jeld-wen.com/en-us']);
+generateCMPTests('auto_US_jeld-wen.com_0', ['https://www.jeld-wen.com/en-us'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['US'],
+});
