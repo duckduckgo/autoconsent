05fd12d47768f1dd60b45a3244ef1f6bea770f62
Dax the Deployer <dax@duckduckgo.com>
2025-07-11 09:21:41 -0400

    Add autoconsent rules for https://www.tackledirect.com/

--- tests/generated/auto_US_tackledirect.com_0.spec.ts
+++ tests/generated/auto_US_tackledirect.com_0.spec.ts
@@ -1,3 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
-
-generateCMPTests('auto_US_tackledirect.com_0', ['https://www.tackledirect.com/']);
+generateCMPTests('auto_US_tackledirect.com_0', ['https://www.tackledirect.com/'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['US'],
+});
