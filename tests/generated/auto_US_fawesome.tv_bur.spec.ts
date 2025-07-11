197aa1555d9d4f6a2e7ac84672d614e88e1ce725
Dax the Deployer <dax@duckduckgo.com>
2025-07-11 09:21:39 -0400

    Add autoconsent rules for https://fawesome.tv/

---
+++ tests/generated/auto_US_fawesome.tv_bur.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_US_fawesome.tv_bur', ['https://fawesome.tv/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['US'] });
