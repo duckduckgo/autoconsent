bfffadabbc2d5c0d57e9bdf7a64fe2ad90ffee93
Dax the Deployer <dax@duckduckgo.com>
2025-07-11 09:21:41 -0400

    Add autoconsent rules for https://www.thingiverse.com/

--- tests/generated/auto_US_thingiverse.com_0.spec.ts
+++ tests/generated/auto_US_thingiverse.com_0.spec.ts
@@ -1,3 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
-
-generateCMPTests('auto_US_thingiverse.com_0', ['https://www.thingiverse.com/']);
+generateCMPTests('auto_US_thingiverse.com_0', ['https://www.thingiverse.com/'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['US'],
+});
