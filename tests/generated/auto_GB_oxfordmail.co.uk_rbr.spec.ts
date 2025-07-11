6fec5edc483d8c34cff2f9e1430a6dcc8f0345d6
Dax the Deployer <dax@duckduckgo.com>
2025-07-11 19:25:03 -0400

    Add autoconsent rules for https://www.oxfordmail.co.uk/

---
+++ tests/generated/auto_GB_oxfordmail.co.uk_rbr.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_GB_oxfordmail.co.uk_rbr', ['https://www.oxfordmail.co.uk/'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['GB'],
+});
