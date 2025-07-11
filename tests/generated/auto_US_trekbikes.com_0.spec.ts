71c2962d693aa0d8aa47846910f365a5e54f0dd1
Dax the Deployer <dax@duckduckgo.com>
2025-07-11 09:21:41 -0400

    Add autoconsent rules for https://www.trekbikes.com/us/en_US/

--- tests/generated/auto_US_trekbikes.com_0.spec.ts
+++ tests/generated/auto_US_trekbikes.com_0.spec.ts
@@ -1,3 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
-
-generateCMPTests('auto_US_trekbikes.com_0', ['https://www.trekbikes.com/us/en_US/']);
+generateCMPTests('auto_US_trekbikes.com_0', ['https://www.trekbikes.com/us/en_US/'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['US'],
+});
