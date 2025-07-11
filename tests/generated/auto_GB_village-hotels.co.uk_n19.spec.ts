0b117902091b21f007f3c53d26eab3db0cc16aaf
Dax the Deployer <dax@duckduckgo.com>
2025-07-11 19:25:05 -0400

    Add autoconsent rules for https://www.village-hotels.co.uk/

---
+++ tests/generated/auto_GB_village-hotels.co.uk_n19.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_GB_village-hotels.co.uk_n19', ['https://www.village-hotels.co.uk/'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['GB'],
+});
