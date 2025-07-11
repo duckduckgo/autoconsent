a1a65ce507725814008320c9c998f856c17e88b1
Dax the Deployer <dax@duckduckgo.com>
2025-07-11 09:21:39 -0400

    Add autoconsent rules for https://monkeytype.com/

--- tests/generated/auto_US_monkeytype.com_0.spec.ts
+++ tests/generated/auto_US_monkeytype.com_0.spec.ts
@@ -1,3 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-
-generateCMPTests('auto_US_monkeytype.com_0', ['https://monkeytype.com/']);
+generateCMPTests('auto_US_monkeytype.com_0', ['https://monkeytype.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['US'] });
