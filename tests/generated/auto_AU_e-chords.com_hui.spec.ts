86523cf01ea85d2b43d237a7acb590db20f7cb37
Dax the Deployer <dax@duckduckgo.com>
2025-07-11 19:25:00 -0400

    Add autoconsent rules for https://www.e-chords.com/

---
+++ tests/generated/auto_AU_e-chords.com_hui.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_AU_e-chords.com_hui', ['https://www.e-chords.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['AU'] });
