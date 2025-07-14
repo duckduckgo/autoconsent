f68fab2652d985056f4370feef38e5f19567a171
Maxim Tsoy <maks.tsoy@gmail.com>
2025-07-14 14:36:50 +0200

    Support reject button in moove

--- tests/moove.spec.ts
+++ tests/moove.spec.ts
@@ -2,6 +2,6 @@ import generateCMPTests from '../playwright/runner';
 
 generateCMPTests(
     'Moove',
-    ['https://impact.parkinson.org/', 'https://wamu.org/', 'https://www.phorest.com/', 'https://theposterclub.com/'],
+    ['https://impact.parkinson.org/', 'https://wamu.org/', 'https://www.phorest.com/', 'https://theposterclub.com/', 'https://gcloyola.com/autor/fernando-vidal/'],
     {},
 );
