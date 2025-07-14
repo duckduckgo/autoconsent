c103c0410f751f031752e58775d9449f9a5d44d6
Dax the Deployer <dax@duckduckgo.com>
2025-07-14 03:32:31 -0400

    Add autoconsent rules for https://suppsworld.com.au/

---
+++ tests/generated/auto_AU_suppsworld.com.au_z8q.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_AU_suppsworld.com.au_z8q', ['https://suppsworld.com.au/'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['AU'],
+});
