9708fc23cca60ccb7c9bc3d3360f1ae595597e1d
Dax the Deployer <dax@duckduckgo.com>
2025-07-14 03:32:34 -0400

    Add autoconsent rules for https://www.solidworks.com/

---
+++ tests/generated/auto_AU_solidworks.com_s3r.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_AU_solidworks.com_s3r', ['https://www.solidworks.com/'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['AU'],
+});
