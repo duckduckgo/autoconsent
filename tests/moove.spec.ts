11ac045ea4493d29d2f839408b8d74e9e9c83d1f
Maxim Tsoy <maks.tsoy@gmail.com>
2025-07-14 15:33:08 +0200

    Lint fix

--- tests/moove.spec.ts
+++ tests/moove.spec.ts
@@ -2,6 +2,12 @@ import generateCMPTests from '../playwright/runner';
 
 generateCMPTests(
     'Moove',
-    ['https://impact.parkinson.org/', 'https://wamu.org/', 'https://www.phorest.com/', 'https://theposterclub.com/', 'https://gcloyola.com/autor/fernando-vidal/'],
+    [
+        'https://impact.parkinson.org/',
+        'https://wamu.org/',
+        'https://www.phorest.com/',
+        'https://theposterclub.com/',
+        'https://gcloyola.com/autor/fernando-vidal/',
+    ],
     {},
 );
