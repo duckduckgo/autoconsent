16fa96d32686cb78848bdee9ae01d4bc406531be
Dax the Deployer <dax@duckduckgo.com>
2025-07-14 03:32:31 -0400

    Add autoconsent rules for https://vrporn.com/

---
+++ tests/generated/auto_AU_vrporn.com_7j8.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_AU_vrporn.com_7j8', ['https://vrporn.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['AU'] });
