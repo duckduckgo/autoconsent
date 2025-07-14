d41046fa53507a39d40061127225f5d8f8becc0e
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-07-14 17:03:59 +0200

    expand existing bbc rule to another dubdomains (#822)

--- tests/generated/auto_GB_genome.ch.bbc.co.uk_0.spec.ts
+++ tests/generated/auto_GB_genome.ch.bbc.co.uk_0.spec.ts
@@ -1,5 +1,5 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_genome.ch.bbc.co.uk_0', ['https://genome.ch.bbc.co.uk/'], {
+generateCMPTests('auto_GB_genome.ch.bbc.co.uk_0', ['https://genome.ch.bbc.co.uk/', 'https://www.bbc.co.uk/iplayer'], {
     testOptIn: false,
     testSelfTest: false,
     onlyRegions: ['GB'],
