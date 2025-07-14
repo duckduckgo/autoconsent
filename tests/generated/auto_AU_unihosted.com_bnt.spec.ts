c49a1bd1d3e39c095b25b3b6227c53846c8e905a
Dax the Deployer <dax@duckduckgo.com>
2025-07-14 03:32:35 -0400

    Add autoconsent rules for https://www.unihosted.com/

---
+++ tests/generated/auto_AU_unihosted.com_bnt.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_AU_unihosted.com_bnt', ['https://www.unihosted.com/'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['AU'],
+});
