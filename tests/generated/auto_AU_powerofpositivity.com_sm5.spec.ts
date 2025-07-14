c6ceda516d2a3c949ac859a378c73e437b55abd1
Dax the Deployer <dax@duckduckgo.com>
2025-07-14 03:32:34 -0400

    Add autoconsent rules for https://www.powerofpositivity.com/

---
+++ tests/generated/auto_AU_powerofpositivity.com_sm5.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_AU_powerofpositivity.com_sm5', ['https://www.powerofpositivity.com/'], {
+    testOptIn: false,
+    testSelfTest: false,
+    onlyRegions: ['AU'],
+});
