516b33121e258e17fdd0bbebff82e2f5e9a474bd
Maxim Tsoy <maks.tsoy@gmail.com>
2025-07-15 12:46:00 +0200

    remove broken rule

--- tests/generated/auto_AU_hyrox.com_j0m.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_AU_hyrox.com_j0m', ['https://hyrox.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['AU'] });
