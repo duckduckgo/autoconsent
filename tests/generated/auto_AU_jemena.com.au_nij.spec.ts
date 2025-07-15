3e3bfb913f07a8949589cac08909ea8ddd13ff4c
Maxim Tsoy <maks.tsoy@gmail.com>
2025-07-15 12:54:18 +0200

    Remove unstable rule

--- tests/generated/auto_AU_jemena.com.au_nij.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_AU_jemena.com.au_nij', ['https://www.jemena.com.au/'], {
-    testOptIn: false,
-    testSelfTest: false,
-    onlyRegions: ['AU'],
-});
