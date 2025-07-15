d9f6f92bdad98081ed19d1cca9bc4908ae58e1be
Maxim Tsoy <maks.tsoy@gmail.com>
2025-07-15 12:14:40 +0200

    remove incorrect rule

--- tests/generated/auto_AU_auda.org.au_w0d.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_AU_auda.org.au_w0d', ['https://www.auda.org.au/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['AU'] });
