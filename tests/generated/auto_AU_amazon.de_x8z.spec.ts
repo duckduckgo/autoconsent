5bbf787e9f77ac18f6b654430bf8d3927120cb98
Maxim Tsoy <maks.tsoy@gmail.com>
2025-07-15 12:25:01 +0200

    rremove unnecessary rule

--- tests/generated/auto_AU_amazon.de_x8z.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_AU_amazon.de_x8z', ['https://www.amazon.de/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['AU'] });
