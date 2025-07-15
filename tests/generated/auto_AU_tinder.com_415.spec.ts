6a4b851b7fa25c93b6a8e14a9a5ae5535dc93033
Maxim Tsoy <maks.tsoy@gmail.com>
2025-07-15 13:52:23 +0200

    Cleanup tinder rules

--- tests/generated/auto_AU_tinder.com_415.spec.ts
+++ tests/generated/auto_AU_tinder.com_415.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_AU_tinder.com_415', ['https://tinder.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['AU'] });
+generateCMPTests('auto_AU_tinder.com_415', ['https://tinder.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['AU', 'US', 'CA', 'GB'] });
