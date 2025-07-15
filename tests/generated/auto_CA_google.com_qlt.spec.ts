9e02b305a129562a1758386f94382c6aec322bf7
Maxim Tsoy <maks.tsoy@gmail.com>
2025-07-15 13:46:34 +0200

    Fix fleshbot rule

--- tests/generated/auto_CA_google.com_qlt.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CA_google.com_qlt', ['https://www.google.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['CA'] });
