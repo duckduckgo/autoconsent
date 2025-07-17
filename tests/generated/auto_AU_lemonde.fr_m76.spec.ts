bcde8e9a2a126207461980d606861eec42ca995d
Dax the Deployer <dax@duckduckgo.com>
2025-07-17 16:10:08 -0400

    Add autoconsent rules for https://www.lemonde.fr/en/opinion/article/2024/03/21/will-china-really-become-the-world-s-leading-economic-power_6640185_23.html

---
+++ tests/generated/auto_AU_lemonde.fr_m76.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests(
+    'auto_AU_lemonde.fr_m76',
+    ['https://www.lemonde.fr/en/opinion/article/2024/03/21/will-china-really-become-the-world-s-leading-economic-power_6640185_23.html'],
+    { testOptIn: false, testSelfTest: true, onlyRegions: ['AU'] },
+);
