402fa604a896340c4a4c752f78307636d8293302
Maxim Tsoy <maks.tsoy@gmail.com>
2025-07-21 11:33:14 +0200

    Fix axeptio popup detection

--- tests/axeptio.spec.ts
+++ tests/axeptio.spec.ts
@@ -1,3 +1,8 @@
 import generateCMPTests from '../playwright/runner';
 
-generateCMPTests('axeptio', ['https://www.welcometothejungle.com/', 'https://frichti.co/landing?redirectTo=/', 'https://www.dorcel.com/']);
+generateCMPTests('axeptio', [
+    'https://www.welcometothejungle.com/',
+    'https://frichti.co/landing?redirectTo=/',
+    'https://www.dorcel.com/',
+    'https://mistral.ai/news/le-chat-dives-deep',
+]);
