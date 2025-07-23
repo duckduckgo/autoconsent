a5a108c2d7a048fd07a791c33ef874daaca57aed
Dax the Deployer <dax@duckduckgo.com>
2025-07-23 13:56:59 -0400

    Add autoconsent rules for https://www.eadt.co.uk/

---
+++ tests/generated/auto_GB_eadt.co.uk_jsu.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_GB_eadt.co.uk_jsu', ['https://www.eadt.co.uk/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
