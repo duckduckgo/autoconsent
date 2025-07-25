ddd60212e480a31e1990ba528be335b7bb91f214
Dax the Deployer <dax@duckduckgo.com>
2025-07-25 14:55:12 -0400

    Add autoconsent rules for https://www.sohohouse.com/en-us/

--- tests/generated/auto_GB_sohohouse.com_0.spec.ts
+++ tests/generated/auto_GB_sohohouse.com_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_sohohouse.com_0', ['https://www.sohohouse.com/en-us/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
