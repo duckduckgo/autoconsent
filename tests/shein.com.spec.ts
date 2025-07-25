a1bc6eb69b80f220ea60f2bef99db56fe07ffc30
Maxim Tsoy <maks.tsoy@gmail.com>
2025-07-25 11:11:45 +0200

    Lint fix

--- tests/shein.com.spec.ts
+++ tests/shein.com.spec.ts
@@ -1,5 +1,3 @@
-import generateCMPTests from "../playwright/runner";
+import generateCMPTests from '../playwright/runner';
 
-generateCMPTests('shein.com', [
-  'https://nl.shein.com/', 'https://us.shein.com/'
-]);
+generateCMPTests('shein.com', ['https://nl.shein.com/', 'https://us.shein.com/']);
