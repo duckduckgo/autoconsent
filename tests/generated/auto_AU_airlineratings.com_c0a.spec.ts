a0e37c5646b41c5a7a5bd82490d7e457fc9a194d
Dax the Deployer <dax@duckduckgo.com>
2025-07-25 14:54:05 -0400

    Add autoconsent rules for https://www.airlineratings.com/

--- tests/generated/auto_AU_airlineratings.com_c0a.spec.ts
+++ tests/generated/auto_AU_airlineratings.com_c0a.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_AU_airlineratings.com_c0a', ['https://www.airlineratings.com/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['AU'],
 });
