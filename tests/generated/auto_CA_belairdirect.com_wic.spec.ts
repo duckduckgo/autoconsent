3fb94629158d3afb6cebd463043ae4c6c3862cac
Dax the Deployer <dax@duckduckgo.com>
2025-07-25 14:54:11 -0400

    Add autoconsent rules for https://www.belairdirect.com/

--- tests/generated/auto_CA_belairdirect.com_wic.spec.ts
+++ tests/generated/auto_CA_belairdirect.com_wic.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_CA_belairdirect.com_wic', ['https://www.belairdirect.com/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['CA'],
 });
