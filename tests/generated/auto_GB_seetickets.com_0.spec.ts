1ed949aa4c1464ba42b242c2a6a910925588e8fc
Dax the Deployer <dax@duckduckgo.com>
2025-07-25 14:55:10 -0400

    Add autoconsent rules for https://www.seetickets.com/

--- tests/generated/auto_GB_seetickets.com_0.spec.ts
+++ tests/generated/auto_GB_seetickets.com_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_seetickets.com_0', ['https://www.seetickets.com/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
