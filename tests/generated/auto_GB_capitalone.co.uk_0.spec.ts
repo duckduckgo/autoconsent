cfabe4448b31e3ac883b601831ac22f63afedf19
Dax the Deployer <dax@duckduckgo.com>
2025-07-25 14:54:16 -0400

    Add autoconsent rules for https://www.capitalone.co.uk/

--- tests/generated/auto_GB_capitalone.co.uk_0.spec.ts
+++ tests/generated/auto_GB_capitalone.co.uk_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_capitalone.co.uk_0', ['https://www.capitalone.co.uk/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
