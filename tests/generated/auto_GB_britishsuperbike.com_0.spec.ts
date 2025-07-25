c4003eb815cc1e7f7f9b704f8a233b6c9fd3690c
Dax the Deployer <dax@duckduckgo.com>
2025-07-25 14:54:14 -0400

    Add autoconsent rules for https://www.britishsuperbike.com/

--- tests/generated/auto_GB_britishsuperbike.com_0.spec.ts
+++ tests/generated/auto_GB_britishsuperbike.com_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_britishsuperbike.com_0', ['https://www.britishsuperbike.com/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
