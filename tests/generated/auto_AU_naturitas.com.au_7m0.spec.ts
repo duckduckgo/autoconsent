aa4fa541014f3417e4cee757dc8806e4a153dbce
Dax the Deployer <dax@duckduckgo.com>
2025-07-25 14:54:54 -0400

    Add autoconsent rules for https://www.naturitas.com.au/

--- tests/generated/auto_AU_naturitas.com.au_7m0.spec.ts
+++ tests/generated/auto_AU_naturitas.com.au_7m0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_AU_naturitas.com.au_7m0', ['https://www.naturitas.com.au/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['AU'],
 });
