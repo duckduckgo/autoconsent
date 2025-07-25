8d6d33cebbaaee08e0f43df6836cb280225e1449
Dax the Deployer <dax@duckduckgo.com>
2025-07-25 14:54:20 -0400

    Add autoconsent rules for https://www.coverworld.com.au/

--- tests/generated/auto_AU_coverworld.com.au_884.spec.ts
+++ tests/generated/auto_AU_coverworld.com.au_884.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_AU_coverworld.com.au_884', ['https://www.coverworld.com.au/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['AU'],
 });
