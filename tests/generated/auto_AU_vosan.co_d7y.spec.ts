5948d93165994d1566d1d683f1c7c38ddb566052
Dax the Deployer <dax@duckduckgo.com>
2025-07-25 14:55:30 -0400

    Add autoconsent rules for https://www.vosan.co/

--- tests/generated/auto_AU_vosan.co_d7y.spec.ts
+++ tests/generated/auto_AU_vosan.co_d7y.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_AU_vosan.co_d7y', ['https://www.vosan.co/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['AU'] });
+generateCMPTests('auto_AU_vosan.co_d7y', ['https://www.vosan.co/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['AU'] });
