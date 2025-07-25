4b2fcf07ded80e575dcdd5448b37c1363f3f74db
Dax the Deployer <dax@duckduckgo.com>
2025-07-25 14:54:55 -0400

    Add autoconsent rules for https://www.nhsgp.net/

--- tests/generated/auto_GB_nhsgp.net_g3s.spec.ts
+++ tests/generated/auto_GB_nhsgp.net_g3s.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_nhsgp.net_g3s', ['https://www.nhsgp.net/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
+generateCMPTests('auto_GB_nhsgp.net_g3s', ['https://www.nhsgp.net/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
