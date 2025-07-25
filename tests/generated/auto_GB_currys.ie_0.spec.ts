5a8fd19f91f7184213c037fd66a4db070962c206
Dax the Deployer <dax@duckduckgo.com>
2025-07-25 14:54:20 -0400

    Add autoconsent rules for https://www.currys.ie/

--- tests/generated/auto_GB_currys.ie_0.spec.ts
+++ tests/generated/auto_GB_currys.ie_0.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_currys.ie_0', ['https://www.currys.ie/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
+generateCMPTests('auto_GB_currys.ie_0', ['https://www.currys.ie/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
