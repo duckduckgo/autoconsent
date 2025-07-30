220bf4f61c5820b25c3306bcfbdc363cf99c9606
Dax the Deployer <dax@duckduckgo.com>
2025-07-30 14:30:14 -0400

    Add autoconsent rules for https://www.nbim.no/no/

---
+++ tests/generated/auto_NO_nbim.no_zlt.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_NO_nbim.no_zlt', ['https://www.nbim.no/no/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NO'] });
