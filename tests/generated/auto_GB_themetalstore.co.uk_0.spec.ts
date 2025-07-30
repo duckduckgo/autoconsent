529cb5136ca96741e453080a49f82ef5578c616f
Dax the Deployer <dax@duckduckgo.com>
2025-07-30 14:30:20 -0400

    Add autoconsent rules for https://www.themetalstore.co.uk/

--- tests/generated/auto_GB_themetalstore.co.uk_0.spec.ts
+++ tests/generated/auto_GB_themetalstore.co.uk_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_themetalstore.co.uk_0', ['https://www.themetalstore.co.uk/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
