80f46f9d32c020b4ed9730d5786e0d4953c2e149
Dax the Deployer <dax@duckduckgo.com>
2025-07-30 14:30:20 -0400

    Add autoconsent rules for https://www.tnt.com/express/fr_fr/site/home.html

---
+++ tests/generated/auto_FR_tnt.com_ie3.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_FR_tnt.com_ie3', ['https://www.tnt.com/express/fr_fr/site/home.html'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['FR'],
+});
