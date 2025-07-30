56051d39eec0ef06a6c8a6932631d6023034067d
Dax the Deployer <dax@duckduckgo.com>
2025-07-30 14:30:01 -0400

    Add autoconsent rules for https://www.407etr.com/

--- tests/generated/auto_CA_407etr.com_wrd.spec.ts
+++ tests/generated/auto_CA_407etr.com_wrd.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CA_407etr.com_wrd', ['https://www.407etr.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['CA'] });
+generateCMPTests('auto_CA_407etr.com_wrd', ['https://www.407etr.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CA'] });
