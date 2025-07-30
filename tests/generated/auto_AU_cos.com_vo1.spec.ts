972ad9f95755df4e742d5bec4f6d7d161d67332d
Dax the Deployer <dax@duckduckgo.com>
2025-07-30 14:30:05 -0400

    Add autoconsent rules for https://www.cos.com/index.html

---
+++ tests/generated/auto_AU_cos.com_vo1.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_AU_cos.com_vo1', ['https://www.cos.com/index.html'], { testOptIn: false, testSelfTest: true, onlyRegions: ['AU'] });
