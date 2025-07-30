0f33d81186171921aa9b40d31c7d180e52f5557f
Dax the Deployer <dax@duckduckgo.com>
2025-07-30 14:30:04 -0400

    Add autoconsent rules for https://www.britishironworkcentre.co.uk/

--- tests/generated/auto_GB_britishironworkcentre.co.uk_fhq.spec.ts
+++ tests/generated/auto_GB_britishironworkcentre.co.uk_fhq.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_britishironworkcentre.co.uk_fhq', ['https://www.britishironworkcentre.co.uk/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
