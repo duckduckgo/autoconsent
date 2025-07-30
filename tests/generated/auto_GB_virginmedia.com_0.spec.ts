2f6280ce949ec94ffdf88674e25517765e0bac67
Dax the Deployer <dax@duckduckgo.com>
2025-07-30 14:30:21 -0400

    Add autoconsent rules for https://www.virginmedia.com/

--- tests/generated/auto_GB_virginmedia.com_0.spec.ts
+++ tests/generated/auto_GB_virginmedia.com_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_virginmedia.com_0', ['https://www.virginmedia.com/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
