e9546ec1d812efac61404407d4b6777386332249
Dax the Deployer <dax@duckduckgo.com>
2025-07-30 14:30:10 -0400

    Add autoconsent rules for https://www.hogeschoolrotterdam.nl/

---
+++ tests/generated/auto_NL_hogeschoolrotterdam.nl_bv6.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_NL_hogeschoolrotterdam.nl_bv6', ['https://www.hogeschoolrotterdam.nl/'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['NL'],
+});
