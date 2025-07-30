7e25bdde5d53fe7b8a192469d2ef9100584506bc
Dax the Deployer <dax@duckduckgo.com>
2025-07-30 14:30:17 -0400

    Add autoconsent rules for https://www.rexel.co.uk/uki/

--- tests/generated/auto_GB_rexel.co.uk_0.spec.ts
+++ tests/generated/auto_GB_rexel.co.uk_0.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_rexel.co.uk_0', ['https://www.rexel.co.uk/uki/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
+generateCMPTests('auto_GB_rexel.co.uk_0', ['https://www.rexel.co.uk/uki/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
