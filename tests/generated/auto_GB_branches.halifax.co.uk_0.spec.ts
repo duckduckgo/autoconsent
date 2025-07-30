6d9149465767f4dff77dae119975c49616d9c24f
Dax the Deployer <dax@duckduckgo.com>
2025-07-30 14:29:52 -0400

    Add autoconsent rules for https://branches.halifax.co.uk/

--- tests/generated/auto_GB_branches.halifax.co.uk_0.spec.ts
+++ tests/generated/auto_GB_branches.halifax.co.uk_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_branches.halifax.co.uk_0', ['https://branches.halifax.co.uk/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
