7be3fe817ab7dc5e78caead15267e4e3d2314706
Dax the Deployer <dax@duckduckgo.com>
2025-07-30 14:30:04 -0400

    Add autoconsent rules for https://www.cam4.com/

--- tests/generated/auto_AU_cam4.com_cyv.spec.ts
+++ tests/generated/auto_AU_cam4.com_cyv.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_AU_cam4.com_cyv', ['https://www.cam4.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['AU'] });
+generateCMPTests('auto_AU_cam4.com_cyv', ['https://www.cam4.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['AU'] });
