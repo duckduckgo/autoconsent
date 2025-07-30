e3fb8d87d06ec7b1ce19129a48357720c6557e33
Dax the Deployer <dax@duckduckgo.com>
2025-07-30 14:29:54 -0400

    Add autoconsent rules for https://finom.co/

---
+++ tests/generated/auto_DE_finom.co_8d7.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_DE_finom.co_8d7', ['https://finom.co/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
