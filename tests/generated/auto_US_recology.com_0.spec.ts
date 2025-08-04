64f70c984c80b2ca899fdeaa21b7eac23be89319
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 13:50:34 -0400

    Add autoconsent rules for $'https://www.recology.com/?cookie-consent-set=true'

--- tests/generated/auto_US_recology.com_0.spec.ts
+++ tests/generated/auto_US_recology.com_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_US_recology.com_0', ['https://www.recology.com/?cookie-consent-set=true'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['US'],
 });
