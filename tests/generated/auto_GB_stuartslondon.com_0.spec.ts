e8f3cf237f7a0d3d78024bbb9adcde2f8947d338
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 12:36:47 -0400

    Add autoconsent rules for https://www.stuartslondon.com/

--- tests/generated/auto_GB_stuartslondon.com_0.spec.ts
+++ tests/generated/auto_GB_stuartslondon.com_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_stuartslondon.com_0', ['https://www.stuartslondon.com/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
