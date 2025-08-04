0b8a0d6858a6b9c8789caf341258d08b051858df
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 13:50:33 -0400

    Add autoconsent rules for https://www.phonely.co.uk/

--- tests/generated/auto_GB_phonely.co.uk_0.spec.ts
+++ tests/generated/auto_GB_phonely.co.uk_0.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_phonely.co.uk_0', ['https://www.phonely.co.uk/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
+generateCMPTests('auto_GB_phonely.co.uk_0', ['https://www.phonely.co.uk/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
