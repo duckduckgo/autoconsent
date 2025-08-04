22fdd550aa7b85dd3ac68e825189f44c58c7a807
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 17:34:06 -0400

    Add autoconsent rules for https://poki.com/

--- tests/generated/auto_GB_poki.com_lwh.spec.ts
+++ tests/generated/auto_GB_poki.com_lwh.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_poki.com_lwh', ['https://poki.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
+generateCMPTests('auto_GB_poki.com_lwh', ['https://poki.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
