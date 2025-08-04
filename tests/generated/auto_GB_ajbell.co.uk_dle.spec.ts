0784f0dc7c9281e71e231683e695d645fa7afb40
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 17:34:10 -0400

    Add autoconsent rules for https://www.ajbell.co.uk/

---
+++ tests/generated/auto_GB_ajbell.co.uk_dle.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_GB_ajbell.co.uk_dle', ['https://www.ajbell.co.uk/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
