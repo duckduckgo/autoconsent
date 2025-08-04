37712ae34a89d57adc39849a65e798f3cdf3f31a
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 12:36:48 -0400

    Add autoconsent rules for https://www.theoutnet.com/en-gb/

--- tests/generated/auto_GB_theoutnet.com_0.spec.ts
+++ tests/generated/auto_GB_theoutnet.com_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_theoutnet.com_0', ['https://www.theoutnet.com/en-gb/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
