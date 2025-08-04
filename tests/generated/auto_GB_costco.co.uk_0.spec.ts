84b92c0848bca12b2b6a4f83af988caf050d5ab4
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 16:38:48 -0400

    Add autoconsent rules for https://www.costco.co.uk/

--- tests/generated/auto_GB_costco.co.uk_0.spec.ts
+++ tests/generated/auto_GB_costco.co.uk_0.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_costco.co.uk_0', ['https://www.costco.co.uk/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
+generateCMPTests('auto_GB_costco.co.uk_0', ['https://www.costco.co.uk/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
