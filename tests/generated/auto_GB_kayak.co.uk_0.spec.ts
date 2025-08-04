4ade9d43bc898e7d9c932242d2e771d148690e0d
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 12:36:40 -0400

    Add autoconsent rules for $'https://www.kayak.co.uk/?ispredir=true'

--- tests/generated/auto_GB_kayak.co.uk_0.spec.ts
+++ tests/generated/auto_GB_kayak.co.uk_0.spec.ts
@@ -1,2 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_kayak.co.uk_0', ['https://www.kayak.co.uk/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
+generateCMPTests('auto_GB_kayak.co.uk_0', ['https://www.kayak.co.uk/?ispredir=true'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['GB'],
+});
