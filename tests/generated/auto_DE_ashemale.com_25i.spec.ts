c33a221c4a09e74444de3b87830a744797ef3666
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 13:50:20 -0400

    Add autoconsent rules for https://www.ashemale.com/

---
+++ tests/generated/auto_DE_ashemale.com_25i.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_DE_ashemale.com_25i', ['https://www.ashemale.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
