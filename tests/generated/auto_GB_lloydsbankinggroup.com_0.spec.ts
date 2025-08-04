7459c7f4b16f8426049062be16c7b35425bc8334
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 13:50:30 -0400

    Add autoconsent rules for https://www.lloydsbankinggroup.com/

--- tests/generated/auto_GB_lloydsbankinggroup.com_0.spec.ts
+++ tests/generated/auto_GB_lloydsbankinggroup.com_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_lloydsbankinggroup.com_0', ['https://www.lloydsbankinggroup.com/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
