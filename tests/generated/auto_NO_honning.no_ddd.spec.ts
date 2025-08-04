5c8026ad7e676aba1ebb74c94da9364278a212f5
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 13:50:14 -0400

    Add autoconsent rules for https://honning.no/

---
+++ tests/generated/auto_NO_honning.no_ddd.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_NO_honning.no_ddd', ['https://honning.no/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NO'] });
