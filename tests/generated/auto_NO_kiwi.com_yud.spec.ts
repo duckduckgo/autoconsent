0dbd2a5c852adda24d1669f9c7d8ab256f91d217
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 16:38:53 -0400

    Add autoconsent rules for https://www.kiwi.com/en/

---
+++ tests/generated/auto_NO_kiwi.com_yud.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_NO_kiwi.com_yud', ['https://www.kiwi.com/en/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NO'] });
