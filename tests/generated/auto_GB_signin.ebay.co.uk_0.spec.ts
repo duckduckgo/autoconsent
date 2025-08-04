c52e1850ef5100ab76f973c9f9f934689a76d2d7
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 12:36:28 -0400

    Add autoconsent rules for $'https://signin.ebay.co.uk/ws/eBayISAPI.dll?SignIn&ru=https%3A%2F%2Fmy.ebay.co.uk%2F&sgfl=sm&smuid=8224f427-6d26-11f0-b153-2b5468523676'

--- tests/generated/auto_GB_signin.ebay.co.uk_0.spec.ts
+++ tests/generated/auto_GB_signin.ebay.co.uk_0.spec.ts
@@ -1,6 +1,8 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests(
     'auto_GB_signin.ebay.co.uk_0',
-    ['https://signin.ebay.co.uk/ws/eBayISAPI.dll?SignIn&ru=https%3A%2F%2Fmy.ebay.co.uk%2F&sgfl=sm&smuid=5dd614ba443b42679a133019ab896beb'],
-    { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] },
+    [
+        'https://signin.ebay.co.uk/ws/eBayISAPI.dll?SignIn&ru=https%3A%2F%2Fmy.ebay.co.uk%2F&sgfl=sm&smuid=8224f427-6d26-11f0-b153-2b5468523676',
+    ],
+    { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] },
 );
