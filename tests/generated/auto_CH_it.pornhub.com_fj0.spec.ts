289c62e23113da1d807cd299afdb7297931fae4b
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 16:38:38 -0400

    Add autoconsent rules for https://it.pornhub.com/

---
+++ tests/generated/auto_CH_it.pornhub.com_fj0.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_CH_it.pornhub.com_fj0', ['https://it.pornhub.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CH'] });
