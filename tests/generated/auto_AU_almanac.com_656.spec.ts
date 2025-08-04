36eae452b5d773dbf4b28885ae72c681dccbf733
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 17:34:10 -0400

    Add autoconsent rules for https://www.almanac.com/

---
+++ tests/generated/auto_AU_almanac.com_656.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_AU_almanac.com_656', ['https://www.almanac.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['AU'] });
