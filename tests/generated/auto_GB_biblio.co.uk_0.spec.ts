c9ac692c4bf4a299355e958cb7cce7619ca87ec5
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 12:36:21 -0400

    Add autoconsent rules for https://biblio.co.uk/

--- tests/generated/auto_GB_biblio.co.uk_0.spec.ts
+++ tests/generated/auto_GB_biblio.co.uk_0.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_biblio.co.uk_0', ['https://biblio.co.uk/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
+generateCMPTests('auto_GB_biblio.co.uk_0', ['https://biblio.co.uk/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
