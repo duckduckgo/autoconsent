8c7452bcdcacf948f04953b85a8525e5672ae4c6
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 16:38:44 -0400

    Add autoconsent rules for https://www.atlasformen.co.uk/

--- tests/generated/auto_GB_atlasformen.co.uk_0.spec.ts
+++ tests/generated/auto_GB_atlasformen.co.uk_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_atlasformen.co.uk_0', ['https://www.atlasformen.co.uk/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
