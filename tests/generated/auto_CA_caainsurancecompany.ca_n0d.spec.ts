f7e7160b79c2cf9178e3c9d77770687ca5ffb58e
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 13:50:22 -0400

    Add autoconsent rules for https://www.caainsurancecompany.ca/

--- tests/generated/auto_CA_caainsurancecompany.ca_n0d.spec.ts
+++ tests/generated/auto_CA_caainsurancecompany.ca_n0d.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_CA_caainsurancecompany.ca_n0d', ['https://www.caainsurancecompany.ca/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['CA'],
 });
