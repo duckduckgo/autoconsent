124346d890a2d8d2d830840f262d8585e4ca19d3
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 13:50:34 -0400

    Add autoconsent rules for https://www.porscheclubgb.com/

--- tests/generated/auto_GB_porscheclubgb.com_0.spec.ts
+++ tests/generated/auto_GB_porscheclubgb.com_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_porscheclubgb.com_0', ['https://www.porscheclubgb.com/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
