52f639d409e06fc0b3673c005beac7d1551b9f7c
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 13:50:41 -0400

    Add autoconsent rules for https://www2.hm.com/en_gb/index.html

--- tests/generated/auto_GB_www2.hm.com_0.spec.ts
+++ tests/generated/auto_GB_www2.hm.com_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_www2.hm.com_0', ['https://www2.hm.com/en_gb/index.html'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
