b7a05ad814b344f57fa6762078f468bc69c487a3
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 16:38:50 -0400

    Add autoconsent rules for https://www.framesdirect.com/

--- tests/generated/auto_US_framesdirect.com_0.spec.ts
+++ tests/generated/auto_US_framesdirect.com_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_US_framesdirect.com_0', ['https://www.framesdirect.com/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['US'],
 });
