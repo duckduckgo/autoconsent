3e90e6771ef157364f4474b139d78a384c3536d5
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 16:38:48 -0400

    Add autoconsent rules for https://www.csagroup.org/

--- tests/generated/auto_CA_csagroup.org_3ho.spec.ts
+++ tests/generated/auto_CA_csagroup.org_3ho.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CA_csagroup.org_3ho', ['https://www.csagroup.org/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['CA'] });
+generateCMPTests('auto_CA_csagroup.org_3ho', ['https://www.csagroup.org/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CA'] });
