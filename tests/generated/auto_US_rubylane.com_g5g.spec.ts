a998c324c0cdbf1ac8c76563f387ebac3304a274
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 12:36:46 -0400

    Add autoconsent rules for https://www.rubylane.com/

--- tests/generated/auto_US_rubylane.com_g5g.spec.ts
+++ tests/generated/auto_US_rubylane.com_g5g.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_US_rubylane.com_g5g', ['https://www.rubylane.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['US'] });
+generateCMPTests('auto_US_rubylane.com_g5g', ['https://www.rubylane.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['US'] });
