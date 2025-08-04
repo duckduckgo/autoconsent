1638a5fef905a8151c270c47d061e88d2910e537
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 12:36:47 -0400

    Add autoconsent rules for https://www.sugarlab.ai/

---
+++ tests/generated/auto_CA_sugarlab.ai_g26.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_CA_sugarlab.ai_g26', ['https://www.sugarlab.ai/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CA'] });
