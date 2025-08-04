70b57ca8ef2f610a0cdcd1d567863abd43548d54
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 12:36:34 -0400

    Add autoconsent rules for https://www.cheapflights.co.uk/

--- tests/generated/auto_GB_cheapflights.co.uk_0.spec.ts
+++ tests/generated/auto_GB_cheapflights.co.uk_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_cheapflights.co.uk_0', ['https://www.cheapflights.co.uk/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
