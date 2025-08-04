aa539284406b12ad9283427e314f2575aa33cdfa
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 13:50:40 -0400

    Add autoconsent rules for https://www.weforum.org/

---
+++ tests/generated/auto_AU_weforum.org_r3k.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_AU_weforum.org_r3k', ['https://www.weforum.org/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['AU'] });
