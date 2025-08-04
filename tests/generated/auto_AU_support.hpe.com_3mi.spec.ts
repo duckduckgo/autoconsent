e1539625fa08a7fd8fff544bc962b331fb793f01
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 16:38:41 -0400

    Add autoconsent rules for $'https://support.hpe.com/connect/s/?language=en_US'

---
+++ tests/generated/auto_AU_support.hpe.com_3mi.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_AU_support.hpe.com_3mi', ['https://support.hpe.com/connect/s/?language=en_US'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['AU'],
+});
