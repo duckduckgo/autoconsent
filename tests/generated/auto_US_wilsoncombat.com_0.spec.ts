d6a91b2ec7f79e41b805c328acad2fb9cfcb5cb2
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 17:34:09 -0400

    Add autoconsent rules for https://wilsoncombat.com/

--- tests/generated/auto_US_wilsoncombat.com_0.spec.ts
+++ tests/generated/auto_US_wilsoncombat.com_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_US_wilsoncombat.com_0', ['https://wilsoncombat.com/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['US'],
 });
