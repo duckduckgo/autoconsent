d814bdd1c5fb7f386d926cef9a81bed715347549
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 16:38:35 -0400

    Add autoconsent rules for https://business.bankofscotland.co.uk/

--- tests/generated/auto_GB_business.bankofscotland.co.uk_0.spec.ts
+++ tests/generated/auto_GB_business.bankofscotland.co.uk_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_business.bankofscotland.co.uk_0', ['https://business.bankofscotland.co.uk/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
