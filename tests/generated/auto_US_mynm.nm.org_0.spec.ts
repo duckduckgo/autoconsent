c21edc9707dd95fba1b6d0db57fca62c60ed237e
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 13:50:15 -0400

    Add autoconsent rules for $'https://mynm.nm.org/MyChart/Authentication/Login?utm_source=mychart.nm.org&utm_medium=301re&utm_campaign=mynm'

--- tests/generated/auto_US_mynm.nm.org_0.spec.ts
+++ tests/generated/auto_US_mynm.nm.org_0.spec.ts
@@ -2,5 +2,5 @@ import generateCMPTests from '../../playwright/runner';
 generateCMPTests(
     'auto_US_mynm.nm.org_0',
     ['https://mynm.nm.org/MyChart/Authentication/Login?utm_source=mychart.nm.org&utm_medium=301re&utm_campaign=mynm'],
-    { testOptIn: false, testSelfTest: false, onlyRegions: ['US'] },
+    { testOptIn: false, testSelfTest: true, onlyRegions: ['US'] },
 );
