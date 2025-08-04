024068bed8935340913c7539075205735d6d606f
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 13:50:31 -0400

    Add autoconsent rules for https://www.marvin.com/

--- tests/generated/auto_US_marvin.com_0.spec.ts
+++ tests/generated/auto_US_marvin.com_0.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_US_marvin.com_0', ['https://www.marvin.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['US'] });
+generateCMPTests('auto_US_marvin.com_0', ['https://www.marvin.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['US'] });
