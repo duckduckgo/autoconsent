c06b01a065f4425a986f69f9a64e395d097509c0
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 16:38:48 -0400

    Add autoconsent rules for https://www.counciltax.info/

--- tests/generated/auto_GB_counciltax.info_0.spec.ts
+++ tests/generated/auto_GB_counciltax.info_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_counciltax.info_0', ['https://www.counciltax.info/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
