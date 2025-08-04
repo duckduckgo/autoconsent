f0649b2631bcb4312b09346359223729a49f5e0c
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 16:38:43 -0400

    Add autoconsent rules for https://www.airport.guide/

--- tests/generated/auto_US_airport.guide_0.spec.ts
+++ tests/generated/auto_US_airport.guide_0.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_US_airport.guide_0', ['https://www.airport.guide/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['US'] });
+generateCMPTests('auto_US_airport.guide_0', ['https://www.airport.guide/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['US'] });
