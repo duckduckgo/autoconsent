3c4b7b63c4888b25714b1cf655d534162ddf1b72
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 12:36:25 -0400

    Add autoconsent rules for https://mycharisma.com/

--- tests/generated/auto_US_mycharisma.com_u6r.spec.ts
+++ tests/generated/auto_US_mycharisma.com_u6r.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_US_mycharisma.com_u6r', ['https://mycharisma.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['US'] });
+generateCMPTests('auto_US_mycharisma.com_u6r', ['https://mycharisma.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['US'] });
