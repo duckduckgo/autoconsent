d5ec9cc632058fd1ae19ccb854b101c37e9e4a9b
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 12:36:22 -0400

    Add autoconsent rules for https://cotswoldsdesigneroutlet.com/

---
+++ tests/generated/auto_GB_cotswoldsdesigneroutlet.com_lu3.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_GB_cotswoldsdesigneroutlet.com_lu3', ['https://cotswoldsdesigneroutlet.com/'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['GB'],
+});
