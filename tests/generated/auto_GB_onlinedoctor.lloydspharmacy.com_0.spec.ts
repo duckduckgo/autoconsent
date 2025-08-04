892fbe8dd38f8be94cfbf6c88d1a70fec71899a3
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 17:34:06 -0400

    Add autoconsent rules for https://onlinedoctor.lloydspharmacy.com/

--- tests/generated/auto_GB_onlinedoctor.lloydspharmacy.com_0.spec.ts
+++ tests/generated/auto_GB_onlinedoctor.lloydspharmacy.com_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_onlinedoctor.lloydspharmacy.com_0', ['https://onlinedoctor.lloydspharmacy.com/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
