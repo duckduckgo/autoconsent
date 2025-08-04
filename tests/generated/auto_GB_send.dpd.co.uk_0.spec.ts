46799197af2fb47e868ba72652e7ea0cf4f9675f
Dax the Deployer <dax@duckduckgo.com>
2025-08-04 16:38:41 -0400

    Add autoconsent rules for https://send.dpd.co.uk/

--- tests/generated/auto_GB_send.dpd.co.uk_0.spec.ts
+++ tests/generated/auto_GB_send.dpd.co.uk_0.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_send.dpd.co.uk_0', ['https://send.dpd.co.uk/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
+generateCMPTests('auto_GB_send.dpd.co.uk_0', ['https://send.dpd.co.uk/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
