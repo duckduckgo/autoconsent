4dfeab52c0270fc049f3c1a5025819d6d78a3f22
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-08-05 09:52:28 -0400

    Add autoconsent rules for https://www.aircanada.com/home/ch/en/aco/flights

---
+++ tests/generated/auto_CH_aircanada.com_gmo.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_CH_aircanada.com_gmo', ['https://www.aircanada.com/home/ch/en/aco/flights'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['CH'],
+});
