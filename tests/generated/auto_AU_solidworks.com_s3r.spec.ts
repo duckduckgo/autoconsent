bb6fbe5f1d3a4f608e9315ff46858ce3aacfeab1
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-08-05 09:53:30 -0400

    Add autoconsent rules for https://www.solidworks.com/

--- tests/generated/auto_AU_solidworks.com_s3r.spec.ts
+++ tests/generated/auto_AU_solidworks.com_s3r.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_AU_solidworks.com_s3r', ['https://www.solidworks.com/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['AU'],
 });
