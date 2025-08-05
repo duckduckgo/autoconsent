0bb3986e66b83aad2e251b80aa1cfcd84036eca4
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-08-05 09:52:02 -0400

    Add autoconsent rules for https://heinnie.com/

--- tests/generated/auto_GB_heinnie.com_0.spec.ts
+++ tests/generated/auto_GB_heinnie.com_0.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_heinnie.com_0', ['https://heinnie.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
+generateCMPTests('auto_GB_heinnie.com_0', ['https://heinnie.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
