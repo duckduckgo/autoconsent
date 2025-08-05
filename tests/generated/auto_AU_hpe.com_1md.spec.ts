44e858731beada2dbb6709a68478799ce7b16550
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-08-05 09:53:01 -0400

    Add autoconsent rules for https://www.hpe.com/us/en/home.html

--- tests/generated/auto_AU_hpe.com_1md.spec.ts
+++ tests/generated/auto_AU_hpe.com_1md.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_AU_hpe.com_1md', ['https://www.hpe.com/us/en/home.html'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['AU'],
 });
