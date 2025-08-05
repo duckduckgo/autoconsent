b485639d8da10d5c47c7fecfea44e0d25129c571
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-08-05 09:52:58 -0400

    Add autoconsent rules for https://www.fruugoaustralia.com/

--- tests/generated/auto_AU_fruugoaustralia.com_ifq.spec.ts
+++ tests/generated/auto_AU_fruugoaustralia.com_ifq.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_AU_fruugoaustralia.com_ifq', ['https://www.fruugoaustralia.com/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['AU'],
 });
