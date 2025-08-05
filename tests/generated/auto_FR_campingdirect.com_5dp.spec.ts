1c9974fffa5011812bff95671ff5c042907bf696
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-08-05 09:52:33 -0400

    Add autoconsent rules for https://www.campingdirect.com/en

---
+++ tests/generated/auto_FR_campingdirect.com_5dp.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_FR_campingdirect.com_5dp', ['https://www.campingdirect.com/en'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['FR'],
+});
