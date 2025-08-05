f7592e8fab0644e91c400e3e3e42e13314981139
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-08-05 09:52:00 -0400

    Add autoconsent rules for $'https://en.powys.gov.uk/?ccp=true'

--- tests/generated/auto_GB_en.powys.gov.uk_0.spec.ts
+++ tests/generated/auto_GB_en.powys.gov.uk_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_en.powys.gov.uk_0', ['https://en.powys.gov.uk/?ccp=true'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
