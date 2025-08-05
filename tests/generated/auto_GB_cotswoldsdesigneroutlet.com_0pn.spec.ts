dbf5a53e33d7a328db36cab7b0e44af2a76822e6
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-08-05 09:51:59 -0400

    Add autoconsent rules for https://cotswoldsdesigneroutlet.com/

---
+++ tests/generated/auto_GB_cotswoldsdesigneroutlet.com_0pn.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_GB_cotswoldsdesigneroutlet.com_0pn', ['https://cotswoldsdesigneroutlet.com/'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['GB'],
+});
