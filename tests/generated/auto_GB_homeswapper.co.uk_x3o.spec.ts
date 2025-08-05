e857211a66f6b39dd51a28e0501299a0195033c1
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-08-05 09:53:00 -0400

    Add autoconsent rules for https://www.homeswapper.co.uk/

--- tests/generated/auto_GB_homeswapper.co.uk_x3o.spec.ts
+++ tests/generated/auto_GB_homeswapper.co.uk_x3o.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_homeswapper.co.uk_x3o', ['https://www.homeswapper.co.uk/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
