ef4ebaf0866167fa320c78f499e26fd41d0b9c39
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-08-05 09:53:06 -0400

    Add autoconsent rules for https://www.nafcamp.no/

---
+++ tests/generated/auto_NO_nafcamp.no_1dk.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_NO_nafcamp.no_1dk', ['https://www.nafcamp.no/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NO'] });
