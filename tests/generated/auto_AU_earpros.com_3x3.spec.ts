f1673d400b0e1c13bc4f1a70c4ec0cd936485ac7
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-08-05 09:52:36 -0400

    Add autoconsent rules for https://www.earpros.com/au

--- tests/generated/auto_AU_earpros.com_3x3.spec.ts
+++ tests/generated/auto_AU_earpros.com_3x3.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_AU_earpros.com_3x3', ['https://www.earpros.com/au'], { testOptIn: false, testSelfTest: false, onlyRegions: ['AU'] });
+generateCMPTests('auto_AU_earpros.com_3x3', ['https://www.earpros.com/au'], { testOptIn: false, testSelfTest: true, onlyRegions: ['AU'] });
