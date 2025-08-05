5775debba7ea5cdf1a3622b09d89e058216e4912
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-08-05 09:53:11 -0400

    Add autoconsent rules for https://www.raymondjames.com/

--- tests/generated/auto_US_raymondjames.com_0.spec.ts
+++ tests/generated/auto_US_raymondjames.com_0.spec.ts
@@ -1,3 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
-
-generateCMPTests('auto_US_raymondjames.com_0', ['https://www.raymondjames.com/']);
+generateCMPTests('auto_US_raymondjames.com_0', ['https://www.raymondjames.com/'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['US'],
+});
