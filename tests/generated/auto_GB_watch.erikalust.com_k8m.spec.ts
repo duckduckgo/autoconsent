7d85f5ac7186ee4154fd05103bec52b715f329b8
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-08-05 09:52:26 -0400

    Add autoconsent rules for https://watch.erikalust.com/age-verification

---
+++ tests/generated/auto_GB_watch.erikalust.com_k8m.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_GB_watch.erikalust.com_k8m', ['https://watch.erikalust.com/age-verification'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['GB'],
+});
