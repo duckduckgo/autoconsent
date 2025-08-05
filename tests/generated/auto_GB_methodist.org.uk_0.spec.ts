33ed0a599ff796e67a0528642ba483a33c47fee4
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-08-05 09:53:05 -0400

    Add autoconsent rules for https://www.methodist.org.uk/

--- tests/generated/auto_GB_methodist.org.uk_0.spec.ts
+++ tests/generated/auto_GB_methodist.org.uk_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_methodist.org.uk_0', ['https://www.methodist.org.uk/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
