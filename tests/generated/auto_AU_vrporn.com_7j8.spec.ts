a78f6c7a28611199851978fcf11baa70f55dbaf3
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-08-05 09:52:26 -0400

    Add autoconsent rules for https://vrporn.com/

--- tests/generated/auto_AU_vrporn.com_7j8.spec.ts
+++ tests/generated/auto_AU_vrporn.com_7j8.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_AU_vrporn.com_7j8', ['https://vrporn.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['AU'] });
+generateCMPTests('auto_AU_vrporn.com_7j8', ['https://vrporn.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['AU'] });
