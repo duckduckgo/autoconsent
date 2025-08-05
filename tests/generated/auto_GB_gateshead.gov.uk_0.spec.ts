45d6c9429a4b3fc378a24975ca3ea8f3978ed68e
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-08-05 09:52:58 -0400

    Add autoconsent rules for https://www.gateshead.gov.uk/

--- tests/generated/auto_GB_gateshead.gov.uk_0.spec.ts
+++ tests/generated/auto_GB_gateshead.gov.uk_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_gateshead.gov.uk_0', ['https://www.gateshead.gov.uk/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
