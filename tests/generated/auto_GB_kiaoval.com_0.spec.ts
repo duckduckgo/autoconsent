3fc11e8cfdd908c5a1e1be43e7f4b9434d27bf15
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-08-05 09:53:02 -0400

    Add autoconsent rules for https://www.kiaoval.com/

--- tests/generated/auto_GB_kiaoval.com_0.spec.ts
+++ tests/generated/auto_GB_kiaoval.com_0.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_kiaoval.com_0', ['https://www.kiaoval.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
+generateCMPTests('auto_GB_kiaoval.com_0', ['https://www.kiaoval.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
