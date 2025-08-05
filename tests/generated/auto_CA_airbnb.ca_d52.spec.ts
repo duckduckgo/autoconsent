ec47d27d65606d1785c1cc42d3f740c62da5b30a
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-08-05 09:52:27 -0400

    Add autoconsent rules for $'https://www.airbnb.ca/?locale=en&_set_bev_on_new_domain=1753877525_EAOGMyZGZlZTYwMz'

--- tests/generated/auto_CA_airbnb.ca_d52.spec.ts
+++ tests/generated/auto_CA_airbnb.ca_d52.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CA_airbnb.ca_d52', ['https://www.airbnb.ca/?locale=en&_set_bev_on_new_domain=1750349954_EAZjM3OTdhMWQyMD'], {
+generateCMPTests('auto_CA_airbnb.ca_d52', ['https://www.airbnb.ca/?locale=en&_set_bev_on_new_domain=1753877525_EAOGMyZGZlZTYwMz'], {
     testOptIn: false,
-    testSelfTest: false,
-    onlyRegions: ['CA', 'AU', 'GB', 'NL'],
+    testSelfTest: true,
+    onlyRegions: ['CA'],
 });
