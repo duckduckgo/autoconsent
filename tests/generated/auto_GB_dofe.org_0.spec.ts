c852f36ed7fe7a9029bea2b943ba5c9e7b730b44
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-08-05 09:52:36 -0400

    Add autoconsent rules for https://www.dofe.org/

--- tests/generated/auto_GB_dofe.org_0.spec.ts
+++ tests/generated/auto_GB_dofe.org_0.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_dofe.org_0', ['https://www.dofe.org/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
+generateCMPTests('auto_GB_dofe.org_0', ['https://www.dofe.org/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
