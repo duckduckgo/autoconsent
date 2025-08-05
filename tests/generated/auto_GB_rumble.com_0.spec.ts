233ead5c615befeb926710de342e594faebdf445
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-08-05 09:52:06 -0400

    Add autoconsent rules for https://rumble.com/

--- tests/generated/auto_GB_rumble.com_0.spec.ts
+++ tests/generated/auto_GB_rumble.com_0.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_rumble.com_0', ['https://rumble.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
+generateCMPTests('auto_GB_rumble.com_0', ['https://rumble.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
