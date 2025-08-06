5b6a12483c52d12f28309fb940ef106815f2cf87
Dax the Deployer <dax@duckduckgo.com>
2025-08-06 14:45:55 -0400

    Add autoconsent rules for https://tfgm.com/

--- tests/generated/auto_GB_tfgm.com_0.spec.ts
+++ tests/generated/auto_GB_tfgm.com_0.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_tfgm.com_0', ['https://tfgm.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
+generateCMPTests('auto_GB_tfgm.com_0', ['https://tfgm.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
