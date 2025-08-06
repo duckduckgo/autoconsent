4d81fa1728cd3bb9c9692a5629b8f4f383cb6256
Dax the Deployer <dax@duckduckgo.com>
2025-08-06 14:45:19 -0400

    Add autoconsent rules for https://bookmygarage.com/

--- tests/generated/auto_GB_bookmygarage.com_0.spec.ts
+++ tests/generated/auto_GB_bookmygarage.com_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_bookmygarage.com_0', ['https://bookmygarage.com/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
