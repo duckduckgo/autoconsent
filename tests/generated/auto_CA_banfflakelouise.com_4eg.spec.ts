eb656b6c8eef4e3d1fc49ad898615d4ac54a3301
Dax the Deployer <dax@duckduckgo.com>
2025-08-06 14:46:23 -0400

    Add autoconsent rules for https://www.banfflakelouise.com/

--- tests/generated/auto_CA_banfflakelouise.com_4eg.spec.ts
+++ tests/generated/auto_CA_banfflakelouise.com_4eg.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_CA_banfflakelouise.com_4eg', ['https://www.banfflakelouise.com/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['CA'],
 });
