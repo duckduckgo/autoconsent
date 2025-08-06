2a25f791a6c229f5e2a4bdf650393eba8e090374
Dax the Deployer <dax@duckduckgo.com>
2025-08-06 14:46:21 -0400

    Add autoconsent rules for https://www.autodesk.com/

---
+++ tests/generated/auto_CA_autodesk.com_oim.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_CA_autodesk.com_oim', ['https://www.autodesk.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CA'] });
