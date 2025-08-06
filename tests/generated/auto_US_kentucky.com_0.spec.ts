467f09e59a0af6d46b3d0182b77eab207f1ca842
Dax the Deployer <dax@duckduckgo.com>
2025-08-06 14:47:33 -0400

    Add autoconsent rules for https://www.kentucky.com/

--- tests/generated/auto_US_kentucky.com_0.spec.ts
+++ tests/generated/auto_US_kentucky.com_0.spec.ts
@@ -1,3 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-
-generateCMPTests('auto_US_kentucky.com_0', ['https://www.kentucky.com/']);
+generateCMPTests('auto_US_kentucky.com_0', ['https://www.kentucky.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['US'] });
