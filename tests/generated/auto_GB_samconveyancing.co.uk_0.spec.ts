f74fb633db8e3cf634d8aa4fa710aceff79b2817
Dax the Deployer <dax@duckduckgo.com>
2025-08-06 14:48:22 -0400

    Add autoconsent rules for https://www.samconveyancing.co.uk/

--- tests/generated/auto_GB_samconveyancing.co.uk_0.spec.ts
+++ tests/generated/auto_GB_samconveyancing.co.uk_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_samconveyancing.co.uk_0', ['https://www.samconveyancing.co.uk/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
