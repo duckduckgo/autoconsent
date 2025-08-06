d1394a1d8913c2e31d79f566e8c29e0e477e5f33
Dax the Deployer <dax@duckduckgo.com>
2025-08-06 14:45:23 -0400

    Add autoconsent rules for https://dunsterhouse.co.uk/

--- tests/generated/auto_GB_dunsterhouse.co.uk_0.spec.ts
+++ tests/generated/auto_GB_dunsterhouse.co.uk_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_dunsterhouse.co.uk_0', ['https://dunsterhouse.co.uk/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
