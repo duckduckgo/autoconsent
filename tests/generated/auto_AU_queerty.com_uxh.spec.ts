14058ac0035e7a7b9eb91df814951c68559d4123
Dax the Deployer <dax@duckduckgo.com>
2025-08-06 14:48:17 -0400

    Add autoconsent rules for https://www.queerty.com/

--- tests/generated/auto_AU_queerty.com_uxh.spec.ts
+++ tests/generated/auto_AU_queerty.com_uxh.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_AU_queerty.com_uxh', ['https://www.queerty.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['AU'] });
+generateCMPTests('auto_AU_queerty.com_uxh', ['https://www.queerty.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['AU'] });
