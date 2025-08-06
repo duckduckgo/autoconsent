09456ba5ef75910e4ba5555a43b4587580bf96bb
Dax the Deployer <dax@duckduckgo.com>
2025-08-06 14:48:18 -0400

    Add autoconsent rules for https://www.rapidonline.com/

--- tests/generated/auto_GB_rapidonline.com_0.spec.ts
+++ tests/generated/auto_GB_rapidonline.com_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_rapidonline.com_0', ['https://www.rapidonline.com/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
