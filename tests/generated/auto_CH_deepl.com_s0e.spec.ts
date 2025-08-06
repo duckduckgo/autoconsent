8aa6da81cabf6123b854ea0755d6b4d678ab0c50
Dax the Deployer <dax@duckduckgo.com>
2025-08-06 14:46:34 -0400

    Add autoconsent rules for https://www.deepl.com/en/translator

---
+++ tests/generated/auto_CH_deepl.com_s0e.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_CH_deepl.com_s0e', ['https://www.deepl.com/en/translator'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['CH'],
+});
