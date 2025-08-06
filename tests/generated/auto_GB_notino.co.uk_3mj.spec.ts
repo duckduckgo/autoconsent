def5fe49abcb54589837a2cf2a8ce5b917ef2076
Dax the Deployer <dax@duckduckgo.com>
2025-08-06 14:48:09 -0400

    Add autoconsent rules for https://www.notino.co.uk/

--- tests/generated/auto_GB_notino.co.uk_3mj.spec.ts
+++ tests/generated/auto_GB_notino.co.uk_3mj.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_notino.co.uk_3mj', ['https://www.notino.co.uk/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
+generateCMPTests('auto_GB_notino.co.uk_3mj', ['https://www.notino.co.uk/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
