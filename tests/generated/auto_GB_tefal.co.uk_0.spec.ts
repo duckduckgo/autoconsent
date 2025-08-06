00e8b1be923dddf8b995658e0e3af0c587d19d5d
Dax the Deployer <dax@duckduckgo.com>
2025-08-06 14:48:53 -0400

    Add autoconsent rules for https://www.tefal.co.uk/

--- tests/generated/auto_GB_tefal.co.uk_0.spec.ts
+++ tests/generated/auto_GB_tefal.co.uk_0.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_tefal.co.uk_0', ['https://www.tefal.co.uk/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
+generateCMPTests('auto_GB_tefal.co.uk_0', ['https://www.tefal.co.uk/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
