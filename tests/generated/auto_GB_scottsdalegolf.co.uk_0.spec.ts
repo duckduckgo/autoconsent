65c132aa947d4910b9c2f6d0df2883bed88313cd
Dax the Deployer <dax@duckduckgo.com>
2025-08-06 14:48:23 -0400

    Add autoconsent rules for https://www.scottsdalegolf.co.uk/

--- tests/generated/auto_GB_scottsdalegolf.co.uk_0.spec.ts
+++ tests/generated/auto_GB_scottsdalegolf.co.uk_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_scottsdalegolf.co.uk_0', ['https://www.scottsdalegolf.co.uk/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
