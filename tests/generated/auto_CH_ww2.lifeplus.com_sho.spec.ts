5df35b12d8084f51f27ae9168805760794916839
Dax the Deployer <dax@duckduckgo.com>
2025-08-06 14:45:58 -0400

    Add autoconsent rules for https://ww2.lifeplus.com/us/en

---
+++ tests/generated/auto_CH_ww2.lifeplus.com_sho.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_CH_ww2.lifeplus.com_sho', ['https://ww2.lifeplus.com/us/en'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['CH'],
+});
