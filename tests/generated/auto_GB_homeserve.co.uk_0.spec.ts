22f85278ce4b91a99b257a373a8d72c0913029e1
Dax the Deployer <dax@duckduckgo.com>
2025-08-06 14:47:09 -0400

    Add autoconsent rules for https://www.homeserve.co.uk/

--- tests/generated/auto_GB_homeserve.co.uk_0.spec.ts
+++ tests/generated/auto_GB_homeserve.co.uk_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_homeserve.co.uk_0', ['https://www.homeserve.co.uk/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
