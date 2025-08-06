4747bff4ed8733432ceddd261b2fca333e61a529
Dax the Deployer <dax@duckduckgo.com>
2025-08-06 14:47:05 -0400

    Add autoconsent rules for https://www.gold-traders.co.uk/

--- tests/generated/auto_GB_gold-traders.co.uk_0.spec.ts
+++ tests/generated/auto_GB_gold-traders.co.uk_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_gold-traders.co.uk_0', ['https://www.gold-traders.co.uk/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
