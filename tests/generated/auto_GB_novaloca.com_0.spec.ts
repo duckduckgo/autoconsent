da96201b75cc7e4f77ec5e9f04c4ec2c873f7add
Dax the Deployer <dax@duckduckgo.com>
2025-08-06 14:48:09 -0400

    Add autoconsent rules for https://www.novaloca.com/

--- tests/generated/auto_GB_novaloca.com_0.spec.ts
+++ tests/generated/auto_GB_novaloca.com_0.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_novaloca.com_0', ['https://www.novaloca.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
+generateCMPTests('auto_GB_novaloca.com_0', ['https://www.novaloca.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
