d571f0f13ad3aa78d4e685fee201b32e9b967b88
Dax the Deployer <dax@duckduckgo.com>
2025-08-06 14:46:00 -0400

    Add autoconsent rules for $'https://www.airbnb.ca/?locale=en&_set_bev_on_new_domain=1754482657_EAOTM2MGQ0NjllNz'

--- tests/generated/auto_CA_airbnb.ca_d52.spec.ts
+++ tests/generated/auto_CA_airbnb.ca_d52.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CA_airbnb.ca_d52', ['https://www.airbnb.ca/?locale=en&_set_bev_on_new_domain=1750349954_EAZjM3OTdhMWQyMD'], {
+generateCMPTests('auto_CA_airbnb.ca_d52', ['https://www.airbnb.ca/?locale=en&_set_bev_on_new_domain=1754482657_EAOTM2MGQ0NjllNz'], {
     testOptIn: false,
-    testSelfTest: false,
-    onlyRegions: ['CA', 'AU', 'GB', 'NL'],
+    testSelfTest: true,
+    onlyRegions: ['CA'],
 });
