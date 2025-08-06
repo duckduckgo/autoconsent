ee19505fd8c1e3a78d94fece4a2dc98b24e51d69
Dax the Deployer <dax@duckduckgo.com>
2025-08-06 14:48:11 -0400

    Add autoconsent rules for https://www.outdooraction.co.uk/

--- tests/generated/auto_GB_outdooraction.co.uk_0.spec.ts
+++ tests/generated/auto_GB_outdooraction.co.uk_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_outdooraction.co.uk_0', ['https://www.outdooraction.co.uk/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
