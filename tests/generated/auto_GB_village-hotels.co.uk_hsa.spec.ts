85a035d5562fc66f71c05e2f686d7b06bd9a2e13
Dax the Deployer <dax@duckduckgo.com>
2025-08-06 14:49:01 -0400

    Add autoconsent rules for https://www.village-hotels.co.uk/

--- tests/generated/auto_GB_village-hotels.co.uk_hsa.spec.ts
+++ tests/generated/auto_GB_village-hotels.co.uk_hsa.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_village-hotels.co.uk_hsa', ['https://www.village-hotels.co.uk/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
