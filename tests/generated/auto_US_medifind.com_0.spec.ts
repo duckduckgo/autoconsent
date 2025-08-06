461a29fe3930d377b482abbdd4869f061aa1d5f8
Dax the Deployer <dax@duckduckgo.com>
2025-08-06 14:47:43 -0400

    Add autoconsent rules for https://www.medifind.com/

--- tests/generated/auto_US_medifind.com_0.spec.ts
+++ tests/generated/auto_US_medifind.com_0.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_US_medifind.com_0', ['https://www.medifind.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['US'] });
+generateCMPTests('auto_US_medifind.com_0', ['https://www.medifind.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['US'] });
