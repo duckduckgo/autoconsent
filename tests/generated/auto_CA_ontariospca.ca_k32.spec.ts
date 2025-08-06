37ff414401937f4909bddafbace1927e834add8c
Dax the Deployer <dax@duckduckgo.com>
2025-08-06 14:45:50 -0400

    Add autoconsent rules for https://ontariospca.ca/

--- tests/generated/auto_CA_ontariospca.ca_k32.spec.ts
+++ tests/generated/auto_CA_ontariospca.ca_k32.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CA_ontariospca.ca_k32', ['https://ontariospca.ca/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['CA'] });
+generateCMPTests('auto_CA_ontariospca.ca_k32', ['https://ontariospca.ca/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CA'] });
