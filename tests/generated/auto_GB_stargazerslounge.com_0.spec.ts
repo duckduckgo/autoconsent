98bcfab2dbe823b241daeebda5e96bf52156dc5e
Dax the Deployer <dax@duckduckgo.com>
2025-08-06 14:45:54 -0400

    Add autoconsent rules for $'https://stargazerslounge.com/index.php?app=core&module=system&controller=cookies&do=cookieConsentToggle&ref=aHR0cHM6Ly9zdGFyZ2F6ZXJzbG91bmdlLmNvbS8=&csrfKey=0026cda7b332b9baea9668fde370a250'

--- tests/generated/auto_GB_stargazerslounge.com_0.spec.ts
+++ tests/generated/auto_GB_stargazerslounge.com_0.spec.ts
@@ -1,6 +1,8 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_stargazerslounge.com_0', ['https://stargazerslounge.com/'], {
-    testOptIn: false,
-    testSelfTest: false,
-    onlyRegions: ['GB'],
-});
+generateCMPTests(
+    'auto_GB_stargazerslounge.com_0',
+    [
+        'https://stargazerslounge.com/index.php?app=core&module=system&controller=cookies&do=cookieConsentToggle&ref=aHR0cHM6Ly9zdGFyZ2F6ZXJzbG91bmdlLmNvbS8=&csrfKey=0026cda7b332b9baea9668fde370a250',
+    ],
+    { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] },
+);
