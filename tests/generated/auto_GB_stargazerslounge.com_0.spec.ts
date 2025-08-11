4adb5e1baeed8e91643e0e3950bf74921538611b
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-08-11 16:30:02 +0200

    Add autoconsent rules for $'https://stargazerslounge.com/index.php?app=core&module=system&controller=cookies&do=cookieConsentToggle&ref=aHR0cHM6Ly9zdGFyZ2F6ZXJzbG91bmdlLmNvbS8=&csrfKey=46da5635bd440e1450e3f880291280f0'

--- tests/generated/auto_GB_stargazerslounge.com_0.spec.ts
+++ tests/generated/auto_GB_stargazerslounge.com_0.spec.ts
@@ -1,6 +1,8 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_stargazerslounge.com_0', ['https://stargazerslounge.com/'], {
-    testOptIn: false,
-    testSelfTest: false,
-    onlyRegions: ['GB'],
-});
+generateCMPTests(
+    'auto_GB_stargazerslounge.com_0',
+    [
+        'https://stargazerslounge.com/index.php?app=core&module=system&controller=cookies&do=cookieConsentToggle&ref=aHR0cHM6Ly9zdGFyZ2F6ZXJzbG91bmdlLmNvbS8=&csrfKey=46da5635bd440e1450e3f880291280f0',
+    ],
+    { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] },
+);
