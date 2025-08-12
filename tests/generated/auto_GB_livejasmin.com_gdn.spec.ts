8139741b2902605372df83114eaa351311562580
Dax the Deployer <dax@duckduckgo.com>
2025-08-12 14:06:07 -0400

    Add autoconsent rules for $'https://www.livejasmin.com/en/girls?forcedesktop=1'

--- tests/generated/auto_GB_livejasmin.com_gdn.spec.ts
+++ tests/generated/auto_GB_livejasmin.com_gdn.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_livejasmin.com_gdn', ['https://www.livejasmin.com/en/girls'], {
+generateCMPTests('auto_GB_livejasmin.com_gdn', ['https://www.livejasmin.com/en/girls?forcedesktop=1'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
