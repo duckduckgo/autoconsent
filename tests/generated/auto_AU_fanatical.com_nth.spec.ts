6cb05cbdf7ff5258d8235d6cd8b196c86a8e3f9a
Dax the Deployer <dax@duckduckgo.com>
2025-08-13 05:31:27 -0400

    Add autoconsent rules for https://www.fanatical.com/en/

--- tests/generated/auto_AU_fanatical.com_nth.spec.ts
+++ tests/generated/auto_AU_fanatical.com_nth.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_AU_fanatical.com_nth', ['https://www.fanatical.com/en/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['AU'],
 });
