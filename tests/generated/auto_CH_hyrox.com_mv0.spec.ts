60bb5e42080940411be956b035d8d38378e0e8dc
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-13 18:16:39 +0200

    Revert "Add autoconsent rules for https://hyrox.com/"
    This reverts commit 971bd26768135d97dcb526f88ccde3dbab23285a.

--- tests/generated/auto_CH_hyrox.com_mv0.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_hyrox.com_mv0', ['https://hyrox.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CH'] });
