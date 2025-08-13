183561755fe304673d96c268363991f44c8c61e2
Dax the Deployer <dax@duckduckgo.com>
2025-08-13 05:33:40 -0400

    Add autoconsent rules for https://www.warwickshire.gov.uk/

--- tests/generated/auto_GB_warwickshire.gov.uk_0.spec.ts
+++ tests/generated/auto_GB_warwickshire.gov.uk_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_warwickshire.gov.uk_0', ['https://www.warwickshire.gov.uk/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
