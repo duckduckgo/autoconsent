33955240acd58653da74427564e6be5ac1fb86bb
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-13 14:16:01 +0200

    Revert "Add autoconsent rules for about:blank"
    This reverts commit aaa164bd2c6eff020ad6dc56d9aa245406340468.

--- tests/generated/auto_AU_wikifeet.com_edn.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_AU_wikifeet.com_edn', ['about:blank'], { testOptIn: false, testSelfTest: true, onlyRegions: ['AU'] });
