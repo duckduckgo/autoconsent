6c43eae6a81a3c40cb5dcf662a8623f10b1844cf
Dax the Deployer <dax@duckduckgo.com>
2025-08-13 05:31:37 -0400

    Add autoconsent rules for https://www.hearth.com/talk/

--- tests/generated/auto_US_hearth.com_0.spec.ts
+++ tests/generated/auto_US_hearth.com_0.spec.ts
@@ -1,3 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-
-generateCMPTests('auto_US_hearth.com_0', ['https://www.hearth.com/talk/']);
+generateCMPTests('auto_US_hearth.com_0', ['https://www.hearth.com/talk/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['US'] });
