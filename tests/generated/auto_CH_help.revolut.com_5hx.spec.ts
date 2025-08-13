27de4df33cf3629ccf6e1dca789e85b6f0d2f60f
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-13 18:16:39 +0200

    Revert "Add autoconsent rules for https://help.revolut.com/help/"
    This reverts commit 1db40b9ab4254afaa4bb54249653d24229075f93.

--- tests/generated/auto_CH_help.revolut.com_5hx.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_help.revolut.com_5hx', ['https://help.revolut.com/help/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['CH'],
-});
