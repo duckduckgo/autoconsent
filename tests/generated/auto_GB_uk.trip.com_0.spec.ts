6c3cb446175c79edbb4b9aadb7b8179ac6b15c5b
Dax the Deployer <dax@duckduckgo.com>
2025-08-13 05:30:20 -0400

    Add autoconsent rules for $'https://uk.trip.com/?locale=en-gb'

--- tests/generated/auto_GB_uk.trip.com_0.spec.ts
+++ tests/generated/auto_GB_uk.trip.com_0.spec.ts
@@ -1,2 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_uk.trip.com_0', ['https://uk.trip.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
+generateCMPTests('auto_GB_uk.trip.com_0', ['https://uk.trip.com/?locale=en-gb'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['GB'],
+});
