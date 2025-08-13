c4e22d05b89acfde195f73c70dd27186f234b657
Dax the Deployer <dax@duckduckgo.com>
2025-08-13 05:31:28 -0400

    Add autoconsent rules for $'https://www.fiat.co.uk/?adobe_mc_ref='

--- tests/generated/auto_GB_fiat.co.uk_cwa.spec.ts
+++ tests/generated/auto_GB_fiat.co.uk_cwa.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_fiat.co.uk_cwa', ['https://www.fiat.co.uk/?adobe_mc_ref='], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
