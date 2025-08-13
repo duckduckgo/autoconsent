b681602580abf74fe877f4fd62d440b5be466bd3
Dax the Deployer <dax@duckduckgo.com>
2025-08-13 05:30:49 -0400

    Add autoconsent rules for https://www.bats.org.uk/

--- tests/generated/auto_GB_bats.org.uk_0.spec.ts
+++ tests/generated/auto_GB_bats.org.uk_0.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_bats.org.uk_0', ['https://www.bats.org.uk/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
+generateCMPTests('auto_GB_bats.org.uk_0', ['https://www.bats.org.uk/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
