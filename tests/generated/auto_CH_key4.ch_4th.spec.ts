4a51cfb98b5e13d8ed187702e03a323d2346d0d7
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-13 18:16:40 +0200

    Revert "Add autoconsent rules for https://key4.ch/en/"
    This reverts commit f4042fd420c7e1927f2b7ad11c5cda6153a71644.

--- tests/generated/auto_CH_key4.ch_4th.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_key4.ch_4th', ['https://key4.ch/en/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CH'] });
