e7115667a83d84727bca268f6267dcf3a7865cdc
Sam Macbeth <smacbeth@duckduckgo.com>
2025-08-13 18:37:49 +0200

    Rewrite livejasmin rule

--- tests/generated/auto_FR_livejasmin.com_c82.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_FR_livejasmin.com_c82', ['https://www.livejasmin.com/en/girls?forcedesktop=1'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['FR'],
-});
--- tests/generated/livejasmin.com.spec.ts
+++ tests/generated/livejasmin.com.spec.ts
@@ -1,5 +1,5 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_livejasmin.com_gdn', ['https://www.livejasmin.com/en/girls?forcedesktop=1'], {
+generateCMPTests('livejasmin.com', ['https://www.livejasmin.com/en/girls?forcedesktop=1'], {
     testOptIn: false,
     testSelfTest: true,
     onlyRegions: ['GB'],
