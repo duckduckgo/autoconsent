bfef3dba8d5e3ed447cd814b601f5eb0c39e5b70
Dax the Deployer <dax@duckduckgo.com>
2025-08-13 05:31:00 -0400

    Add autoconsent rules for https://www.cottonpatch.co.uk/

--- tests/generated/auto_GB_cottonpatch.co.uk_0.spec.ts
+++ tests/generated/auto_GB_cottonpatch.co.uk_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_cottonpatch.co.uk_0', ['https://www.cottonpatch.co.uk/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
