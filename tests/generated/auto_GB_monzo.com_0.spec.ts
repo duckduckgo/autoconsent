cc30ad864aae5a0f2a82842ca0ad98c8eb9ffc2f
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-13 10:52:23 +0200

    Merge rules for monzo

--- tests/generated/auto_GB_monzo.com_0.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_monzo.com_0', ['https://monzo.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
