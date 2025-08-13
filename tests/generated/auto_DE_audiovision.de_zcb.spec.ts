6627f7fec0e7d4ddffcb4f6c6f4c9fc11928be7d
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-13 18:16:39 +0200

    Revert "Add autoconsent rules for https://audiovision.de/"
    This reverts commit 8cfb304e48e6943457883f4aa35fbda93678800c.

--- tests/generated/auto_DE_audiovision.de_zcb.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_audiovision.de_zcb', ['https://audiovision.de/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
