a407a4a88abc2b0a771f2948997d01417ca69336
Dax the Deployer <dax@duckduckgo.com>
2025-08-13 05:30:23 -0400

    Add autoconsent rules for $'https://www.adisc.org/forum/misc/cookies?update=1&reject=1&t=1755041761%2C5c96c647160b51fbf32845b29970fbb2'

--- tests/generated/auto_GB_adisc.org_0.spec.ts
+++ tests/generated/auto_GB_adisc.org_0.spec.ts
@@ -1,2 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_adisc.org_0', ['https://www.adisc.org/forum/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
+generateCMPTests(
+    'auto_GB_adisc.org_0',
+    ['https://www.adisc.org/forum/misc/cookies?update=1&reject=1&t=1755041761%2C5c96c647160b51fbf32845b29970fbb2'],
+    { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] },
+);
