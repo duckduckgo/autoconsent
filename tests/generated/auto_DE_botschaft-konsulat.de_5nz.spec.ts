16b45f26318a176dea7dbdb8df2973bf0f2f08e8
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-13 18:16:40 +0200

    Revert "Add autoconsent rules for https://botschaft-konsulat.de/"
    This reverts commit 1bbe1bb3a5fd5257516a7642a07b61cb9e839e3e.

--- tests/generated/auto_DE_botschaft-konsulat.de_5nz.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_botschaft-konsulat.de_5nz', ['https://botschaft-konsulat.de/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['DE'],
-});
