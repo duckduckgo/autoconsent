f1a4df8c467aaca70e759ff452e4e68189690143
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-13 18:16:39 +0200

    Revert "Add autoconsent rules for https://finanzdepot.ch/"
    This reverts commit 189e3e2170829165ad76617b3dcc3b2fd6770a96.

--- tests/generated/auto_CH_finanzdepot.ch_n6v.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_finanzdepot.ch_n6v', ['https://finanzdepot.ch/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CH'] });
