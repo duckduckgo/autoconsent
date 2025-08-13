4cec62535ca4e8f1fe865d147c317920a02cff45
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-08-13 10:52:21 +0200

    Add autoconsent rules for https://arbtalk.co.uk/

--- tests/generated/auto_GB_arbtalk.co.uk_0.spec.ts
+++ tests/generated/auto_GB_arbtalk.co.uk_0.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_arbtalk.co.uk_0', ['https://arbtalk.co.uk/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
+generateCMPTests('auto_GB_arbtalk.co.uk_0', ['https://arbtalk.co.uk/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
