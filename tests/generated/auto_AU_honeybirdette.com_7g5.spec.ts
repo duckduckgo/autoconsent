5edf6915c332fec4ec3bbe01f4ad54f74ec5603c
Dax the Deployer <dax@duckduckgo.com>
2025-08-13 05:31:39 -0400

    Add autoconsent rules for https://www.honeybirdette.com/

--- tests/generated/auto_AU_honeybirdette.com_7g5.spec.ts
+++ tests/generated/auto_AU_honeybirdette.com_7g5.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_AU_honeybirdette.com_7g5', ['https://www.honeybirdette.com/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['AU'],
 });
