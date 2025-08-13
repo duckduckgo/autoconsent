e749e8458c19fc54f47d949360e07c7914f94438
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-13 18:16:40 +0200

    Revert "Add autoconsent rules for https://homeoftravel.de/"
    This reverts commit 33a2d75917334dba28c55e90e8bb7202de6ec286.

--- tests/generated/auto_DE_homeoftravel.de_w5m.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_homeoftravel.de_w5m', ['https://homeoftravel.de/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['DE'],
-});
