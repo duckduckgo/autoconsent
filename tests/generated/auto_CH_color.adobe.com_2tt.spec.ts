677255ac5dc9338d570d66acd4b44b4e5fbf401d
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-13 18:16:21 +0200

    Revert "Add autoconsent rules for https://color.adobe.com/"
    This reverts commit 8610018d0049cf99ed78e611d82d4b0357e657ef.

--- tests/generated/auto_CH_color.adobe.com_2tt.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_color.adobe.com_2tt', ['https://color.adobe.com/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['CH'],
-});
