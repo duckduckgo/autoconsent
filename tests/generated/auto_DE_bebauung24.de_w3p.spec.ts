cabb4e3ca5b91138f3b1b77f3f0edce65aff1d25
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-13 18:16:40 +0200

    Revert "Add autoconsent rules for https://bebauung24.de/"
    This reverts commit 86d0ed489d01ff86ee16d9961772a528887fa2d6.

--- tests/generated/auto_DE_bebauung24.de_w3p.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_bebauung24.de_w3p', ['https://bebauung24.de/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
