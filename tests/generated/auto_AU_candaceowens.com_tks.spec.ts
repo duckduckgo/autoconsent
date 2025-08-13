7377d819c0bb3a495ca8e171a4b57765b5ca4e12
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-13 14:14:59 +0200

    merge rules for cancaceowens

--- tests/generated/auto_AU_candaceowens.com_tks.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_AU_candaceowens.com_tks', ['https://candaceowens.com/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['AU'],
-});
