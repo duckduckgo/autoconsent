5510d7051ac804c947943c6da85c3e4da53e3d7e
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-13 18:16:40 +0200

    Revert "Add autoconsent rules for https://choisirleservicepublic.gouv.fr/"
    This reverts commit a227a6c94e3678c536052eea86e17acf92e76819.

--- tests/generated/auto_FR_choisirleservicepublic.gouv.fr_0tp.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_FR_choisirleservicepublic.gouv.fr_0tp', ['https://choisirleservicepublic.gouv.fr/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['FR'],
-});
