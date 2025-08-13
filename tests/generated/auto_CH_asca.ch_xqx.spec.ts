c3198fdd4deca8db71fc9a1b8711f8eb4107959b
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-13 18:16:40 +0200

    Revert "Add autoconsent rules for https://asca.ch/"
    This reverts commit b4c05f6be1b431c75c43116e17224f612930e1d7.

--- tests/generated/auto_CH_asca.ch_xqx.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_asca.ch_xqx', ['https://asca.ch/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CH'] });
