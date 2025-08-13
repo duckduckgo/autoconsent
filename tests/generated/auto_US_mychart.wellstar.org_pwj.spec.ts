e38ef0a3b778afdc47594c741e17555ce0f61487
Dax the Deployer <dax@duckduckgo.com>
2025-08-13 05:33:41 -0400

    Add autoconsent rules for https://www.wellstar.org/

--- tests/generated/auto_US_mychart.wellstar.org_pwj.spec.ts
+++ tests/generated/auto_US_mychart.wellstar.org_pwj.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_US_mychart.wellstar.org_pwj', ['https://mychart.wellstar.org/MyChart/Authentication/Login?'], {
+generateCMPTests('auto_US_mychart.wellstar.org_pwj', ['https://www.wellstar.org/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['US'],
 });
