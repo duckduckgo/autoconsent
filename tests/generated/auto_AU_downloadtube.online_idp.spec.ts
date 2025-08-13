a5cdd4f8de9e2d94605fbd93803dfb79360ee5db
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-13 18:16:21 +0200

    Revert "Add autoconsent rules for http://downloadtube.online/en"
    This reverts commit 455ed7e555bd53d59a5f144168a2ab0c28a6bd02.

--- tests/generated/auto_AU_downloadtube.online_idp.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_AU_downloadtube.online_idp', ['http://downloadtube.online/en'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['AU'],
-});
