6626074d57309b1efa9dd74879d75910706ae72c
Dax the Deployer <dax@duckduckgo.com>
2025-08-13 14:06:51 +0200

    Add autoconsent rules for https://careers.bbc.co.uk/

--- tests/generated/auto_GB_genome.ch.bbc.co.uk_0.spec.ts
+++ tests/generated/auto_GB_genome.ch.bbc.co.uk_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_genome.ch.bbc.co.uk_0', ['https://genome.ch.bbc.co.uk/', 'https://www.bbc.co.uk/iplayer'], {
+generateCMPTests('auto_GB_genome.ch.bbc.co.uk_0', ['https://careers.bbc.co.uk/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
