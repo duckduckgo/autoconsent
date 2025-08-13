69f585a48955ffb372f5a99b21e7da234e7fbac9
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-08-13 10:52:23 +0200

    Add autoconsent rules for https://students.flinders.edu.au/

--- tests/generated/auto_AU_students.flinders.edu.au_eh4.spec.ts
+++ tests/generated/auto_AU_students.flinders.edu.au_eh4.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_AU_students.flinders.edu.au_eh4', ['https://students.flinders.edu.au/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['AU'],
 });
