4db755260a0acb9b9c8adca0b2137f8a0d7896cc
Dax the Deployer <dax@duckduckgo.com>
2025-08-13 05:32:42 -0400

    Add autoconsent rules for https://www.orkney.com/

--- tests/generated/auto_GB_orkney.com_0.spec.ts
+++ tests/generated/auto_GB_orkney.com_0.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_orkney.com_0', ['https://www.orkney.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
+generateCMPTests('auto_GB_orkney.com_0', ['https://www.orkney.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
