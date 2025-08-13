68d86b2a55b3dd0f107a7700313f3882922ada6b
Dax the Deployer <dax@duckduckgo.com>
2025-08-13 14:06:51 +0200

    Add autoconsent rules for https://de.redtube.com/

---
+++ tests/generated/auto_CH_de.redtube.com_qd6.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_CH_de.redtube.com_qd6', ['https://de.redtube.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CH'] });
