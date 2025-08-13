003c6fab1100c73436365b299956fd2f045ff889
Dax the Deployer <dax@duckduckgo.com>
2025-08-13 05:34:03 -0400

    Add autoconsent rules for https://www.zopa.com/

--- tests/generated/auto_GB_zopa.com_0.spec.ts
+++ tests/generated/auto_GB_zopa.com_0.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_zopa.com_0', ['https://www.zopa.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
+generateCMPTests('auto_GB_zopa.com_0', ['https://www.zopa.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
