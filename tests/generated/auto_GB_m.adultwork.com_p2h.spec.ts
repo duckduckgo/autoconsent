05b7d55d8022366676663c8f78670d2cc58c590c
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-08-13 10:52:22 +0200

    Add autoconsent rules for https://m.adultwork.com/

--- tests/generated/auto_GB_m.adultwork.com_p2h.spec.ts
+++ tests/generated/auto_GB_m.adultwork.com_p2h.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_m.adultwork.com_p2h', ['https://m.adultwork.com/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
