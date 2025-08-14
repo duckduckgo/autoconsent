a0fc560c9b2f7b6e0d3791e65d7dc7d01188d1cc
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:09:20 +0200

    Revert "Add autoconsent rules for https://www.lloydsbank.com/"
    This reverts commit ba71895d43d9bb31c4594acc3d9dbeee39d98361.

--- tests/generated/auto_CH_lloydsbank.com_f4p.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_lloydsbank.com_f4p', ['https://www.lloydsbank.com/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['CH'],
-});
