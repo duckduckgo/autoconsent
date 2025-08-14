7ad825ed3dba19b6f4ff6be32e25b4e6274a6857
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:09:20 +0200

    Revert "Add autoconsent rules for https://www.kvk.nl/"
    This reverts commit 0c61ca26443f62b82cd3fcc924cfd9bf743a790b.

--- tests/generated/auto_NL_kvk.nl_vcq.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_kvk.nl_vcq', ['https://www.kvk.nl/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
