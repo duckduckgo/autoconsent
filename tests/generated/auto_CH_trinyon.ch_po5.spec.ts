4f10cca4d654991da2c5fa007e5f147056a68631
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:37:20 +0200

    Revert "Add autoconsent rules for https://www.trinyon.ch/"
    This reverts commit cb3ebafe8b156621f2665b4ddc9436a340f01934.

--- tests/generated/auto_CH_trinyon.ch_po5.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_trinyon.ch_po5', ['https://www.trinyon.ch/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CH'] });
