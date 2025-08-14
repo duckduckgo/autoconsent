f3978b48bb5f16f7a0616195530057364d75af98
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:09:21 +0200

    Revert "Add autoconsent rules for https://www.milieucentraal.nl/"
    This reverts commit 5c2d80d83b74d77ba28b714cc1ebd0d91e30cf0d.

--- tests/generated/auto_NL_milieucentraal.nl_f3u.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_milieucentraal.nl_f3u', ['https://www.milieucentraal.nl/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['NL'],
-});
