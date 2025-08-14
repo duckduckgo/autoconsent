3bce0b7eead677ba516c33c25a04c3d8954e8f31
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:25:39 +0200

    Revert "Add autoconsent rules for https://www.odido.nl/"
    This reverts commit a04ccdad9ce698217880990030766e6194326e94.

--- tests/generated/auto_NL_odido.nl_ia6.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_odido.nl_ia6', ['https://www.odido.nl/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
