024cc46763a373084cb764a79dff029e18aead95
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:25:39 +0200

    Revert "Add autoconsent rules for https://www.omroepwest.nl/"
    This reverts commit e155658bab6dc170295285a60cb22a6acc6cbe7e.

--- tests/generated/auto_NL_omroepwest.nl_k8o.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_omroepwest.nl_k8o', ['https://www.omroepwest.nl/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['NL'],
-});
