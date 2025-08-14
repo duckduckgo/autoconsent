610c4ee967cdbbd4ca786d8a5de53dfaf12a34e3
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 15:13:24 +0200

    Revert "Add autoconsent rules for https://www.familienreisefieber.de/"
    This reverts commit 42c908da0b65340da2ac60e7707f92014b99608c.

--- tests/generated/auto_DE_familienreisefieber.de_svv.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_familienreisefieber.de_svv', ['https://www.familienreisefieber.de/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['DE'],
-});
