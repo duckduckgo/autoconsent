b1ec7468c40c0af3b0820e1177ebb48f0f8572f0
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 15:13:24 +0200

    Revert "Add autoconsent rules for https://www.genussguide-hamburg.com/"
    This reverts commit cc943b4c8d4ed550ea419d25046e6c5bb6ed726f.

--- tests/generated/auto_DE_genussguide-hamburg.com_ku2.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_genussguide-hamburg.com_ku2', ['https://www.genussguide-hamburg.com/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['DE'],
-});
