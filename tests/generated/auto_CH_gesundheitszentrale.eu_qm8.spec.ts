818daa8d85f6115afd2aef012b459df70c60c4ca
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 15:13:24 +0200

    Revert "Add autoconsent rules for https://www.gesundheitszentrale.eu/"
    This reverts commit a16db70e9a70dd4aa31d5d49ca1f874af3dc426b.

--- tests/generated/auto_CH_gesundheitszentrale.eu_qm8.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_gesundheitszentrale.eu_qm8', ['https://www.gesundheitszentrale.eu/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['CH'],
-});
