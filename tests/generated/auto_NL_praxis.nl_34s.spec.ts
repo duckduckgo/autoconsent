91f8b58d974916dcbcdd5b4da6f464a42e8c84a3
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 13:51:49 +0200

    cleanup rules for praxis

--- tests/generated/auto_NL_praxis.nl_34s.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_praxis.nl_34s', ['https://www.praxis.nl/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
