07f661a2d64b7d12216957c7ab5dc9e122cc97e5
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 14:39:31 +0200

    Revert "Add autoconsent rules for https://www.der-windows-papst.de/"
    This reverts commit fd299f221ac90efe5ff635dc1003d5a08f237a1c.

--- tests/generated/auto_DE_der-windows-papst.de_ehi.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_der-windows-papst.de_ehi', ['https://www.der-windows-papst.de/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['DE'],
-});
