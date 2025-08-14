a2697505941f5cb7f33667f2571e0754445fa98d
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:25:39 +0200

    Revert "Add autoconsent rules for https://www.nwo.nl/"
    This reverts commit 2d0a6ad72c1c2841e5cd532694a54cb3d35c2c15.

--- tests/generated/auto_NL_nwo.nl_8qg.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_nwo.nl_8qg', ['https://www.nwo.nl/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
