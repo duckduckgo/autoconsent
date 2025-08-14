ce70fa726f71177c87926c48007a529935d2fa44
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 13:54:21 +0200

    clean www.rheinwerk-verlag.de/ rules

--- tests/generated/auto_CH_rheinwerk-verlag.de_m4r.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_rheinwerk-verlag.de_m4r', ['https://www.rheinwerk-verlag.de/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['CH'],
-});
