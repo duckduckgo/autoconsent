d7dd84dbd2b72caadae702c7e7f472b64ebc0e75
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 12:25:17 +0200

    Revert "Add autoconsent rules for https://rm-kurier.de/"
    This reverts commit 70ef99c89818969e65554c4fcf4d0eff11370154.

--- tests/generated/auto_CH_rm-kurier.de_y79.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_rm-kurier.de_y79', ['https://rm-kurier.de/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CH'] });
