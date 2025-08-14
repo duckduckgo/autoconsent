07bfcd58f9d4625c596b485f6ac88e4b41a6b8d9
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 13:34:43 +0200

    clean up hansgrohe

--- tests/generated/auto_CH_hansgrohe.ch_iye.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_hansgrohe.ch_iye', ['https://www.hansgrohe.ch/de'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['CH'],
-});
--- tests/generated/auto_DE_hansgrohe.de_j8h.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_hansgrohe.de_j8h', ['https://www.hansgrohe.de/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
