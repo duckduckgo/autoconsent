449813dd6160e0892d1f83d66489b27dbeb30039
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:37:20 +0200

    Revert "Add autoconsent rules for https://www.wehkamp.nl/"
    This reverts commit 2a6d1c8c77bca537123a8e5240c5c0fabef6e141.

--- tests/generated/auto_NL_wehkamp.nl_gzl.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_wehkamp.nl_gzl', ['https://www.wehkamp.nl/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
