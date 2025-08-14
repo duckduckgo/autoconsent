98e815b7ec1659f1ba5409850ebbcc6857b0ddd1
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:37:20 +0200

    Revert "Add autoconsent rules for https://www.tingstad.com/se-sv"
    This reverts commit 5715bd4e8409006f3eaad556eb423cdc3a616533.

--- tests/generated/auto_NO_tingstad.com_z23.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NO_tingstad.com_z23', ['https://www.tingstad.com/se-sv'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['NO'],
-});
