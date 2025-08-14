6e5b2eea49406563cdeda3747c6665a4ff36c366
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 15:13:24 +0200

    Revert "Add autoconsent rules for https://www.gld.nl/"
    This reverts commit 39cdb5b2741c3faa7fa9234294732315ba062e6f.

--- tests/generated/auto_NL_gld.nl_s21.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_gld.nl_s21', ['https://www.gld.nl/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
