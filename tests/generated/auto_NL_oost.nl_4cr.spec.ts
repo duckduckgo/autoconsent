8a21551f8499293b3cae2a407ad47791698f908c
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:25:39 +0200

    Revert "Add autoconsent rules for https://www.oost.nl/"
    This reverts commit c5582e664fab28d6bf7982446aa1be11d50c032a.

--- tests/generated/auto_NL_oost.nl_4cr.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_oost.nl_4cr', ['https://www.oost.nl/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
