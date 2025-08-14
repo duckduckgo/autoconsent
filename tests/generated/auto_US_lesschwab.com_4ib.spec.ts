14a967115d1ff327da3d06e1a3ef90d30552817e
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:09:20 +0200

    Revert "Add autoconsent rules for https://www.lesschwab.com/"
    This reverts commit 6602702d8048c525cb320155645b6a36172d2b3f.

--- tests/generated/auto_US_lesschwab.com_4ib.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_US_lesschwab.com_4ib', ['https://www.lesschwab.com/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['US'],
-});
