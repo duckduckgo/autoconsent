5589016e33b37c45f92ba2972aebd1609b7a1145
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:37:20 +0200

    Revert "Add autoconsent rules for https://www.uu.se/"
    This reverts commit 50c772ceec8938a15cb0efe21778d962c25ce7f5.

--- tests/generated/auto_NO_uu.se_u88.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NO_uu.se_u88', ['https://www.uu.se/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NO'] });
