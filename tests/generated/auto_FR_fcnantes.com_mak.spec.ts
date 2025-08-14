2cfebf24675fe7829121033f2465e28717b30360
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 15:13:23 +0200

    Revert "Add autoconsent rules for https://www.fcnantes.com/"
    This reverts commit 064c8b53bd5f1ecdfb5a79657e1bf9350d9c32c7.

--- tests/generated/auto_FR_fcnantes.com_mak.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_FR_fcnantes.com_mak', ['https://www.fcnantes.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['FR'] });
