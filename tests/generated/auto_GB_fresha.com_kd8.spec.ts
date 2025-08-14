fe0d58b55fe6a2cde03f955f28ed3fe2b71379d6
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 15:13:24 +0200

    Revert "Add autoconsent rules for https://www.fresha.com/"
    This reverts commit 0baf2c7df830e60bbb535ddf8c0d25432e7127c8.

--- tests/generated/auto_GB_fresha.com_kd8.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_fresha.com_kd8', ['https://www.fresha.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
