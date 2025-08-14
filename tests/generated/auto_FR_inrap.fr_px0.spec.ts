eca4ccee8a5977f0813b103aecac1257dfe008d9
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 15:13:24 +0200

    Revert "Add autoconsent rules for https://www.inrap.fr/"
    This reverts commit 6fa8e032c9bd835c5c8aab9978b87136f3952c30.

--- tests/generated/auto_FR_inrap.fr_px0.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_FR_inrap.fr_px0', ['https://www.inrap.fr/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['FR'] });
