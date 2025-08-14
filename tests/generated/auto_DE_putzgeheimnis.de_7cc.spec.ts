17ffbc7700146bb910a4adc28d0ed0278359c9b6
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:25:39 +0200

    Revert "Add autoconsent rules for https://www.putzgeheimnis.de/"
    This reverts commit 7ad3f34b37bcae735e96006d36c2171dbb06a9c0.

--- tests/generated/auto_DE_putzgeheimnis.de_7cc.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_putzgeheimnis.de_7cc', ['https://www.putzgeheimnis.de/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['DE'],
-});
