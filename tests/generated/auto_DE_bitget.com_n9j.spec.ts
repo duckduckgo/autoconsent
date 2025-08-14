c809b30099220598eb8dde44d71d41ceefaaee18
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 12:50:04 +0200

    merge bitget rules

--- tests/generated/auto_DE_bitget.com_n9j.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_bitget.com_n9j', ['https://www.bitget.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
