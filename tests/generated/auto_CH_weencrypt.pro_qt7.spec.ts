438ef3847f372b19a32d3f9bbb218d61c0d9ff4e
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 12:25:17 +0200

    Revert "Add autoconsent rules for https://weencrypt.pro/"
    This reverts commit fc213ac41cfdb47b2d4a4a1c6ef7e8babdf5db93.

--- tests/generated/auto_CH_weencrypt.pro_qt7.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_weencrypt.pro_qt7', ['https://weencrypt.pro/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CH'] });
