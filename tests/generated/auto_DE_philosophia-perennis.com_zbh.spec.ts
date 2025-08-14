ae3697cbaa0c0aa9a82c9cb403ac17d235496ecb
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 12:25:17 +0200

    Revert "Add autoconsent rules for https://philosophia-perennis.com/"
    This reverts commit e56c6c548fdd619202a3ac16d7d58f288743d63d.

--- tests/generated/auto_DE_philosophia-perennis.com_zbh.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_philosophia-perennis.com_zbh', ['https://philosophia-perennis.com/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['DE'],
-});
