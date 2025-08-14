221ae6006e6b47140dad41347ae2229dbf931210
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:09:20 +0200

    Revert "Add autoconsent rules for https://www.mbna.co.uk/"
    This reverts commit 8083e83333ce5ff716559ab2f6b3682824c37472.

--- tests/generated/auto_GB_mbna.co.uk_fqt.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_mbna.co.uk_fqt', ['https://www.mbna.co.uk/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
