983369592cf4e36ebdb4ee9c9fd7783d6195374d
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:37:20 +0200

    Revert "Add autoconsent rules for https://www.tutonaut.de/"
    This reverts commit d5172b30235acbee9b75ce3c234f95d876018f8b.

--- tests/generated/auto_DE_tutonaut.de_o50.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_tutonaut.de_o50', ['https://www.tutonaut.de/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
