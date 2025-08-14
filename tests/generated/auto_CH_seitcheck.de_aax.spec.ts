3cbb29523bc25707567403640ab0261ea5c95f26
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 12:25:17 +0200

    Revert "Add autoconsent rules for https://seitcheck.de/"
    This reverts commit a0f516cc9c02475a159e2ab2cf87b03e92ac2c8e.

--- tests/generated/auto_CH_seitcheck.de_aax.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_seitcheck.de_aax', ['https://seitcheck.de/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CH'] });
