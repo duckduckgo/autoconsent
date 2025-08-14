64fbac8a84894c29254ba14928ee3c33b5dae7c5
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:09:21 +0200

    Revert "Add autoconsent rules for https://www.l1.nl/"
    This reverts commit 1ce03d887810d896905e5dcb4b37ec2663dcbfd6.

--- tests/generated/auto_NL_l1.nl_0ov.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_l1.nl_0ov', ['https://www.l1.nl/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
