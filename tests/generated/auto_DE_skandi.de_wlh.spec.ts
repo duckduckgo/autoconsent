4779f89fd4ef244d965f98a1c4581255e09ae8e7
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:25:39 +0200

    Revert "Add autoconsent rules for https://www.skandi.de/"
    This reverts commit 798d0149d153eee2c6545ed4773e2bdd28ce568a.

--- tests/generated/auto_DE_skandi.de_wlh.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_skandi.de_wlh', ['https://www.skandi.de/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
