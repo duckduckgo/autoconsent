25471ea8204fe40b7fafd2c442997cbe3fc58f68
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:09:20 +0200

    Revert "Add autoconsent rules for https://www.louis-moto.ch/de"
    This reverts commit 314633438e1fc8989221228c61cdf3c79542aac4.

--- tests/generated/auto_CH_louis-moto.ch_t1c.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_louis-moto.ch_t1c', ['https://www.louis-moto.ch/de'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['CH'],
-});
