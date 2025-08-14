f519ecc3df420edb4e21730eb3f4d61adcdf0d37
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:25:38 +0200

    Revert "Add autoconsent rules for https://www.schweizergarten.ch/"
    This reverts commit c3ee953af21a6de74a2ebf020bce6437422f3ed6.

--- tests/generated/auto_CH_schweizergarten.ch_j26.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_schweizergarten.ch_j26', ['https://www.schweizergarten.ch/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['CH'],
-});
