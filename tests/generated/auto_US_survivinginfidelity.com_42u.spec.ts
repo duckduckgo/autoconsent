48bae6012ed05db5ef5ca2b64021fbbdfe0d4810
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:37:20 +0200

    Revert "Add autoconsent rules for https://www.survivinginfidelity.com/"
    This reverts commit 6684cacb9357e521ff1011f590d0c2a016265257.

--- tests/generated/auto_US_survivinginfidelity.com_42u.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_US_survivinginfidelity.com_42u', ['https://www.survivinginfidelity.com/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['US'],
-});
