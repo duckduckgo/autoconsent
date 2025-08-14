ffb3036dd8d3eaa3a680394d1801e9d468df4419
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:25:39 +0200

    Revert "Add autoconsent rules for https://www.sp.nl/"
    This reverts commit 4a60798a942b93e742f19e849164893a47aff73a.

--- tests/generated/auto_NL_sp.nl_uxh.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_sp.nl_uxh', ['https://www.sp.nl/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
