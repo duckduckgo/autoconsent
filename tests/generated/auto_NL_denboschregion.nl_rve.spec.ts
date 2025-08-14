10d91e7773c3d5ef188f12fe1d2df80f5d891de3
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 14:39:31 +0200

    Revert "Add autoconsent rules for https://www.denboschregion.nl/nl"
    This reverts commit 058e6c44df4d36b2b402dedce7fca8f5c0f020e4.

--- tests/generated/auto_NL_denboschregion.nl_rve.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_denboschregion.nl_rve', ['https://www.denboschregion.nl/nl'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['NL'],
-});
