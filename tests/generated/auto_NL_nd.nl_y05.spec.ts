863210fd2dcfd54051b1a2ab36f0dfdd9dc48e0e
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:09:20 +0200

    Revert "Add autoconsent rules for https://www.nd.nl/"
    This reverts commit 74e661dfb1dcaf3e9ef02c96990801275766236d.

--- tests/generated/auto_NL_nd.nl_y05.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_nd.nl_y05', ['https://www.nd.nl/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
