8badb3f9f9cf0e1329c915c49fed61c52f09136b
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 13:45:12 +0200

    merge klm rules

--- tests/generated/auto_DE_klm.de_4uy.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_klm.de_4uy', ['https://www.klm.de/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
--- tests/generated/auto_NL_klm.nl_arz.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_klm.nl_arz', ['https://www.klm.nl/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
