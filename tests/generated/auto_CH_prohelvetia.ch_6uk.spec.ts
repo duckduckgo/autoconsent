413bf0def8851fa228651c65f4b5fedf4eecc56a
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 12:25:17 +0200

    Revert "Add autoconsent rules for https://prohelvetia.ch/en/"
    This reverts commit f2ded58e777525577f7cfa0b5fb846fa482fce1b.

--- tests/generated/auto_CH_prohelvetia.ch_6uk.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_prohelvetia.ch_6uk', ['https://prohelvetia.ch/en/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['CH'],
-});
