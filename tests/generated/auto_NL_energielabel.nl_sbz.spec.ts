cfd5931ff17c73c7a9e3c5cd9ac286648e79aa38
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 15:13:24 +0200

    Revert "Add autoconsent rules for https://www.energielabel.nl/"
    This reverts commit a0e7270a05136fb2fbdc659d6c4f8e71244f0cbf.

--- tests/generated/auto_NL_energielabel.nl_sbz.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_energielabel.nl_sbz', ['https://www.energielabel.nl/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['NL'],
-});
