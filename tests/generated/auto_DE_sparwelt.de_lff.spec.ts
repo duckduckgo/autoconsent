00d5656fec457945e4d37533a2be2045d2ba1738
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 13:55:37 +0200

    remove rules for sparwelt

--- tests/generated/auto_DE_sparwelt.de_lff.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_sparwelt.de_lff', ['https://www.sparwelt.de/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
