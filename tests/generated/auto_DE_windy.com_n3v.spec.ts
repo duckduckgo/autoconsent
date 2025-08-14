9afa99c0056d8870b979bce7701c484cc14d4bb6
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 14:03:04 +0200

    clean up rules for windy.com

--- tests/generated/auto_DE_windy.com_n3v.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_windy.com_n3v', ['https://www.windy.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
