7a131c3f33d1b6221444b3948b238cfb073c0a63
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:25:39 +0200

    Revert "Add autoconsent rules for https://www.piecesauto.fr/"
    This reverts commit 24c83bd0e9a04d088288c097cb4bc13f5e16f718.

--- tests/generated/auto_FR_piecesauto.fr_pzk.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_FR_piecesauto.fr_pzk', ['https://www.piecesauto.fr/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['FR'],
-});
