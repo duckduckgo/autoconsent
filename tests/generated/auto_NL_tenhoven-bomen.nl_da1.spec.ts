96f5cc875391b17e3e7da200e2434e81f81b2711
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:37:20 +0200

    Revert "Add autoconsent rules for https://www.tenhoven-bomen.nl/"
    This reverts commit 2b92f060d23304f7f2482aa7090cce55598e60ac.

--- tests/generated/auto_NL_tenhoven-bomen.nl_da1.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_tenhoven-bomen.nl_da1', ['https://www.tenhoven-bomen.nl/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['NL'],
-});
