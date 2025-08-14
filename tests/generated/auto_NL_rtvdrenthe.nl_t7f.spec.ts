ba2d9d8f98c14def89300fa251a5923887ed5e97
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:25:39 +0200

    Revert "Add autoconsent rules for https://www.rtvdrenthe.nl/"
    This reverts commit 9501b91eaa2733ec6f0e46d8660b5a57caaed4c9.

--- tests/generated/auto_NL_rtvdrenthe.nl_t7f.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_rtvdrenthe.nl_t7f', ['https://www.rtvdrenthe.nl/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['NL'],
-});
