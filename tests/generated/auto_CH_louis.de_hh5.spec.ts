3721e34aeef5a281a67d9f0c1d5cb75a6fd01c88
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:09:20 +0200

    Revert "Add autoconsent rules for https://www.louis.de/"
    This reverts commit b7b0b76b5362bda58d1463a93b7aa2faa914c64c.

--- tests/generated/auto_CH_louis.de_hh5.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_louis.de_hh5', ['https://www.louis.de/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CH'] });
