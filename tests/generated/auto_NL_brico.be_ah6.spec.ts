01a657d42ef0d85a2211cf03493c32a0bbc60b71
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 13:05:58 +0200

    clean up brico rules

--- tests/generated/auto_NL_brico.be_ah6.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_brico.be_ah6', ['https://www.brico.be/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
