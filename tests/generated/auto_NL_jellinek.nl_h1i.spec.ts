1e1d2c06cf87675c7050ff65089af46b9f39a353
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:09:20 +0200

    Revert "Add autoconsent rules for https://www.jellinek.nl/"
    This reverts commit 033cdf286606fb4e0797c92d503cafc7892facfd.

--- tests/generated/auto_NL_jellinek.nl_h1i.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_jellinek.nl_h1i', ['https://www.jellinek.nl/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
