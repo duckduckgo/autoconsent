f74a934250756ecfe135f81b17d7be6736dd9422
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 14:27:06 +0200

    fix rules for consumentenbond

--- tests/generated/auto_NO_consumentenbond.nl_306.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NO_consumentenbond.nl_306', ['https://www.consumentenbond.nl/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['NO'],
-});
