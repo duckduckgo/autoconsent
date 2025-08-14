6039b2529538998ec73160c9c2eaf704bcc42f37
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 14:00:42 +0200

    fix rules for zonecouture

--- tests/generated/auto_FR_zonecouture.fr_vdh.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_FR_zonecouture.fr_vdh', ['https://www.zonecouture.fr/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['FR'],
-});
