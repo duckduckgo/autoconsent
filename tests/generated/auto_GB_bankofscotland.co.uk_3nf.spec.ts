0da64ab86f6531089ef7c25f5b061dd2b89a5fd2
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 14:39:31 +0200

    Revert "Add autoconsent rules for https://www.bankofscotland.co.uk/"
    This reverts commit ef88a2b2b828302203bcffbd1affa3b44055f59a.

--- tests/generated/auto_GB_bankofscotland.co.uk_3nf.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_bankofscotland.co.uk_3nf', ['https://www.bankofscotland.co.uk/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['GB'],
-});
