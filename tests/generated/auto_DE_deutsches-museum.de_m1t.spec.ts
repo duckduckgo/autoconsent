ebf98a6ddff719b370d1f76b5a273119f5ffafa6
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 13:08:12 +0200

    clean deutches-museum

--- tests/generated/auto_DE_deutsches-museum.de_m1t.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_deutsches-museum.de_m1t', ['https://www.deutsches-museum.de/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['DE'],
-});
