b85bc3bbeb66a2073e178da0e63473acb1360874
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 12:25:18 +0200

    Revert "Add autoconsent rules for https://travchat.fr/"
    This reverts commit b210fe356408b4522de08f46a5ad3a46a6c018a1.

--- tests/generated/auto_FR_travchat.fr_xk9.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_FR_travchat.fr_xk9', ['https://travchat.fr/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['FR'] });
