f56571de319938b0862eb12c2a9d67d8f5979dfc
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:25:39 +0200

    Revert "Add autoconsent rules for https://www.snsbank.nl/particulier/home.html"
    This reverts commit 64116a01fb7994c6a1c1d4fe8ae0457e5225608f.

--- tests/generated/auto_NL_snsbank.nl_rzl.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_snsbank.nl_rzl', ['https://www.snsbank.nl/particulier/home.html'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['NL'],
-});
