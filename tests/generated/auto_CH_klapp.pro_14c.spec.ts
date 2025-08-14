bbd358cc2ea361975298ee4e1d5f14abe029dc56
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:09:20 +0200

    Revert "Add autoconsent rules for https://www.klapp.pro/"
    This reverts commit 385817b0f307996cadfbc7edccd51fe45aef69a9.

--- tests/generated/auto_CH_klapp.pro_14c.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_klapp.pro_14c', ['https://www.klapp.pro/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CH'] });
