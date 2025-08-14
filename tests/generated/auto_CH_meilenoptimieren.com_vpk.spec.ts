9fb23d3547fa05efaaa6760e5db21289622bd171
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 12:25:17 +0200

    Revert "Add autoconsent rules for https://meilenoptimieren.com/"
    This reverts commit 9d3d5fb524550c69ec7347e4f67ca0927846b5b3.

--- tests/generated/auto_CH_meilenoptimieren.com_vpk.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_meilenoptimieren.com_vpk', ['https://meilenoptimieren.com/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['CH'],
-});
