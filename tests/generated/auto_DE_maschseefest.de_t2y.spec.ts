d07526e346b92bd922ff8dbd90517dcd25406b62
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-14 16:09:20 +0200

    Revert "Add autoconsent rules for https://www.maschseefest.de/"
    This reverts commit 1e9cc814f616967cc9b1765e7143d7e4ef48ba9d.

--- tests/generated/auto_DE_maschseefest.de_t2y.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_maschseefest.de_t2y', ['https://www.maschseefest.de/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['DE'],
-});
