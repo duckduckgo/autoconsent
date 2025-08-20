60c938e7bd4ee7f77b54e656f27d39cf266c53fb
Dax the Deployer <dax@duckduckgo.com>
2025-08-20 11:03:24 -0400

    Add autoconsent rules for https://bookclubs.com/

--- tests/generated/auto_US_bookclubs.com_gxf.spec.ts
+++ tests/generated/auto_US_bookclubs.com_gxf.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_US_bookclubs.com_gxf', ['https://bookclubs.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['US'] });
+generateCMPTests('auto_US_bookclubs.com_gxf', ['https://bookclubs.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['US'] });
