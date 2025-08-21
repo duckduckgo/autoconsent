4ed0ee7e21cd559ce1404bd9a2996ca471656f88
Sam Macbeth <smacbeth@duckduckgo.com>
2025-08-21 09:53:55 +0200

    Use reject-all button on iubenda form.

--- tests/iubenda.spec.ts
+++ tests/iubenda.spec.ts
@@ -1,3 +1,7 @@
 import generateCMPTests from '../playwright/runner';
 
-generateCMPTests('iubenda', ['https://www.rossignol.com/us/', 'https://www.lofficielusa.com/', 'https://www.3bmeteo.com/'], {});
+generateCMPTests(
+    'iubenda',
+    ['https://www.rossignol.com/us/', 'https://www.lofficielusa.com/', 'https://www.3bmeteo.com/', 'https://www.tuttoandroid.net/'],
+    {},
+);
