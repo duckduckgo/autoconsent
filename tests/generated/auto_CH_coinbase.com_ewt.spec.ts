e8223e604466cddaf2d7a43b70a5974cfd3b4ede
Sam Macbeth <smacbeth@duckduckgo.com>
2025-08-21 15:47:27 +0200

    Skip tests in AU

--- tests/generated/auto_CH_coinbase.com_ewt.spec.ts
+++ tests/generated/auto_CH_coinbase.com_ewt.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_coinbase.com_ewt', ['https://www.coinbase.com/'], { testOptIn: false, testSelfTest: true });
+generateCMPTests('auto_CH_coinbase.com_ewt', ['https://www.coinbase.com/'], { testOptIn: false, testSelfTest: true, skipRegions: ['AU'] });
