f893e2878161c7fb6ff7960c76a99fab3b305078
Sam Macbeth <sammacbeth@users.noreply.github.com>
2025-08-22 19:43:48 +0200

    Clean duplicated asus.com rules (#914)

--- tests/generated/auto_CA_asus.com_dm1.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CA_asus.com_dm1', ['https://www.asus.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['CA'] });
