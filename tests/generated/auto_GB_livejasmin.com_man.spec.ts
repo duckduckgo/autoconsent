39007a9d0d572004ad4204dd7c65bbbeb20a8041
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-25 16:31:38 +0200

    rename livejasmin rule

--- tests/generated/auto_GB_livejasmin.com_man.spec.ts
+++ tests/generated/auto_GB_livejasmin.com_man.spec.ts
@@ -1,5 +1,5 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('livejasmin.com', ['https://www.livejasmin.com/en/girls?forcedesktop=1'], {
+generateCMPTests('auto_GB_livejasmin.com_man', ['https://www.livejasmin.com/en/girls?forcedesktop=1'], {
     testOptIn: false,
     testSelfTest: true,
     onlyRegions: ['GB'],
