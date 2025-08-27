474fca5cdff0241456930d701f8941c8db946f3c
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-08-27 07:35:35 -0400

    add heuristic patterns for NL,FR,DE

--- lib/heuristics.ts
+++ lib/heuristics.ts
@@ -1,3 +1,4 @@
+// FIXME: the detection patterns are defined both in the crawl postprocessing codebase and here. We should consolidate them in one place.
 // TODO: check for false positive detections per pattern
 export const DETECT_PATTERNS = [
     /accept cookies/gi,
@@ -22,6 +23,70 @@ export const DETECT_PATTERNS = [
     // it might be tempting to add the patterns below, but they cause too many false positives. Don't do it :)
     // /cookies? settings/i,
     // /cookies? preferences/i,
+
+    // FR
+    /utilisons.*des.*cookies/gi,
+    /nous.*utilisons.*des/gi,
+    /des.*cookies.*pour/gi,
+    /des.*informations.*sur/gi,
+    /retirer.*votre.*consentement/gi,
+    /accéder.*à.*des/gi,
+    /à.*des.*informations/gi,
+    /et.*nos.*partenaires/gi,
+    /publicités.*et.*du.*contenu/gi,
+    /utilise.*des.*cookies/gi,
+    /utilisent.*des.*cookies/gi,
+    /stocker.*et.*ou.*accéder/gi,
+    /consentement.*à.*tout.*moment/gi,
+    /votre.*consentement/gi,
+    /accepter.*tout/gi,
+    /utilisation.*des.*cookies/gi,
+    /cookies.*ou.*technologies/gi,
+    /acceptez.*l.*utilisation/gi,
+    /continuer sans accepter/gi,
+    /tout refuser/gi,
+    /(refuser|rejeter) tous les cookies/gi,
+    /je refuse/gi,
+    /refuser et continuer/gi,
+    /refuser les cookies/gi,
+    /seulement nécessaires/gi,
+    /je désactive les finalités non essentielles/gi,
+    /cookies essentiels uniquement/gi,
+    /nécessaires uniquement/gi,
+
+    // DE
+    /wir.*verwenden.*cookies/gi,
+    /wir.*und.*unsere.*partner/gi,
+    /zugriff.*auf.*informationen.*auf/gi,
+    /inhalte.*messung.*von.*werbeleistung.*und/gi,
+    /cookies.*und.*andere/gi,
+    /verwendung.*von.*cookies/gi,
+    /wir.*nutzen.*cookies/gi,
+    /verwendet.*cookies/gi,
+    /sie.*können.*ihre.*auswahl/gi,
+    /und.*ähnliche.*technologien/gi,
+    /cookies.*wir.*verwenden/gi,
+
+    /alles?.*ablehnen/gi,
+    /(nur|nicht).*(zusätzliche|essenzielle|funktionale|notwendige|erforderliche).*(cookies|akzeptieren|erlauben|ablehnen)/gi,
+    /weiter.*(ohne|mit).*(einwilligung|zustimmung|cookies)/gi,
+    /(cookies|einwilligung).*ablehnen/gi,
+    /nur funktionale cookies akzeptieren/gi,
+    /optionale ablehnen/gi,
+    /zustimmung verweigern/gi,
+
+    // NL
+    /gebruik.*van.*cookies/gi,
+    /(we|wij).*gebruiken.*cookies.*om/gi,
+    /cookies.*en.*vergelijkbare/gi,
+
+    /(alles|cookies).*(afwijzen|weigeren|verwerpen)/gi,
+    /alleen.*noodzakelijke?/gi,
+    /cookies weigeren/gi,
+    /weiger.*(cookies|alles)/gi,
+    /doorgaan zonder (te accepteren|akkoord te gaan)/gi,
+    /alleen.*(optionele|functionele|functioneel|noodzakelijke|essentiële).*cookies/gi,
+    /wijs alles af/gi,
 ];
 
 export function checkHeuristicPatterns() {
