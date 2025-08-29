0e5995e7c02853dd7ee45a056c5c16914181b689
Maxim Tsoy <maks.tsoy@gmail.com>
2025-08-29 12:05:32 +0200

    update axeptio tests

--- tests/axeptio.spec.ts
+++ tests/axeptio.spec.ts
@@ -2,7 +2,7 @@ import generateCMPTests from '../playwright/runner';
 
 generateCMPTests('axeptio', [
     'https://www.welcometothejungle.com/',
-    'https://frichti.co/landing?redirectTo=/',
     'https://www.dorcel.com/',
     'https://mistral.ai/news/le-chat-dives-deep',
+    'https://lahalledesgourmets.com/pages/angelina',
 ]);
