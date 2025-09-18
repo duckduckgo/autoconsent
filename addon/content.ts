50f2cccf7bf709b16f9b31e92e2b33d91bb2cde0
Maxim Tsoy <maks.tsoy@gmail.com>
2025-09-18 17:03:15 +0200

    Exclude filterlist from the test extension

--- addon/content.ts
+++ addon/content.ts
@@ -1,5 +1,5 @@
 import { BackgroundMessage } from '../lib/messages';
-import AutoConsent from '../lib/web-extra';
+import AutoConsent from '../lib/web';
 
 const consent = new AutoConsent(chrome.runtime.sendMessage);
 chrome.runtime.onMessage.addListener((message: BackgroundMessage) => {
