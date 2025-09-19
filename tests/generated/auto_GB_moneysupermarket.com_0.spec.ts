cb7ab0f26b612757bfb1d241dcbe6dd1ce0297b4
Dax <dax@duckduckgo.com>
2025-09-19 01:41:04 -0400

    Add autoconsent rules for $'https://www.moneysupermarket.com/?feature=headerSearch' (#940)

--- tests/generated/auto_GB_moneysupermarket.com_0.spec.ts
+++ tests/generated/auto_GB_moneysupermarket.com_0.spec.ts
@@ -1,5 +1,5 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_moneysupermarket.com_0', ['https://www.moneysupermarket.com/'], {
+generateCMPTests('auto_GB_moneysupermarket.com_0', ['https://www.moneysupermarket.com/?feature=headerSearch'], {
     testOptIn: false,
     testSelfTest: true,
     onlyRegions: ['GB'],
