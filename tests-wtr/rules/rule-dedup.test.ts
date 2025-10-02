1e0921dc86a8b6055c0dc36a25f47f5d6fa5e6bc
Sam Macbeth <smacbeth@duckduckgo.com>
2025-10-02 12:28:02 +0200

    Update tests

--- tests-wtr/rules/rule-dedup.test.ts
+++ tests-wtr/rules/rule-dedup.test.ts
@@ -58,24 +58,33 @@ describe('deduplicateRules', () => {
         expect(urlPatternMatches('https://c.com/')).to.be.false;
     });
 
-    it('does not deduplicate non-simple rules', () => {
+    it('deduplicates complex rules', () => {
         const rules: AutoConsentCMPRule[] = [
             {
                 name: 'complex',
                 detectCmp: [{ exists: '#selector1' }],
                 detectPopup: [{ visible: '#selector1' }],
-                optOut: [{ click: '#selector1' }],
-                optIn: [],
+                optOut: [{ click: '#selector1' }, { waitForThenClick: '#selector2' }],
+                optIn: [{ click: '#selector2' }],
                 runContext: { urlPattern: 'a.com', main: true, frame: false },
+                prehideSelectors: ['#something'],
+            },
+            {
+                name: 'complex2',
+                detectCmp: [{ exists: '#selector1' }],
+                detectPopup: [{ visible: '#selector1' }],
+                optOut: [{ click: '#selector1' }, { waitForThenClick: '#selector2' }],
+                optIn: [{ click: '#selector2' }],
+                runContext: { urlPattern: 'b.com', main: true, frame: false },
                 prehideSelectors: ['#something'],
             },
         ];
         const deduped = deduplicateRules(rules);
         expect(deduped).to.have.length(1);
-        expect(deduped[0].name).to.equal('complex');
+        expect(deduped[0]._metadata?.deduplicatedFrom).to.deep.equal(['complex', 'complex2']);
     });
 
-    it('deduplicates only simple rules and keeps others', () => {
+    it('keeps other rules when deduplicating', () => {
         const rules: AutoConsentCMPRule[] = [
             {
                 name: 'rule1',
@@ -94,7 +103,7 @@ describe('deduplicateRules', () => {
                 runContext: { urlPattern: '^https?://(www\\.)?b\\.com/', main: true, frame: false },
             },
             {
-                name: 'complex',
+                name: 'rule3',
                 detectCmp: [{ exists: '#selector2' }],
                 detectPopup: [{ visible: '#selector2' }],
                 optOut: [{ waitForThenClick: '#selector2' }],
@@ -105,13 +114,55 @@ describe('deduplicateRules', () => {
         ];
         const deduped = deduplicateRules(rules);
         expect(deduped).to.have.length(2);
-        expect(deduped.some((r) => r.name === 'complex')).to.be.true;
+        expect(deduped.some((r) => r.name === 'rule3')).to.be.true;
         expect(deduped.some((r) => /^rule1_\+\d+$/.test(r.name))).to.be.true;
-        const combinedRule = deduped.find((r) => r.name !== 'complex');
+        const combinedRule = deduped.find((r) => r.name !== 'rule3');
         const urlPatternMatches = (url: string) => url.match(combinedRule!.runContext!.urlPattern!) !== null;
         expect(urlPatternMatches('http://www.a.com/')).to.be.true;
         expect(urlPatternMatches('https://b.com/')).to.be.true;
         expect(urlPatternMatches('https://c.com/')).to.be.false;
+    });
+
+    it('does not deduplicate generic rules (with no urlPattern)', () => {
+        const rules: AutoConsentCMPRule[] = [
+            {
+                name: 'generic1',
+                detectCmp: [{ exists: '#selector1' }],
+                detectPopup: [{ visible: '#selector1' }],
+                optOut: [{ waitForThenClick: '#selector1' }],
+                optIn: [],
+                runContext: { main: true, frame: false },
+            },
+            {
+                name: 'generic2',
+                detectCmp: [{ exists: '#selector1' }],
+                detectPopup: [{ visible: '#selector1' }],
+                optOut: [{ waitForThenClick: '#selector1' }],
+                optIn: [],
+                runContext: { main: true, frame: false },
+            },
+            {
+                name: 'specific1',
+                detectCmp: [{ exists: '#selector1' }],
+                detectPopup: [{ visible: '#selector1' }],
+                optOut: [{ waitForThenClick: '#selector1' }],
+                optIn: [],
+                runContext: { main: true, frame: false, urlPattern: 'b.com' },
+            },
+            {
+                name: 'specific2',
+                detectCmp: [{ exists: '#selector1' }],
+                detectPopup: [{ visible: '#selector1' }],
+                optOut: [{ waitForThenClick: '#selector1' }],
+                optIn: [],
+                runContext: { main: true, frame: false, urlPattern: 'a.com' },
+            },
+        ];
+        const deduped = deduplicateRules(rules);
+        expect(deduped).to.have.length(3);
+        expect(deduped.map((r) => r.name))
+            .to.include('generic1')
+            .and.include('generic2');
     });
 
     it('returns empty array for empty input', () => {
