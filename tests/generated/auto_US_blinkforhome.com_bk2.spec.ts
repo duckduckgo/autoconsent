a8df1d8c62b7fd90723fcebd920009bcc2fd5d55
Dax the Deployer <dax@duckduckgo.com>
2025-10-03 19:10:24 -0400

    Add autoconsent rules for https://blinkforhome.com/

--- tests/generated/auto_US_blinkforhome.com_bk2.spec.ts
+++ tests/generated/auto_US_blinkforhome.com_bk2.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_US_blinkforhome.com_bk2', ['https://blinkforhome.com/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['US'],
 });
