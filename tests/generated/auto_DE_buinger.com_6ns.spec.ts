63dbbe3a6b9006215e06c2f5f4211d30fcfe98c2
Dax the Deployer <dax@duckduckgo.com>
2025-10-03 19:10:25 -0400

    Add autoconsent rules for https://buinger.com/

---
+++ tests/generated/auto_DE_buinger.com_6ns.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_DE_buinger.com_6ns', ['https://buinger.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
