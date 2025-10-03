c6a7353d2f0c5ea2b0e1e1f46cece4da50f6bdcf
Dax the Deployer <dax@duckduckgo.com>
2025-10-03 19:11:43 -0400

    Add autoconsent rules for https://www.moaa.org/

---
+++ tests/generated/auto_US_moaa.org_bbb.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_US_moaa.org_bbb', ['https://www.moaa.org/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['US'] });
