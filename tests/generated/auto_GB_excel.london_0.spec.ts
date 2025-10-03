95faa8686ef739503b7cd30dc6ca609aa6e03401
Dax the Deployer <dax@duckduckgo.com>
2025-10-03 19:11:30 -0400

    Add autoconsent rules for https://www.excel.london/

--- tests/generated/auto_GB_excel.london_0.spec.ts
+++ tests/generated/auto_GB_excel.london_0.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_excel.london_0', ['https://www.excel.london/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
+generateCMPTests('auto_GB_excel.london_0', ['https://www.excel.london/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
