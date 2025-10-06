77484b501b2093c269c823441eb218fa8701cdf0
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-06 11:34:57 +0200

    Remove redundant rule

--- tests/generated/auto_AU_theoriginalshotels.com_5y8.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_AU_theoriginalshotels.com_5y8', ['https://www.theoriginalshotels.com/en/hotels/la-villa-vicha'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['AU'],
-});
