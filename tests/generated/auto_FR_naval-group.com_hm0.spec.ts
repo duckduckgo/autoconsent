1bf95988f69f1dd7d79b4aac3e12cee96f0b5f74
Dax the Deployer <dax@duckduckgo.com>
2025-10-07 08:07:34 -0400

    Add autoconsent rules for https://www.naval-group.com/en

---
+++ tests/generated/auto_FR_naval-group.com_hm0.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_FR_naval-group.com_hm0', ['https://www.naval-group.com/en'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['FR'],
+});
