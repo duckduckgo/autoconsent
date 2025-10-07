4d5cf9dedb2522b4ce84a8e1bd8a57deef8b5624
Dax the Deployer <dax@duckduckgo.com>
2025-10-07 08:07:27 -0400

    Add autoconsent rules for https://www.johdeheer.nl/

---
+++ tests/generated/auto_NL_johdeheer.nl_dxc.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_NL_johdeheer.nl_dxc', ['https://www.johdeheer.nl/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
