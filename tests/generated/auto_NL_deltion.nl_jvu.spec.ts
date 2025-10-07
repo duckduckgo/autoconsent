59cca6eb28588c696f70b1a2d85520c4241eef71
Dax the Deployer <dax@duckduckgo.com>
2025-10-07 08:06:59 -0400

    Add autoconsent rules for https://www.deltion.nl/

---
+++ tests/generated/auto_NL_deltion.nl_jvu.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_NL_deltion.nl_jvu', ['https://www.deltion.nl/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
