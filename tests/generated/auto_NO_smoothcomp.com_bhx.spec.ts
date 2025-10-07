bd4c9bc91ea0cf4c2c7fe37e688033910ee82596
Dax the Deployer <dax@duckduckgo.com>
2025-10-07 04:01:59 -0400

    Add autoconsent rules for https://smoothcomp.com/en

---
+++ tests/generated/auto_NO_smoothcomp.com_bhx.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_NO_smoothcomp.com_bhx', ['https://smoothcomp.com/en'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['NO'],
+});
