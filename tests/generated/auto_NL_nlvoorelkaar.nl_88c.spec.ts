057ef7d46831c9f1212ae2da1d0411007666bee5
Dax the Deployer <dax@duckduckgo.com>
2025-10-07 08:07:34 -0400

    Add autoconsent rules for https://www.nlvoorelkaar.nl/

---
+++ tests/generated/auto_NL_nlvoorelkaar.nl_88c.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_NL_nlvoorelkaar.nl_88c', ['https://www.nlvoorelkaar.nl/'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['NL'],
+});
