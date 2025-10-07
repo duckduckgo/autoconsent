8fb83724748fd5007f5662ab3ed54257ece023c6
Dax the Deployer <dax@duckduckgo.com>
2025-10-07 04:02:31 -0400

    Add autoconsent rules for https://www.swisswine.com/en

---
+++ tests/generated/auto_CH_swisswine.com_kp8.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_CH_swisswine.com_kp8', ['https://www.swisswine.com/en'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['CH'],
+});
