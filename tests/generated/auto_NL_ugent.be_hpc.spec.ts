8ea24e32db2c55bc393443a622be86f0abc2c59c
Dax the Deployer <dax@duckduckgo.com>
2025-10-07 04:02:32 -0400

    Add autoconsent rules for https://www.ugent.be/

---
+++ tests/generated/auto_NL_ugent.be_hpc.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_NL_ugent.be_hpc', ['https://www.ugent.be/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
