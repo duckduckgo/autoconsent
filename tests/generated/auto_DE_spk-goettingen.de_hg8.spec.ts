d40770a3306ae2be6e83831fa1a5873fc29b0b9a
Dax the Deployer <dax@duckduckgo.com>
2025-10-07 08:08:02 -0400

    Add autoconsent rules for https://www.spk-goettingen.de/de/home.html

---
+++ tests/generated/auto_DE_spk-goettingen.de_hg8.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_DE_spk-goettingen.de_hg8', ['https://www.spk-goettingen.de/de/home.html'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['DE'],
+});
