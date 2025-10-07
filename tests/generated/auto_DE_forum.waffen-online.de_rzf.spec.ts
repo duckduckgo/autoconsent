74070675cc4406206127cfe6094bdecb4af96565
Dax the Deployer <dax@duckduckgo.com>
2025-10-07 04:01:55 -0400

    Add autoconsent rules for https://forum.waffen-online.de/

---
+++ tests/generated/auto_DE_forum.waffen-online.de_rzf.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_DE_forum.waffen-online.de_rzf', ['https://forum.waffen-online.de/'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['DE'],
+});
