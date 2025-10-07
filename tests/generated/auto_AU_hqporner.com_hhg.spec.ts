e14c5bbfe79835bbe674a76e5a2615dcd822b915
Dax the Deployer <dax@duckduckgo.com>
2025-10-07 08:06:22 -0400

    Add autoconsent rules for https://hqporner.com/

---
+++ tests/generated/auto_AU_hqporner.com_hhg.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_AU_hqporner.com_hhg', ['https://hqporner.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['AU'] });
