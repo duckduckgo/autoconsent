c0b273e0c49e918dea4309db94c543cf92542def
Dax the Deployer <dax@duckduckgo.com>
2025-10-07 04:02:27 -0400

    Add autoconsent rules for https://www.livejasmin.com/en/girls

--- tests/generated/auto_GB_livejasmin.com_man.spec.ts
+++ tests/generated/auto_GB_livejasmin.com_man.spec.ts
@@ -1,5 +1,5 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_livejasmin.com_man', ['https://www.livejasmin.com/en/girls?forcedesktop=1'], {
+generateCMPTests('auto_GB_livejasmin.com_man', ['https://www.livejasmin.com/en/girls'], {
     testOptIn: false,
     testSelfTest: true,
     onlyRegions: ['GB'],
