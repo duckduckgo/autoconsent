0aabf1e8f91de685380903b0b4d826d902ded1d6
Dax the Deployer <dax@duckduckgo.com>
2025-10-07 08:08:09 -0400

    Add autoconsent rules for https://www.y4kconvert.com/

---
+++ tests/generated/auto_AU_y4kconvert.com_pf1.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_AU_y4kconvert.com_pf1', ['https://www.y4kconvert.com/'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['AU'],
+});
