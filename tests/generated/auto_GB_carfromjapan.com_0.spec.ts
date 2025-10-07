bd168bd9f662ba4653bdd7d5621846fceb34a32e
Dax the Deployer <dax@duckduckgo.com>
2025-10-07 04:01:53 -0400

    Add autoconsent rules for https://carfromjapan.com/

--- tests/generated/auto_GB_carfromjapan.com_0.spec.ts
+++ tests/generated/auto_GB_carfromjapan.com_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_carfromjapan.com_0', ['https://carfromjapan.com/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
