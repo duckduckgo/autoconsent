9dfbe5a6caa2c7ed87471d890dcb26bf925bef51
Dax the Deployer <dax@duckduckgo.com>
2025-10-07 04:02:04 -0400

    Add autoconsent rules for https://www.engadget.com/

--- tests/generated/auto_GB_engadget.com_0.spec.ts
+++ tests/generated/auto_GB_engadget.com_0.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_engadget.com_0', ['https://www.engadget.com/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
+generateCMPTests('auto_GB_engadget.com_0', ['https://www.engadget.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
