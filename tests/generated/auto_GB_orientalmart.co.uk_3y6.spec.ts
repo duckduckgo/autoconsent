da0e57fbfc619af19aef80d65d5ca3d6a05c99b7
Dax the Deployer <dax@duckduckgo.com>
2025-10-07 08:07:36 -0400

    Add autoconsent rules for https://www.orientalmart.co.uk/

---
+++ tests/generated/auto_GB_orientalmart.co.uk_3y6.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_GB_orientalmart.co.uk_3y6', ['https://www.orientalmart.co.uk/'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['GB'],
+});
