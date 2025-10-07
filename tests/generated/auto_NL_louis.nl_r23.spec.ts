2636c203ac616c9c79cbac932387c46be155b339
Dax the Deployer <dax@duckduckgo.com>
2025-10-07 08:07:31 -0400

    Add autoconsent rules for https://www.louis.nl/nl

---
+++ tests/generated/auto_NL_louis.nl_r23.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_NL_louis.nl_r23', ['https://www.louis.nl/nl'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
