381d2f5e6307aef0d7c6cc5f23876989f5769de3
Dax the Deployer <dax@duckduckgo.com>
2025-10-07 04:02:25 -0400

    Add autoconsent rules for https://www.gymglish.com/en

---
+++ tests/generated/auto_FR_gymglish.com_eiv.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_FR_gymglish.com_eiv', ['https://www.gymglish.com/en'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['FR'],
+});
