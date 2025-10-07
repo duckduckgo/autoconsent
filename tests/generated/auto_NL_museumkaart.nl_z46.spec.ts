445e967d93cdd797c3ab0b72eead5ff2ea4b73bb
Dax the Deployer <dax@duckduckgo.com>
2025-10-07 04:02:28 -0400

    Add autoconsent rules for https://www.museum.nl/en

--- tests/generated/auto_NL_museumkaart.nl_z46.spec.ts
+++ tests/generated/auto_NL_museumkaart.nl_z46.spec.ts
@@ -1,6 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_museumkaart.nl_z46', ['https://www.museum.nl/nl/museumkaart'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['NL'],
-});
+generateCMPTests('auto_NL_museumkaart.nl_z46', ['https://www.museum.nl/en'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
