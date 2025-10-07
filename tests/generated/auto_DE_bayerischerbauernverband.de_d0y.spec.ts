3d1fd3f9688111ec3781cca43b8b071856af4e9a
Dax the Deployer <dax@duckduckgo.com>
2025-10-07 08:06:54 -0400

    Add autoconsent rules for $'https://www.bayerischerbauernverband.de/?lat=50.1109221&long=8.6821267'

---
+++ tests/generated/auto_DE_bayerischerbauernverband.de_d0y.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_DE_bayerischerbauernverband.de_d0y', ['https://www.bayerischerbauernverband.de/?lat=50.1109221&long=8.6821267'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['DE'],
+});
