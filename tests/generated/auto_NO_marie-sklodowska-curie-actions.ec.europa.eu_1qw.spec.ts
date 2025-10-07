e1c37d51af09ae10963e0e7043f5000c9fe2e116
Dax the Deployer <dax@duckduckgo.com>
2025-10-07 04:01:57 -0400

    Add autoconsent rules for https://marie-sklodowska-curie-actions.ec.europa.eu/

---
+++ tests/generated/auto_NO_marie-sklodowska-curie-actions.ec.europa.eu_1qw.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_NO_marie-sklodowska-curie-actions.ec.europa.eu_1qw', ['https://marie-sklodowska-curie-actions.ec.europa.eu/'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['NO'],
+});
