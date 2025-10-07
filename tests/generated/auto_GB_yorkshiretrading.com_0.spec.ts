e3d579cd8a875c883cd235474bcebbbcf90b19a2
Dax the Deployer <dax@duckduckgo.com>
2025-10-07 08:08:09 -0400

    Add autoconsent rules for https://www.yorkshiretrading.com/

--- tests/generated/auto_GB_yorkshiretrading.com_0.spec.ts
+++ tests/generated/auto_GB_yorkshiretrading.com_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_yorkshiretrading.com_0', ['https://www.yorkshiretrading.com/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
