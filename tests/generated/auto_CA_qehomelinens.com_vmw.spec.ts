d0edce4f677a8cc6a17d4115534b15e4db2baae0
Dax the Deployer <dax@duckduckgo.com>
2025-10-07 04:02:30 -0400

    Add autoconsent rules for https://www.qehomelinens.com/

---
+++ tests/generated/auto_CA_qehomelinens.com_vmw.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_CA_qehomelinens.com_vmw', ['https://www.qehomelinens.com/'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['CA'],
+});
