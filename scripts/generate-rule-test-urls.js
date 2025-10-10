83a4e447385fe27e311bdbf112e66899f9badd81
Sam Macbeth <smacbeth@duckduckgo.com>
2025-10-10 16:38:12 +0200

    Add tests for rule filtering

---
+++ scripts/generate-rule-test-urls.js
@@ -0,0 +1,20 @@
+const RandExp = require('randexp');
+const fs = require('fs');
+const compactRules = require('../rules/compact-rules.json');
+
+const testUrlMap = [];
+
+compactRules.r.forEach(rule => {
+    const urlPattern = rule[3];
+    if (urlPattern.length === 0) {
+        return;
+    }
+    try {
+        const testUrl = new URL(RandExp.randexp(urlPattern)).href;
+        testUrlMap.push([rule[1], testUrl]);
+    } catch (error) {
+        console.log('Error generating URL for pattern:', urlPattern, error.input);
+    }
+});
+fs.writeFileSync('./tests-wtr/rules/cmp-test-urls.json', JSON.stringify(testUrlMap, null, 2));
+console.log(`Generated ${testUrlMap.length} test URLs and saved to cmp-test-urls.json`);
