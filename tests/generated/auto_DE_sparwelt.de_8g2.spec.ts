dbdc1405a6d1bb8fcb47883eadd14d0485a3323d
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-12 21:09:31 +0200

    Clean up erroneous rules

--- tests/generated/auto_DE_sparwelt.de_8g2.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_sparwelt.de_8g2', ['https://www.sparwelt.de/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
--- tests/generated/auto_NL_winkelstraat.nl_ma7.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_winkelstraat.nl_ma7', ['https://www.winkelstraat.nl/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['NL'],
-});
