626d30814e41e00ab0247c4da9c93d8f43353e69
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-12 22:18:00 +0200

    clean up ionos rule

--- tests/generated/auto_GB_ionos.co.uk_fz1.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_ionos.co.uk_fz1', ['https://www.ionos.co.uk/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
