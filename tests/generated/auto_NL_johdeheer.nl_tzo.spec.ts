434155e5feb32253cec386aef4bac40b8a3108f6
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-12 22:17:48 +0200

    Revert "Add autoconsent rules for https://www.johdeheer.nl/"
    This reverts commit 7461c2d3ca80eec1052c770a74823c1021624941.

--- tests/generated/auto_NL_johdeheer.nl_tzo.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_johdeheer.nl_tzo', ['https://www.johdeheer.nl/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
