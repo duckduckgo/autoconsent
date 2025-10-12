0a3b3165db04727fe0177306520f066f5a233d5b
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-12 21:32:52 +0200

    Revert "Add autoconsent rules for https://www.schweiztipps.ch/"
    This reverts commit 7f93f47f575aadb9c585f64d121dcb1d16a9f37f.

--- tests/generated/auto_CH_schweiztipps.ch_r3x.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_schweiztipps.ch_r3x', ['https://www.schweiztipps.ch/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['CH'],
-});
