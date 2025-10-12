b1ddb32f5e86521933270d16b1d4687eb5ae298f
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-12 21:32:53 +0200

    Revert "Add autoconsent rules for https://www.regiobank.nl/home.html"
    This reverts commit f804276d3100883b9cabd8adc36e1cd2e253f84f.

--- tests/generated/auto_NL_regiobank.nl_kmq.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_regiobank.nl_kmq', ['https://www.regiobank.nl/home.html'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['NL'],
-});
