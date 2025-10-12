0d974e1742c0b31385f20ad6121f1d88a9c7b478
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-12 21:32:53 +0200

    Revert "Add autoconsent rules for https://www.telefoonhoesje.nl/"
    This reverts commit f02b1f9715827061ac436cda180af7aae113e90e.

--- tests/generated/auto_NL_telefoonhoesje.nl_p1j.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_telefoonhoesje.nl_p1j', ['https://www.telefoonhoesje.nl/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['NL'],
-});
