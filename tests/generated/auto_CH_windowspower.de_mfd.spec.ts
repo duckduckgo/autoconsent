a9ce44f373b5ec04c114a92de87da6181648f718
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-12 21:32:52 +0200

    Revert "Add autoconsent rules for https://www.windowspower.de/"
    This reverts commit 8d50cb4ef8f077c5903131b24da9847732824de1.

--- tests/generated/auto_CH_windowspower.de_mfd.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_windowspower.de_mfd', ['https://www.windowspower.de/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['CH'],
-});
