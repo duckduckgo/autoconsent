8c9a0b92f1da0dcf9e8e2b5d7fee80f0ba397af3
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-12 21:53:45 +0200

    Add rule for real-cookie-banner

---
+++ tests/real-cookie-banner.spec.ts
@@ -0,0 +1,7 @@
+import generateCMPTests from "../playwright/runner";
+
+generateCMPTests('real-cookie-banner', [
+  'https://botschaft-konsulat.de/',
+  'https://www.schweiztipps.ch/',
+  'https://www.windowspower.de/',
+]);
