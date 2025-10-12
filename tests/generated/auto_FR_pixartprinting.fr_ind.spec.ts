8afd38a3c96070aa0692592894fe0f968c3df9ec
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-12 21:32:53 +0200

    Revert "Add autoconsent rules for https://www.pixartprinting.fr/"
    This reverts commit 685b763211fc35e34a14487e62e7768bc1867935.

--- tests/generated/auto_FR_pixartprinting.fr_ind.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_FR_pixartprinting.fr_ind', ['https://www.pixartprinting.fr/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['FR'],
-});
