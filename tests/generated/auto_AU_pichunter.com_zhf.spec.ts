f6b8a9228237e9f301d5c7c1298c8fbf1f1d0fee
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-12 21:32:53 +0200

    Revert "Add autoconsent rules for https://www.pichunter.com/"
    This reverts commit bc9b065568899065e68b554fc07e41e60297b7e8.

--- tests/generated/auto_AU_pichunter.com_zhf.spec.ts
+++ tests/generated/auto_AU_pichunter.com_zhf.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_AU_pichunter.com_zhf', ['https://www.pichunter.com/'], {
     testOptIn: false,
-    testSelfTest: true,
+    testSelfTest: false,
     onlyRegions: ['AU'],
 });
