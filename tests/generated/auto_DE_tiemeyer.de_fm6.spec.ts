0c64ad237559508295837c6da109001f754ab99b
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-12 21:32:52 +0200

    Revert "Add autoconsent rules for https://www.tiemeyer.de/"
    This reverts commit 3584e16fd6830d92df45a7339b84d739c4057612.

--- tests/generated/auto_DE_tiemeyer.de_fm6.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_tiemeyer.de_fm6', ['https://www.tiemeyer.de/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
