6e421829e8c69167f3a34ddf379c383eef1628f2
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 09:47:31 +0200

    Revert "Add autoconsent rules for https://www.compact-online.de/"
    This reverts commit d4a8bcd5bab6540b85f728efc4972ca3b7fe95cc.

--- tests/generated/auto_DE_compact-online.de_r6l.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_compact-online.de_r6l', ['https://www.compact-online.de/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['DE'],
-});
