079a506d19803faefef41e7082df67c6aae564b6
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 09:42:03 +0200

    Revert "Add autoconsent rules for https://www.bitget.com/"
    This reverts commit 14e242148300c70c3d39a773def0a285f4716ab4.

--- tests/generated/auto_CH_bitget.com_so5.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_bitget.com_so5', ['https://www.bitget.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CH'] });
