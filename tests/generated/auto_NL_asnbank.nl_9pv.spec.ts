3f29db3a195bf2cb5e92c547ee913d68cad75389
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 09:47:32 +0200

    Revert "Add autoconsent rules for https://www.asnbank.nl/home.html"
    This reverts commit cb2c30eda257fe8a6201369c6f5aaf80fecb4762.

--- tests/generated/auto_NL_asnbank.nl_9pv.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_asnbank.nl_9pv', ['https://www.asnbank.nl/home.html'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['NL'],
-});
