1afa9ed7b171b6319999ccf71eb79295648da72b
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 09:47:32 +0200

    Revert "Add autoconsent rules for https://www.elkerliek.nl/"
    This reverts commit 9a09fdc491dec450e7b38babad69a2841fdc01c0.

--- tests/generated/auto_NL_elkerliek.nl_wm3.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_elkerliek.nl_wm3', ['https://www.elkerliek.nl/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
