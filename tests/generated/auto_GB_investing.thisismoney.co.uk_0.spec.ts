e4ea202c96549bf8ba7a1902d7d04baa52ddd54c
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 10:54:00 +0200

    Revert "Add autoconsent rules for https://investing.thisismoney.co.uk/companies/A"
    This reverts commit c0e246c7a5f097ab58ea03b7299ebd330d087d09.

--- tests/generated/auto_GB_investing.thisismoney.co.uk_0.spec.ts
+++ tests/generated/auto_GB_investing.thisismoney.co.uk_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_investing.thisismoney.co.uk_0', ['https://investing.thisismoney.co.uk/companies/A'], {
     testOptIn: false,
-    testSelfTest: true,
+    testSelfTest: false,
     onlyRegions: ['GB'],
 });
