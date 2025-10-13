00a68f8342eee5313a641a497d6942b2ba889217
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 09:47:31 +0200

    Revert "Add autoconsent rules for https://tuhlteim.de/"
    This reverts commit 1f8b34da0a2863290a829643136ab39ededdc323.

--- tests/generated/auto_DE_tuhlteim.de_11d.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_tuhlteim.de_11d', ['https://tuhlteim.de/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
