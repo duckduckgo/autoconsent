395e6bba9b339a9e389d4e14bd926e341c8872d1
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 10:54:00 +0200

    Revert "Add autoconsent rules for https://bommelwereld.nl/"
    This reverts commit 667659e8ea7dce5d54c52e7e67dc42866b569c6e.

--- tests/generated/auto_NL_bommelwereld.nl_ty9.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_bommelwereld.nl_ty9', ['https://bommelwereld.nl/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['NL'],
-});
