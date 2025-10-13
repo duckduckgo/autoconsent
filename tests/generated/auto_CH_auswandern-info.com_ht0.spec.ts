76cc3c81cc2e21adf892a364021392d0a1c7413d
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 10:53:58 +0200

    Revert "Add autoconsent rules for https://auswandern-info.com/"
    This reverts commit 1259f52b30b7c163b6fb237eb74d9a82bd13446f.

--- tests/generated/auto_CH_auswandern-info.com_ht0.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_auswandern-info.com_ht0', ['https://auswandern-info.com/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['CH'],
-});
