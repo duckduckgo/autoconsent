8758c1280d1b06c0241518cd549eba625f171f64
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 09:47:31 +0200

    Revert "Add autoconsent rules for https://smarthome-assistant.info/"
    This reverts commit da19f3a0b3ccb7f7891707c895920d119e838d9e.

--- tests/generated/auto_DE_smarthome-assistant.info_ngg.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_smarthome-assistant.info_ngg', ['https://smarthome-assistant.info/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['DE'],
-});
