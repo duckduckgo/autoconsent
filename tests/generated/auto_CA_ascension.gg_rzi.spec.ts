684dbc26c1429ce6b07635169f88b6e698302b77
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 10:53:58 +0200

    Revert "Add autoconsent rules for https://ascension.gg/en"
    This reverts commit 14f84bffa00e18a30766048b89fed8cb5815457c.

--- tests/generated/auto_CA_ascension.gg_rzi.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CA_ascension.gg_rzi', ['https://ascension.gg/en'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CA'] });
