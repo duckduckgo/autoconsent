9b7370f066611e34adb1739b555e3d62f6a8dd3e
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 10:54:00 +0200

    Revert "Add autoconsent rules for https://france-visas.gouv.fr/"
    This reverts commit fd4233121d1faacf8301fead0473386caeb33982.

--- tests/generated/auto_FR_france-visas.gouv.fr_8xq.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_FR_france-visas.gouv.fr_8xq', ['https://france-visas.gouv.fr/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['FR'],
-});
