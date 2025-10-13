4edf290d74dcb3e8396162a6fb34387a51565eeb
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 10:53:58 +0200

    Revert "Add autoconsent rules for https://ihjo.de/"
    This reverts commit 2b9159aab5757f7617a2eb2fdca52e673ab906cd.

--- tests/generated/auto_CH_ihjo.de_xlb.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_ihjo.de_xlb', ['https://ihjo.de/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CH'] });
