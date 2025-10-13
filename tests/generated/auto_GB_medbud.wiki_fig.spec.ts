5e680fed406564a6750242bb513f254fca0261dd
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 10:54:00 +0200

    Revert "Add autoconsent rules for https://medbud.wiki/"
    This reverts commit ababb23a79234c94bed317365b8d2de816c4be39.

--- tests/generated/auto_GB_medbud.wiki_fig.spec.ts
+++ tests/generated/auto_GB_medbud.wiki_fig.spec.ts
@@ -1,2 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_medbud.wiki_fig', ['https://medbud.wiki/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
+generateCMPTests('auto_GB_medbud.wiki_fig', ['https://medbud.wiki/'], { testOptIn: false, testSelfTest: false, onlyRegions: ['GB'] });
