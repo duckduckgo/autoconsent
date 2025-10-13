330f5dd73140bae4f35c4bfa021a31f487a82e53
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 10:53:58 +0200

    Revert "Add autoconsent rules for https://beamten-infoportal.de/"
    This reverts commit aa20ac99d81f7eedf268daee5ff1e38addf8ae4e.

--- tests/generated/auto_DE_beamten-infoportal.de_49t.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_beamten-infoportal.de_49t', ['https://beamten-infoportal.de/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['DE'],
-});
