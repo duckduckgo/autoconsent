4a396e96cfa37f7868ea0920fd96ab2039e86a47
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 08:50:38 +0200

    Revert "Add autoconsent rules for https://www.livejasmin.com/en/girls"
    This reverts commit 47327630c04f0f9e6bf043895314d480564f6def.

--- tests/generated/auto_GB_livejasmin.com_man.spec.ts
+++ tests/generated/auto_GB_livejasmin.com_man.spec.ts
@@ -1,5 +1,5 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_livejasmin.com_man', ['https://www.livejasmin.com/en/girls'], {
+generateCMPTests('auto_GB_livejasmin.com_man', ['https://www.livejasmin.com/en/girls?forcedesktop=1'], {
     testOptIn: false,
     testSelfTest: true,
     onlyRegions: ['GB'],
