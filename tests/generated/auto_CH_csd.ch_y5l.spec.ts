74db450858769e6928f5837f8d773a17d837b1e7
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 09:47:32 +0200

    Revert "Add autoconsent rules for https://www.csd.ch/en"
    This reverts commit 69b41b6742a809df969e89dbd7a243f25a1a8d42.

--- tests/generated/auto_CH_csd.ch_y5l.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_csd.ch_y5l', ['https://www.csd.ch/en'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CH'] });
