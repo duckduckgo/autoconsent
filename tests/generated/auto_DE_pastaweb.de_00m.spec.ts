0696b9b44027f2b73434d89f77f333be4601575c
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 10:53:59 +0200

    Revert "Add autoconsent rules for https://pastaweb.de/"
    This reverts commit 8363968e15be3cee2bd36f527bdd4a1649477708.

--- tests/generated/auto_DE_pastaweb.de_00m.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_pastaweb.de_00m', ['https://pastaweb.de/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
