dbaabeec930903f2b5972a8d63eae1299c88e6b4
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 09:47:32 +0200

    Revert "Add autoconsent rules for https://www.blgwonen.nl/home.html"
    This reverts commit 3eb8de30a6b2cb2679dba48470c34a1190ca4590.

--- tests/generated/auto_NL_blgwonen.nl_8ac.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_blgwonen.nl_8ac', ['https://www.blgwonen.nl/home.html'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['NL'],
-});
