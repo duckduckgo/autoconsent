f7ccb85a17504ee69f118700863c19a4627cc7e3
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 09:47:32 +0200

    Revert "Add autoconsent rules for https://www.6relax.de/"
    This reverts commit 9d26ae67ccc4fc173b8bcc8f40934ede24535ad8.

--- tests/generated/auto_DE_6relax.de_dsr.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_6relax.de_dsr', ['https://www.6relax.de/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
