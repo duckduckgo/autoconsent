190e73567d63cafb2a185fc1ab584f7d6ac484bf
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 09:47:31 +0200

    Revert "Add autoconsent rules for https://www.abacus.ch/en"
    This reverts commit 79555ad5ef8484fb98e5691f04dab51b9959a842.

--- tests/generated/auto_CH_abacus.ch_ltb.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_abacus.ch_ltb', ['https://www.abacus.ch/en'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CH'] });
