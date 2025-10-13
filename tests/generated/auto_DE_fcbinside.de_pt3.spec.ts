1b34aa0ee87e6b0e3eea78ed1d5e04b30ef83abc
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 10:53:59 +0200

    Revert "Add autoconsent rules for https://fcbinside.de/"
    This reverts commit df7eef6a88a82d687aa4370dc84ec44663f5e78d.

--- tests/generated/auto_DE_fcbinside.de_pt3.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_fcbinside.de_pt3', ['https://fcbinside.de/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
