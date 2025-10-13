24431f9670d21694889a27536d1e3ded0c238839
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 09:16:42 +0200

    Revert "Add autoconsent rules for https://www.netze-bw.de/"
    This reverts commit 879da154317a1288cddf25c975d33efb8168d1d8.

--- tests/generated/auto_DE_netze-bw.de_lbr.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_netze-bw.de_lbr', ['https://www.netze-bw.de/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
