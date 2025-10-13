cf6326f07ee038cdf224068761335930b68b9f6a
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 09:16:42 +0200

    Revert "Add autoconsent rules for https://www.klinikkompass.com/"
    This reverts commit 42a98de0512a9076eb1e264d59550f0b47e9c285.

--- tests/generated/auto_DE_klinikkompass.com_y6o.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_klinikkompass.com_y6o', ['https://www.klinikkompass.com/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['DE'],
-});
