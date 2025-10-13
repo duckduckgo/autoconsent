86bbc45153a61887f132d045ae79ae3e9a7668a1
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 09:47:31 +0200

    Revert "Add autoconsent rules for https://robert-franz-naturprodukte.de/"
    This reverts commit 5d3ab17d3fce00fbeb70019b9d08ae71405984fe.

--- tests/generated/auto_DE_robert-franz-naturprodukte.de_nim.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_robert-franz-naturprodukte.de_nim', ['https://robert-franz-naturprodukte.de/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['DE'],
-});
