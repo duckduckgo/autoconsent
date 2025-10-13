cf8050f6fa198e3e86de591c7c22e63665048c4e
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 10:53:59 +0200

    Revert "Add autoconsent rules for https://fr.bongacams.com/"
    This reverts commit 0d4271a38a6056e7f74c3c5ae8c3f9b34c7b714f.

--- tests/generated/auto_FR_fr.bongacams.com_epo.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_FR_fr.bongacams.com_epo', ['https://fr.bongacams.com/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['FR'],
-});
