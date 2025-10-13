ead9ec23574b9261543140deb9db89d62874cad2
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 09:47:31 +0200

    Revert "Add autoconsent rules for https://www.carolin-escort.de/"
    This reverts commit 5e6ca722c30f254da20807db7575f08fbf4a2184.

--- tests/generated/auto_DE_carolin-escort.de_tdj.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_carolin-escort.de_tdj', ['https://www.carolin-escort.de/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['DE'],
-});
