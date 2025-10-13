23adfe97af4d3f064afe44804468c0e942c6f9c1
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 09:15:29 +0200

    Revert "Add autoconsent rules for https://www.hansgrohe.de/"
    This reverts commit 72c167eb1e50b03cfbf6e0d3647675b17c99fc49.

--- tests/generated/auto_DE_hansgrohe.de_i08.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_hansgrohe.de_i08', ['https://www.hansgrohe.de/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
