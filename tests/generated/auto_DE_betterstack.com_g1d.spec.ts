66aba238c4044f24eb2706dc4924ae1bcd88e787
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 10:22:19 +0200

    remove notworking rule

--- tests/generated/auto_DE_betterstack.com_g1d.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_betterstack.com_g1d', ['https://betterstack.com/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['DE'],
-});
