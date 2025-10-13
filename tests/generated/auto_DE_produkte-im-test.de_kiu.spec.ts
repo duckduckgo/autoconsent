42475c57e9c196b8414a4ffcd8d64369dd013da4
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 10:53:59 +0200

    Revert "Add autoconsent rules for https://produkte-im-test.de/"
    This reverts commit 8f09575f992a0fd8fe306ec9f336d28f2f51041b.

--- tests/generated/auto_DE_produkte-im-test.de_kiu.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_produkte-im-test.de_kiu', ['https://produkte-im-test.de/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['DE'],
-});
