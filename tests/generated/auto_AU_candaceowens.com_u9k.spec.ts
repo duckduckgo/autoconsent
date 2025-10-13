590543a79c7e9108a5e0c4ceb0506223b7b4d308
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 10:23:47 +0200

    Revert "Add autoconsent rules for https://candaceowens.com/"
    This reverts commit be3038b2811acf4b1c170a07da78d7568acb5560.

--- tests/generated/auto_AU_candaceowens.com_u9k.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_AU_candaceowens.com_u9k', ['https://candaceowens.com/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['AU'],
-});
