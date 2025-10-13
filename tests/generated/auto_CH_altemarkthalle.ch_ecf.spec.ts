d78c7b8ddaf7d9d479af754e5f4ed0fad2f38c23
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 10:53:58 +0200

    Revert "Add autoconsent rules for https://altemarkthalle.ch/"
    This reverts commit 28fc60dc8b56c2d9ba38f21521688276ee4a4cd6.

--- tests/generated/auto_CH_altemarkthalle.ch_ecf.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_altemarkthalle.ch_ecf', ['https://altemarkthalle.ch/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['CH'],
-});
