bdf693cf74fcd8c5025dce23dd628d39ccf9b4f5
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 10:54:00 +0200

    Revert "Add autoconsent rules for https://fleshbot.com/"
    This reverts commit 14ec86549d840d0a1121899676af24022939313c.

--- tests/generated/auto_AU_fleshbot.com_jm3.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_AU_fleshbot.com_jm3', ['https://fleshbot.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['AU'] });
