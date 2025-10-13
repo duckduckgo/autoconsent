12e60d271fa67173f36f8a8b06a54fd052847f90
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 09:05:19 +0200

    Fix rule for fresha.com

--- tests/generated/auto_FR_fresha.com_ekk.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_FR_fresha.com_ekk', ['https://www.fresha.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['FR'] });
--- tests/generated/auto_NL_fresha.com_4l5.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_fresha.com_4l5', ['https://www.fresha.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
