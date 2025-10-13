fe14efb84bc31f6a112a435d774b46d9ebb83215
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 10:54:00 +0200

    Revert "Add autoconsent rules for https://forum.prusa3d.com/"
    This reverts commit 0b0ff37c37003d538969e45e16f55babe09a33a3.

--- tests/generated/auto_CA_forum.prusa3d.com_zco.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CA_forum.prusa3d.com_zco', ['https://forum.prusa3d.com/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['CA'],
-});
