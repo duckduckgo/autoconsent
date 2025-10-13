bd424f9935a8877ed019d73c5b6e45cfe4bfe03a
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 10:53:59 +0200

    Revert "Add autoconsent rules for https://goneuland.de/"
    This reverts commit 2d070be02b4deb75bdf9790b02e333592a6044c2.

--- tests/generated/auto_DE_goneuland.de_wem.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_goneuland.de_wem', ['https://goneuland.de/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
