88f96415329bb19c5ec101f827cb673944bdd242
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 09:47:31 +0200

    Revert "Add autoconsent rules for https://rentenbescheid24.de/"
    This reverts commit 080e05f99f42363f62263391035c146c69e0b175.

--- tests/generated/auto_DE_rentenbescheid24.de_7bn.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_rentenbescheid24.de_7bn', ['https://rentenbescheid24.de/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['DE'],
-});
