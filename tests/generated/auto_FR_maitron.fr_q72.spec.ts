dbba018f0eee26c7f8dca7b1b1a0fd38df64741a
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 10:54:00 +0200

    Revert "Add autoconsent rules for https://maitron.fr/"
    This reverts commit 6777cc1ecf2c3588617be2f450d274a392387284.

--- tests/generated/auto_FR_maitron.fr_q72.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_FR_maitron.fr_q72', ['https://maitron.fr/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['FR'] });
