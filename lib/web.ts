1957fceaafaa1c5c6dcc277c1227622c458f7093
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 18:38:23 +0200

    Report the number of clicks and total opt-out time

--- lib/web.ts
+++ lib/web.ts
@@ -40,6 +40,9 @@ export default class AutoConsent {
         heuristicPatterns: [],
         heuristicSnippets: [],
         selfTest: null,
+        clicks: 0,
+        startTime: 0,
+        endTime: 0,
     };
     domActions: DomActions;
     filtersEngine?: FiltersEngine;
@@ -297,7 +300,7 @@ export default class AutoConsent {
             }
         };
         const mutationObserver = this.domActions.waitForMutation('html');
-        mutationObserver.catch(() => {}); // ensure promise rejection is caught
+        mutationObserver.catch(() => { }); // ensure promise rejection is caught
 
         logsConfig.lifecycle &&
             siteSpecificRules.length > 0 &&
@@ -379,7 +382,7 @@ export default class AutoConsent {
                 this.detectHeuristics();
                 onFirstPopupAppears(cmp);
             })
-            .catch(() => {});
+            .catch(() => { });
 
         const results = await Promise.allSettled(tasks);
         const popups: AutoCMP[] = [];
@@ -394,7 +397,7 @@ export default class AutoConsent {
     }
 
     async handlePopup(cmp: AutoCMP): Promise<boolean> {
-        this.updateState({ lifecycle: 'openPopupDetected' });
+        this.updateState({ lifecycle: 'openPopupDetected', startTime: Date.now() });
         if (this.shouldPrehide && !this.state.prehideOn) {
             // prehide might have timeouted by this time, apply it again
             this.prehideElements();
@@ -444,11 +447,15 @@ export default class AutoConsent {
         });
 
         if (optOutResult && this.foundCmp && !this.foundCmp.isIntermediate) {
+            this.state.endTime = Date.now();
+            logsConfig.lifecycle && console.log(`${this.foundCmp.name}: done in ${this.state.endTime - this.state.startTime}ms with ${this.state.clicks} clicks`);
             this.sendContentMessage({
                 type: 'autoconsentDone',
                 cmp: this.foundCmp?.name,
                 isCosmetic: this.foundCmp?.isCosmetic,
                 url: location.href,
+                duration: this.state.endTime - this.state.startTime,
+                totalClicks: this.state.clicks,
             });
             this.updateState({ lifecycle: 'done' });
         } else {
@@ -487,11 +494,15 @@ export default class AutoConsent {
         });
 
         if (optInResult && this.foundCmp && !this.foundCmp.isIntermediate) {
+            this.state.endTime = Date.now();
+            logsConfig.lifecycle && console.log(`${this.foundCmp.name}: done in ${this.state.endTime - this.state.startTime}ms with ${this.state.clicks} clicks`);
             this.sendContentMessage({
                 type: 'autoconsentDone',
                 cmp: this.foundCmp.name,
                 isCosmetic: this.foundCmp.isCosmetic,
                 url: location.href,
+                duration: this.state.endTime - this.state.startTime,
+                totalClicks: this.state.clicks,
             });
             this.updateState({ lifecycle: 'done' });
         } else {
