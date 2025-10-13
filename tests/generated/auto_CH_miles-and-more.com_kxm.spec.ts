1640707b9aa55175bf273a8e2271ed221a403fe3
Maxim Tsoy <maks.tsoy@gmail.com>
2025-10-13 10:57:36 +0200

    Fix rule

--- tests/generated/auto_CH_miles-and-more.com_kxm.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_miles-and-more.com_kxm', ['https://www.miles-and-more.com/ch/en.html'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['CH'],
-});
