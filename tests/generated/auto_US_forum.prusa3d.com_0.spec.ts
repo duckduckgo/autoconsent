4b7f3d636cbbbeee3dcc20f0abc8b6c9e523a172
Dax the Deployer <dax@duckduckgo.com>
2025-10-27 20:11:25 -0400

    Add autoconsent rules for https://forum.prusa3d.com/

--- tests/generated/auto_US_forum.prusa3d.com_0.spec.ts
+++ tests/generated/auto_US_forum.prusa3d.com_0.spec.ts
@@ -1,3 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
-
-generateCMPTests('auto_US_forum.prusa3d.com_0', ['https://forum.prusa3d.com/']);
+generateCMPTests('auto_US_forum.prusa3d.com_0', ['https://forum.prusa3d.com/'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['US'],
+});
