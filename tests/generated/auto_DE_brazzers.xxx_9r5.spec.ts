ee35b4314bbc22f0b9a9d88bd1e9f29826a3ef97
Dax the Deployer <dax@duckduckgo.com>
2025-10-27 20:15:03 -0400

    Add autoconsent rules for https://www.brazzers.xxx/

---
+++ tests/generated/auto_DE_brazzers.xxx_9r5.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_DE_brazzers.xxx_9r5', ['https://www.brazzers.xxx/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
