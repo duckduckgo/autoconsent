3f70cd316c505ac8eab45013f548b5890c37ac73
Dax the Deployer <dax@duckduckgo.com>
2025-10-27 20:17:26 -0400

    Add autoconsent rules for https://www.klm.ca/

---
+++ tests/generated/auto_CA_klm.ca_2o3.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_CA_klm.ca_2o3', ['https://www.klm.ca/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CA'] });
