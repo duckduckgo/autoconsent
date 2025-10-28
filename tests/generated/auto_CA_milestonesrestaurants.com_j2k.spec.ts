dd1958f1b780ef595725691d4ba7258c74983853
Dax the Deployer <dax@duckduckgo.com>
2025-10-27 20:12:31 -0400

    Add autoconsent rules for https://milestonesrestaurants.com/

--- tests/generated/auto_CA_milestonesrestaurants.com_j2k.spec.ts
+++ tests/generated/auto_CA_milestonesrestaurants.com_j2k.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_CA_milestonesrestaurants.com_j2k', ['https://milestonesrestaurants.com/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['CA'],
 });
