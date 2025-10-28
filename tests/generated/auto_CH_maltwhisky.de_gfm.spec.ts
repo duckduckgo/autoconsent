439d413abd7ab3ee1a41ba092be958bcb9bbf364
Dax the Deployer <dax@duckduckgo.com>
2025-10-27 20:18:06 -0400

    Add autoconsent rules for https://www.maltwhisky.de/

---
+++ tests/generated/auto_CH_maltwhisky.de_gfm.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_CH_maltwhisky.de_gfm', ['https://www.maltwhisky.de/'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['CH'],
+});
