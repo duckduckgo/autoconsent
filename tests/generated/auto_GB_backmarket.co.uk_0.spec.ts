391a0bc013ac569f39009b9ffffff017c8ae6f09
Dax the Deployer <dax@duckduckgo.com>
2025-10-27 20:14:32 -0400

    Add autoconsent rules for https://www.backmarket.co.uk/en-gb

--- tests/generated/auto_GB_backmarket.co.uk_0.spec.ts
+++ tests/generated/auto_GB_backmarket.co.uk_0.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_GB_backmarket.co.uk_0', ['https://www.backmarket.co.uk/en-gb'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['GB'],
 });
