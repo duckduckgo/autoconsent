b62bdcef7134b10263b96a407efc7f7405fe4e80
Dax the Deployer <dax@duckduckgo.com>
2025-10-27 20:18:33 -0400

    Add autoconsent rules for https://www.metronomeonline.com/

--- tests/generated/auto_AU_metronomeonline.com_mco.spec.ts
+++ tests/generated/auto_AU_metronomeonline.com_mco.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_AU_metronomeonline.com_mco', ['https://www.metronomeonline.com/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['AU'],
 });
