48328b09aae3c2f147bed17865463458ee8f9133
Dax the Deployer <dax@duckduckgo.com>
2025-10-27 20:15:42 -0400

    Add autoconsent rules for https://www.dahuasecurity.com/au

--- tests/generated/auto_AU_dahuasecurity.com_j0i.spec.ts
+++ tests/generated/auto_AU_dahuasecurity.com_j0i.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_AU_dahuasecurity.com_j0i', ['https://www.dahuasecurity.com/'], {
+generateCMPTests('auto_AU_dahuasecurity.com_j0i', ['https://www.dahuasecurity.com/au'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['AU'],
 });
