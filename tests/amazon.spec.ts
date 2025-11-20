28c6552af481a5932244d03b37c10e946f348426
Maxim Tsoy <maks.tsoy@gmail.com>
2025-11-20 16:54:34 +0100

    Fix popup detection on amazon.com

--- tests/amazon.spec.ts
+++ tests/amazon.spec.ts
@@ -1,9 +1,9 @@
 import generateCMPTests from '../playwright/runner';
 
-generateCMPTests('amazon.com', ['https://www.amazon.co.uk/', 'https://www.amazon.de/', 'https://www.amazon.nl/'], {
+generateCMPTests('amazon.com', ['https://www.amazon.com/', 'https://www.amazon.co.uk/', 'https://www.amazon.de/', 'https://www.amazon.nl/'], {
     mobile: true,
 });
 
-generateCMPTests('amazon.com', ['https://www.amazon.co.uk/', 'https://www.amazon.de/', 'https://www.amazon.nl/'], {
+generateCMPTests('amazon.com', ['https://www.amazon.com/', 'https://www.amazon.co.uk/', 'https://www.amazon.de/', 'https://www.amazon.nl/'], {
     mobile: false,
 });
