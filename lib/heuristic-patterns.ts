52f14741ea873b0a194e8232a8f32ffba4a94f44
Maxim Tsoy <maks.tsoy@gmail.com>
2025-11-26 17:21:25 +0100

    Move all regexes to a separate file

--- lib/heuristic-patterns.ts
+++ lib/heuristic-patterns.ts
@@ -1,3 +1,95 @@
+// TODO: check for false positive detections per pattern
+// these patterns are matched against popup-like elements or full document text to detect unknown cookie popups
+export const DETECT_PATTERNS = [
+    /accept cookies/gi,
+    /accept all/gi,
+    /reject all/gi,
+    /only necessary cookies/gi, // "only necessary" is probably too broad
+    /(?:by continuing.{0,100}cookie)|(?:cookie.{0,100}by continuing)/gi,
+    /(?:by continuing.{0,100}privacy)|(?:privacy.{0,100}by continuing)/gi,
+    /by clicking.{0,100}(?:accept|agree|allow)/gi,
+    /we (?:use|serve)(?: optional)? cookies/gi,
+    /we are using cookies/gi,
+    /use of cookies/gi,
+    /(?:this|our) (?:web)?site.{0,100}cookies/gi,
+    /cookies (?:and|or) .{0,100} technologies/gi,
+    /such as cookies/gi,
+    /read more about.{0,100}cookies/gi,
+    /consent to.{0,100}cookies/gi,
+    /we and our partners.{0,100}cookies/gi,
+    /we.{0,100}store.{0,100}information.{0,100}such as.{0,100}cookies/gi,
+    /store and\/or access information.{0,100}on a device/gi,
+    /personalised ads and content, ad and content measurement/gi,
+
+    // it might be tempting to add the patterns below, but they cause too many false positives. Don't do it :)
+    // /cookies? settings/i,
+    // /cookies? preferences/i,
+
+    // FR
+    /utilisons.{0,100}des.{0,100}cookies/gi,
+    /nous.{0,100}utilisons.{0,100}des/gi,
+    /des.{0,100}cookies.{0,100}pour/gi,
+    /des.{0,100}informations.{0,100}sur/gi,
+    /retirer.{0,100}votre.{0,100}consentement/gi,
+    /accéder.{0,100}à.{0,100}des/gi,
+    /à.{0,100}des.{0,100}informations/gi,
+    /et.{0,100}nos.{0,100}partenaires/gi,
+    /publicités.{0,100}et.{0,100}du.{0,100}contenu/gi,
+    /utilise.{0,100}des.{0,100}cookies/gi,
+    /utilisent.{0,100}des.{0,100}cookies/gi,
+    /stocker.{0,100}et.{0,100}ou.{0,100}accéder/gi,
+    /consentement.{0,100}à.{0,100}tout.{0,100}moment/gi,
+    /votre.{0,100}consentement/gi,
+    /accepter.{0,100}tout/gi,
+    /utilisation.{0,100}des.{0,100}cookies/gi,
+    /cookies.{0,100}ou.{0,100}technologies/gi,
+    /acceptez.{0,100}l.{0,100}utilisation/gi,
+    /continuer sans accepter/gi,
+    /tout refuser/gi,
+    /(?:refuser|rejeter) tous les cookies/gi,
+    /je refuse/gi,
+    /refuser et continuer/gi,
+    /refuser les cookies/gi,
+    /seulement nécessaires/gi,
+    /je désactive les finalités non essentielles/gi,
+    /cookies essentiels uniquement/gi,
+    /nécessaires uniquement/gi,
+
+    // DE
+    /wir.{0,100}verwenden.{0,100}cookies/gi,
+    /wir.{0,100}und.{0,100}unsere.{0,100}partner/gi,
+    /zugriff.{0,100}auf.{0,100}informationen.{0,100}auf/gi,
+    /inhalte.{0,100}messung.{0,100}von.{0,100}werbeleistung.{0,100}und/gi,
+    /cookies.{0,100}und.{0,100}andere/gi,
+    /verwendung.{0,100}von.{0,100}cookies/gi,
+    /wir.{0,100}nutzen.{0,100}cookies/gi,
+    /verwendet.{0,100}cookies/gi,
+    /sie.{0,100}können.{0,100}ihre.{0,100}auswahl/gi,
+    /und.{0,100}ähnliche.{0,100}technologien/gi,
+    /cookies.{0,100}wir.{0,100}verwenden/gi,
+
+    /alles?.{0,100}ablehnen/gi,
+    /(?:nur|nicht).{0,100}(?:zusätzliche|essenzielle|funktionale|notwendige|erforderliche).{0,100}(?:cookies|akzeptieren|erlauben|ablehnen)/gi,
+    /weiter.{0,100}(?:ohne|mit).{0,100}(?:einwilligung|zustimmung|cookies)/gi,
+    /(?:cookies|einwilligung).{0,100}ablehnen/gi,
+    /nur funktionale cookies akzeptieren/gi,
+    /optionale ablehnen/gi,
+    /zustimmung verweigern/gi,
+
+    // NL
+    /gebruik.{0,100}van.{0,100}cookies/gi,
+    /(?:we|wij).{0,100}gebruiken.{0,100}cookies.{0,100}om/gi,
+    /cookies.{0,100}en.{0,100}vergelijkbare/gi,
+
+    /(?:alles|cookies).{0,100}(?:afwijzen|weigeren|verwerpen)/gi,
+    /alleen.{0,100}noodzakelijke?\b/gi,
+    /cookies weigeren/gi,
+    /weiger.{0,100}(?:cookies|alles)/gi,
+    /doorgaan zonder (?:te accepteren|akkoord te gaan)/gi,
+    /alleen.{0,100}(?:optionele|functionele|functioneel|noodzakelijke|essentiële).{0,100}cookies/gi,
+    /wijs alles af/gi,
+];
+
 const REJECT_PATTERNS_ENGLISH = [
     // e.g. "i reject cookies", "reject all", "reject all cookies", "reject cookies", "deny all", "deny all cookies", "refuse", "refuse all", "refuse cookies", "refuse all cookies", "deny", "reject all and close", "deny all and close", "reject non-essential cookies", "reject all non-essential cookies and continue", "reject optional cookies", "reject additional cookies", "reject targeting cookies", "reject marketing cookies", "reject analytics cookies", "reject tracking cookies", "reject advertising cookies", "reject all and close", "deny all and close"
     // note that "reject and subscribe" and "reject and pay" are excluded
@@ -593,7 +685,7 @@ const REJECT_PATTERNS_SWEDISH = [
 /**
  * @type {Array<string|RegExp>}
  */
-const REJECT_PATTERNS = [
+export const REJECT_PATTERNS = [
     ...REJECT_PATTERNS_ENGLISH,
     ...REJECT_PATTERNS_DUTCH,
     ...REJECT_PATTERNS_FRENCH,
@@ -604,7 +696,7 @@ const REJECT_PATTERNS = [
     ...REJECT_PATTERNS_SWEDISH,
 ];
 
-const NEVER_MATCH_PATTERNS = [
+export const NEVER_MATCH_PATTERNS = [
     /pay|subscribe/is,
     /abonneer/is,
     /abonnier/is,
@@ -616,5 +708,3 @@ const NEVER_MATCH_PATTERNS = [
     /sostienici/is,
     /suscribir/is,
 ];
-
-export { REJECT_PATTERNS, NEVER_MATCH_PATTERNS };
