ff048058fa160d77acba3980ad65c3cce8acb7a0
Maxim Tsoy <mtsoy@duckduckgo.com>
2025-12-04 11:59:25 +0100

    Support shadowDOM version of axeptio (#837)
    * Support shadowDOM version of axeptio
    
    * Fix axeptio popup detection
    
    * Increase the popup detection timeout for long popups like axeptio
    
    * add delays in axeptio rule
    
    * update axeptio tests
    
    * Support another discard button in axeptio
    
    * Support another version of axeptio

--- tests/axeptio.spec.ts
+++ tests/axeptio.spec.ts
@@ -1,3 +1,9 @@
 import generateCMPTests from '../playwright/runner';
 
-generateCMPTests('axeptio', ['https://www.welcometothejungle.com/', 'https://frichti.co/landing?redirectTo=/', 'https://www.dorcel.com/']);
+generateCMPTests('axeptio', [
+    'https://www.welcometothejungle.com/',
+    'https://www.dorcel.com/',
+    'https://mistral.ai/news/le-chat-dives-deep',
+    'https://lahalledesgourmets.com/pages/angelina',
+    'https://www.narbonneaccessoires.fr/fr-fr/',
+]);
