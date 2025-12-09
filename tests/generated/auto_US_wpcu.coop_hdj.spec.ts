f2f7dcbe60a73df64a706304c5b85f6e99968ee5
Dax the Deployer <dax@duckduckgo.com>
2025-12-08 19:37:45 -0500

    Add autoconsent rules for https://www.wpcu.coop/

---
+++ tests/generated/auto_US_wpcu.coop_hdj.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_US_wpcu.coop_hdj', ['https://www.wpcu.coop/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['US'] });
