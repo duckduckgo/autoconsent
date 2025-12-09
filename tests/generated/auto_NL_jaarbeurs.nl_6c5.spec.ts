3550f765eaad8b0b730619b9b464f2d228b08514
Dax the Deployer <dax@duckduckgo.com>
2025-12-08 19:36:45 -0500

    Add autoconsent rules for https://www.jaarbeurs.nl/

--- tests/generated/auto_NL_jaarbeurs.nl_6c5.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_jaarbeurs.nl_6c5', ['https://www.jaarbeurs.nl/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
