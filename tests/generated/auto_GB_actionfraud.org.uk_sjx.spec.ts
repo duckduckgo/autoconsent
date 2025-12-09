63c9368c6f04d11ab0be44fe5d8ae546ab663f49
Dax the Deployer <dax@duckduckgo.com>
2025-12-08 19:36:08 -0500

    Add autoconsent rules for https://www.actionfraud.org.uk/

---
+++ tests/generated/auto_GB_actionfraud.org.uk_sjx.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_GB_actionfraud.org.uk_sjx', ['https://www.actionfraud.org.uk/'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['GB'],
+});
