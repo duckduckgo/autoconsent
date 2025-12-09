70ad60a463e647df049803ef024255ea8f30a32d
Dax the Deployer <dax@duckduckgo.com>
2025-12-08 19:37:19 -0500

    Add autoconsent rules for https://www.sparkasse-oberhessen.de/de/home.html

---
+++ tests/generated/auto_DE_sparkasse-oberhessen.de_mm2.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_DE_sparkasse-oberhessen.de_mm2', ['https://www.sparkasse-oberhessen.de/de/home.html'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['DE'],
+});
