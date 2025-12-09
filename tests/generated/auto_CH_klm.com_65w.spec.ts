791d37ecad242ecae113543a97e3ed87c1ce5829
Dax the Deployer <dax@duckduckgo.com>
2025-12-08 19:36:47 -0500

    Add autoconsent rules for https://www.klm.ch/

--- tests/generated/auto_CH_klm.com_65w.spec.ts
+++ tests/generated/auto_CH_klm.com_65w.spec.ts
@@ -1,6 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_klm.com_65w', ['https://www.klm.ch/en?showredirectnotice=1'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['CH'],
-});
+generateCMPTests('auto_CH_klm.com_65w', ['https://www.klm.ch/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CH'] });
