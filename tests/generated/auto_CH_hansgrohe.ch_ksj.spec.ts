1500d743396408c15d1706cae9ccb67110ecb128
Dax the Deployer <dax@duckduckgo.com>
2025-12-08 19:36:43 -0500

    Add autoconsent rules for https://www.hansgrohe.ch/de/

--- tests/generated/auto_CH_hansgrohe.ch_ksj.spec.ts
+++ tests/generated/auto_CH_hansgrohe.ch_ksj.spec.ts
@@ -1,5 +1,5 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_hansgrohe.ch_ksj', ['https://www.hansgrohe.ch/de'], {
+generateCMPTests('auto_CH_hansgrohe.ch_ksj', ['https://www.hansgrohe.ch/de/'], {
     testOptIn: false,
     testSelfTest: true,
     onlyRegions: ['CH'],
