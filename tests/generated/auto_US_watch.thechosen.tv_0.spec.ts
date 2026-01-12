c465b75eafb24b5a186bdd5d4423ede8f1300685
Dax the Deployer <dax@duckduckgo.com>
2026-01-12 14:50:17 -0500

    Add autoconsent rules for https://watch.thechosen.tv/

--- tests/generated/auto_US_watch.thechosen.tv_0.spec.ts
+++ tests/generated/auto_US_watch.thechosen.tv_0.spec.ts
@@ -1,3 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
-
-generateCMPTests('auto_US_watch.thechosen.tv_0', ['https://watch.thechosen.tv/']);
+generateCMPTests('auto_US_watch.thechosen.tv_0', ['https://watch.thechosen.tv/'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['US'],
+});
