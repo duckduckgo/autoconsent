0fafed4ad723102d0974b2e8e08bd4513862a24d
Dax the Deployer <dax@duckduckgo.com>
2026-01-12 14:50:51 -0500

    Add autoconsent rules for https://www.e-chords.com/

--- tests/generated/auto_AU_e-chords.com_087.spec.ts
+++ tests/generated/auto_AU_e-chords.com_087.spec.ts
@@ -1,6 +1,2 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_AU_e-chords.com_087', ['https://www.e-chords.com/chords/the-beatles/im-only-sleeping'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['AU'],
-});
+generateCMPTests('auto_AU_e-chords.com_087', ['https://www.e-chords.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['AU'] });
