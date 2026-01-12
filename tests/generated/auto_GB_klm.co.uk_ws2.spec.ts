95f64a1455a75ddbbb62164d28ad7fdabc12f33a
Dax the Deployer <dax@duckduckgo.com>
2026-01-12 14:51:20 -0500

    Add autoconsent rules for https://www.klm.co.uk/

---
+++ tests/generated/auto_GB_klm.co.uk_ws2.spec.ts
@@ -0,0 +1,2 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_GB_klm.co.uk_ws2', ['https://www.klm.co.uk/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['GB'] });
