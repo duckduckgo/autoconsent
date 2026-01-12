6eba89f3796e8f8529bb0e4f04b414510913e8ae
Dax the Deployer <dax@duckduckgo.com>
2026-01-12 14:51:26 -0500

    Add autoconsent rules for https://www.modellbahnshop.de/

---
+++ tests/generated/auto_DE_modellbahnshop.de_fmu.spec.ts
@@ -0,0 +1,6 @@
+import generateCMPTests from '../../playwright/runner';
+generateCMPTests('auto_DE_modellbahnshop.de_fmu', ['https://www.modellbahnshop.de/'], {
+    testOptIn: false,
+    testSelfTest: true,
+    onlyRegions: ['DE'],
+});
