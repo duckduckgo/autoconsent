dc8bf9ed2115432c8b5978824a9d85c874505c37
Robert Anderson <randerson@duckduckgo.com>
2026-01-15 12:36:29 +1100

    Revert "Add autoconsent rules for https://lethpolytech.ca/"
    This reverts commit a39424c52370b293dd2c4a7f6db0dc099dd7009c.

--- tests/generated/auto_CA_lethpolytech.ca_1r1.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CA_lethpolytech.ca_1r1', ['https://lethpolytech.ca/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['CA'],
-});
