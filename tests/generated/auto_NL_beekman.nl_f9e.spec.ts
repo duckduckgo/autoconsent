1c6394fc288dc930f683a2673b172ac27d427fa4
Robert Anderson <randerson@duckduckgo.com>
2026-01-15 15:32:45 +1100

    Revert "Add autoconsent rules for https://www.beekman.nl/"
    This reverts commit 5937930e6bd8be272305aeebbea09b8e3a87259e.

--- tests/generated/auto_NL_beekman.nl_f9e.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_beekman.nl_f9e', ['https://www.beekman.nl/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
