b13044457f9e71b7e972308e471ee7caab0c9584
Robert Anderson <randerson@duckduckgo.com>
2026-01-15 12:36:35 +1100

    Revert "Add autoconsent rules for https://fr.tommy.com/"
    This reverts commit a5e8ea383d8a162b82758f6e00c821bf5e1f926a.

--- tests/generated/auto_FR_fr.tommy.com_bms.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_FR_fr.tommy.com_bms', ['https://fr.tommy.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['FR'] });
