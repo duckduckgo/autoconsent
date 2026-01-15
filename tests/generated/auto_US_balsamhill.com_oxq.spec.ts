ebdba450b7245bb3b283c143cbadb044ce325e3a
Robert Anderson <randerson@duckduckgo.com>
2026-01-15 15:32:47 +1100

    Revert "Add autoconsent rules for https://www.balsamhill.com/"
    This reverts commit 09d045636180c784792b8ca39367d1411e79fa01.

--- tests/generated/auto_US_balsamhill.com_oxq.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_US_balsamhill.com_oxq', ['https://www.balsamhill.com/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['US'],
-});
