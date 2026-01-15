9eab34fcc6046928d52840ddbdf3408962b2a28a
Robert Anderson <randerson@duckduckgo.com>
2026-01-15 15:32:47 +1100

    Revert "Add autoconsent rules for https://www.bitget.com/"
    This reverts commit b2a79f031b4f914828a85b4a7dd8cbb8c59086b3.

--- tests/generated/auto_CH_bitget.com_z0w.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_bitget.com_z0w', ['https://www.bitget.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CH'] });
