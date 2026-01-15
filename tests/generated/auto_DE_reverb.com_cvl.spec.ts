e68c0de628f2f080c4d22c98135816d549577496
Robert Anderson <randerson@duckduckgo.com>
2026-01-15 15:32:44 +1100

    Revert "Add autoconsent rules for https://reverb.com/"
    This reverts commit 07cce8fe73fc1e8cdde968052b5dc8618dc3d06e.

--- tests/generated/auto_DE_reverb.com_cvl.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_reverb.com_cvl', ['https://reverb.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
