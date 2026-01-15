0a04b7aef4a6a73e01b72933402dae4a84ef386b
Robert Anderson <randerson@duckduckgo.com>
2026-01-15 11:54:07 +1100

    Fix betterstack.com

--- tests/generated/auto_DE_betterstack.com_5q7.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_betterstack.com_5q7', ['https://betterstack.com/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['DE'],
-});
