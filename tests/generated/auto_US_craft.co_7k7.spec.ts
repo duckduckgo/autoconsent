176f2588a36a803d498a29bef6cd9f053886aa54
Robert Anderson <randerson@duckduckgo.com>
2026-01-15 12:36:30 +1100

    Revert "Add autoconsent rules for https://global.craft.co/"
    This reverts commit 303f7714c2e0723f3b313829ed6b6f7cdcb9654a.

--- tests/generated/auto_US_craft.co_7k7.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_US_craft.co_7k7', ['https://global.craft.co/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['US'] });
