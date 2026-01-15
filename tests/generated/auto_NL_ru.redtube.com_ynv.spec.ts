f07c7be6eddc87fc3366c926e03d1333fefa986e
Robert Anderson <randerson@duckduckgo.com>
2026-01-15 15:32:43 +1100

    Revert "Add autoconsent rules for https://ru.redtube.com/"
    This reverts commit 7ea632e0f669aa6cfdccadb862846cb624de9328.

--- tests/generated/auto_NL_ru.redtube.com_ynv.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_ru.redtube.com_ynv', ['https://ru.redtube.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
