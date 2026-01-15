5df8d11c1f448c1703e57defb015e9ac13cbe529
Robert Anderson <randerson@duckduckgo.com>
2026-01-15 12:36:33 +1100

    Revert "Add autoconsent rules for https://at.pinterest.com/"
    This reverts commit 414d100c2b6f0b3e9d7ccaec6bdc4c68c891c99f.

--- tests/generated/auto_DE_at.pinterest.com_wfi.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_at.pinterest.com_wfi', ['https://at.pinterest.com/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['DE'],
-});
