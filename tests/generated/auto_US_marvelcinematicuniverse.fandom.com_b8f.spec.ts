f22c911d7d08b3612bf3d24d22e66b6afff0d5d8
Robert Anderson <randerson@duckduckgo.com>
2026-01-15 12:36:31 +1100

    Revert "Add autoconsent rules for https://marvelcinematicuniverse.fandom.com/wiki/Marvel_Cinematic_Universe_Wiki"
    This reverts commit 9a9175617e2e2d6ca4538bc559ce75b773b5c917.

--- tests/generated/auto_US_marvelcinematicuniverse.fandom.com_b8f.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests(
-    'auto_US_marvelcinematicuniverse.fandom.com_b8f',
-    ['https://marvelcinematicuniverse.fandom.com/wiki/Marvel_Cinematic_Universe_Wiki'],
-    { testOptIn: false, testSelfTest: true, onlyRegions: ['US'] },
-);
