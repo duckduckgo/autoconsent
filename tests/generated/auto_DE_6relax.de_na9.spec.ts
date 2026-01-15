7e16c92855779dbd6a1e27cb7f3d79fb660b8577
Robert Anderson <randerson@duckduckgo.com>
2026-01-15 15:32:45 +1100

    Revert "Add autoconsent rules for https://www.6relax.de/"
    This reverts commit f7f53a3e92438d43fbb66a178452daede14f0e18.

--- tests/generated/auto_DE_6relax.de_na9.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_6relax.de_na9', ['https://www.6relax.de/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
