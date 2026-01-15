79411217749a01a12b5ec8a24f720d8372f52b26
Robert Anderson <randerson@duckduckgo.com>
2026-01-15 12:36:33 +1100

    Revert "Add autoconsent rules for https://fcbinside.de/"
    This reverts commit 4d70c5a236ff7676e0c8976c9c61ceeab5aa8126.

--- tests/generated/auto_DE_fcbinside.de_0d6.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_fcbinside.de_0d6', ['https://fcbinside.de/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
