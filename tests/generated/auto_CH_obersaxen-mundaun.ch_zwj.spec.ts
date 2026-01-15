28b653b27c92f14063c9dad173adb2acaf0e8e89
Robert Anderson <randerson@duckduckgo.com>
2026-01-15 12:36:29 +1100

    Revert "Add autoconsent rules for https://obersaxen-mundaun.ch/"
    This reverts commit 04e6ed805dc8649b6baea1f0f7576ea27e4a94fc.

--- tests/generated/auto_CH_obersaxen-mundaun.ch_zwj.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_obersaxen-mundaun.ch_zwj', ['https://obersaxen-mundaun.ch/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['CH'],
-});
