dfcf1893d4554ecf4fc56003187ae870eb30cde9
Robert Anderson <randerson@duckduckgo.com>
2026-01-15 12:36:34 +1100

    Revert "Add autoconsent rules for https://connect.mayoclinic.org/"
    This reverts commit c1e14fb7fa1731551488f9a6b1b6e5101e16d4c1.

--- tests/generated/auto_AU_connect.mayoclinic.org_kdf.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_AU_connect.mayoclinic.org_kdf', ['https://connect.mayoclinic.org/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['AU'],
-});
