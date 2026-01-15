835b88e05eb9ecf2831057490f01c506d7582037
Robert Anderson <randerson@duckduckgo.com>
2026-01-15 12:36:31 +1100

    Revert "Add autoconsent rules for https://escapefromtarkov.fandom.com/wiki/Escape_from_Tarkov_Wiki"
    This reverts commit aecf72cceb491fbd755510232a443de0adda06b8.

--- tests/generated/auto_US_escapefromtarkov.fandom.com_3mi.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_US_escapefromtarkov.fandom.com_3mi', ['https://escapefromtarkov.fandom.com/wiki/Escape_from_Tarkov_Wiki'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['US'],
-});
