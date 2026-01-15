4859eeadc55dd82440fef7b94a918c0f487290fe
Robert Anderson <randerson@duckduckgo.com>
2026-01-15 12:36:33 +1100

    Revert "Add autoconsent rules for https://de.pinterest.com/"
    This reverts commit f1764120b9e33d49399a22f01c7e41fc48fc396a.

--- tests/generated/auto_DE_de.pinterest.com_5xo.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_de.pinterest.com_5xo', ['https://de.pinterest.com/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['DE'],
-});
