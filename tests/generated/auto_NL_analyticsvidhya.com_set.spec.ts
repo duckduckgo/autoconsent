1eea90c178e7b3e4070e12ee354caa2f4d187cf4
Robert Anderson <randerson@duckduckgo.com>
2026-01-15 14:53:54 +1100

    Remove duplicate analyticsvidhya.com rule

--- tests/generated/auto_NL_analyticsvidhya.com_set.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_analyticsvidhya.com_set', ['https://www.analyticsvidhya.com/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['NL'],
-});
