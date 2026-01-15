96cba04e71e8f7c1339de2cb4637035467208e68
Robert Anderson <randerson@duckduckgo.com>
2026-01-15 15:32:47 +1100

    Revert "Add autoconsent rules for https://sante-espaceparticuliers.atoutmh.com/"
    This reverts commit d96b4d26b26b71170d73ffbf3ddb1d97e1a450ef.

--- tests/generated/auto_FR_sante-espaceparticuliers.atoutmh.com_5cd.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_FR_sante-espaceparticuliers.atoutmh.com_5cd', ['https://sante-espaceparticuliers.atoutmh.com/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['FR'],
-});
