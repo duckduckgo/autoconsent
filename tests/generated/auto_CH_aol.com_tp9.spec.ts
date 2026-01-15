096aa3c535bbafe2dea792b2f00a929b0b38e898
Robert Anderson <randerson@duckduckgo.com>
2026-01-15 12:36:33 +1100

    Revert "Add autoconsent rules for $'https://consent.aol.com/v2/collectConsent?sessionId=3_cc-session_06a2ddd8-9e43-4162-b9b2-2c5340569ebb'"
    This reverts commit b962bafd2fa6cf8cda3b5af939e704d265e71169.

--- tests/generated/auto_CH_aol.com_tp9.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests(
-    'auto_CH_aol.com_tp9',
-    ['https://consent.aol.com/v2/collectConsent?sessionId=3_cc-session_06a2ddd8-9e43-4162-b9b2-2c5340569ebb'],
-    { testOptIn: false, testSelfTest: true, onlyRegions: ['CH'] },
-);
