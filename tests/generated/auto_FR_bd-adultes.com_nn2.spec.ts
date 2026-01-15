4bb49143d5940487c1eb5dbff0589036d5953bbc
Robert Anderson <randerson@duckduckgo.com>
2026-01-15 12:36:32 +1100

    Revert "Add autoconsent rules for https://bd-adultes.com/"
    This reverts commit 923035561384a1a788da425ae1129a64429291c9.

--- tests/generated/auto_FR_bd-adultes.com_nn2.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_FR_bd-adultes.com_nn2', ['https://bd-adultes.com/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['FR'] });
