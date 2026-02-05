a1ac9b27d1f80db1439f3e2148844648061ce929
Robert Anderson <randerson@duckduckgo.com>
2026-02-05 19:51:08 +1100

    Revert "Add autoconsent rules for https://www.cci.fr/"
    This reverts commit fccec174966411091c399bddf5ad88cc510d9aaf.

--- tests/generated/auto_FR_cci.fr_gd1.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_FR_cci.fr_gd1', ['https://www.cci.fr/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['FR'] });
