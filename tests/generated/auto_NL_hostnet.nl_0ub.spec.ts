0a402c5eb78f4827b99d0497bdd49b91db5eac7b
Robert Anderson <randerson@duckduckgo.com>
2026-02-05 19:51:10 +1100

    Revert "Add autoconsent rules for https://www.hostnet.nl/"
    This reverts commit e8ab6735291a25588735f3e3163c745d52bb141a.

--- tests/generated/auto_NL_hostnet.nl_0ub.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_hostnet.nl_0ub', ['https://www.hostnet.nl/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
