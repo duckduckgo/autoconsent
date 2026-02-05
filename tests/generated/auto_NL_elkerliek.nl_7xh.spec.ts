5e53620acc30c8790b9f495f0e96b6a848742047
Robert Anderson <randerson@duckduckgo.com>
2026-02-05 19:51:10 +1100

    Revert "Add autoconsent rules for https://www.elkerliek.nl/"
    This reverts commit fdf6f79184cbefee7254402dcdacdd4524e5a1d1.

--- tests/generated/auto_NL_elkerliek.nl_7xh.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_elkerliek.nl_7xh', ['https://www.elkerliek.nl/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['NL'] });
