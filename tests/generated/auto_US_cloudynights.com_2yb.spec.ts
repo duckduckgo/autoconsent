d7617372d97d74042a39d2d02ee2008891c74999
Robert Anderson <randerson@duckduckgo.com>
2026-02-05 19:51:12 +1100

    Revert "Add autoconsent rules for https://www.cloudynights.com/"
    This reverts commit 12aa5ef762d16c6f4a2292038abecaeeaa18e71f.

--- tests/generated/auto_US_cloudynights.com_2yb.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_US_cloudynights.com_2yb', ['https://www.cloudynights.com/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['US'],
-});
