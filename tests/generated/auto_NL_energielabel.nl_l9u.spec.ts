6a57a5af553133dc7b8686150a60ff33c15e297a
Robert Anderson <randerson@duckduckgo.com>
2026-02-05 19:51:10 +1100

    Revert "Add autoconsent rules for https://www.energielabel.nl/"
    This reverts commit d71f60cb26a351598a58e301294ba609ab50551f.

--- tests/generated/auto_NL_energielabel.nl_l9u.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_energielabel.nl_l9u', ['https://www.energielabel.nl/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['NL'],
-});
