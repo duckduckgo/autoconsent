3d3c197d5e6dc701f4c4504054b8e4863161b4f7
Robert Anderson <randerson@duckduckgo.com>
2026-02-05 19:51:08 +1100

    Revert "Add autoconsent rules for https://www.fireangel.co.uk/"
    This reverts commit ba15901ed8b73db9ecac86917a310ed43d397faa.

--- tests/generated/auto_GB_fireangel.co.uk_dm0.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_GB_fireangel.co.uk_dm0', ['https://www.fireangel.co.uk/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['GB'],
-});
