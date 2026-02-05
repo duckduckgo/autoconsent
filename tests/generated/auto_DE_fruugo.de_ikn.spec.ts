2d8226b8be96c175fbc306d457f0670b490bd139
Robert Anderson <randerson@duckduckgo.com>
2026-02-05 19:51:11 +1100

    Revert "Add autoconsent rules for https://www.fruugo.de/"
    This reverts commit c3f2d11c1f44bad6b0b52326fb025bc082e64901.

--- tests/generated/auto_DE_fruugo.de_ikn.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_fruugo.de_ikn', ['https://www.fruugo.de/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
