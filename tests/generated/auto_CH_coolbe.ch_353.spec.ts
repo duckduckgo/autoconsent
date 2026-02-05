6ffb4af19e2a2ab74729d3e38ee002f2dae08c15
Robert Anderson <randerson@duckduckgo.com>
2026-02-05 19:51:07 +1100

    Revert "Add autoconsent rules for https://www.coolbe.ch/en"
    This reverts commit c4060378eed921d0c8ac9fa16c00e737c3b90f64.

--- tests/generated/auto_CH_coolbe.ch_353.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_coolbe.ch_353', ['https://www.coolbe.ch/en'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CH'] });
