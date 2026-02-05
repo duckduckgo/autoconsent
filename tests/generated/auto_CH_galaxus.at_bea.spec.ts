a3f34965dd55d7644474d063e1aa7edce1c04071
Robert Anderson <randerson@duckduckgo.com>
2026-02-05 19:51:11 +1100

    Revert "Add autoconsent rules for https://www.galaxus.at/"
    This reverts commit 7af690d2cc9dc7183c5dd7bea5df162c3144c741.

--- tests/generated/auto_CH_galaxus.at_bea.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_galaxus.at_bea', ['https://www.galaxus.at/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['CH'] });
