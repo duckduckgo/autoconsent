d7dd231c249e178aafb5574aad12172db07c3ebd
Robert Anderson <randerson@duckduckgo.com>
2026-02-05 19:51:09 +1100

    Revert "Add autoconsent rules for https://www.dutchitchannel.nl/"
    This reverts commit f14d3b46815576e47d9a19fefebab6a96b7b63a3.

--- tests/generated/auto_NL_dutchitchannel.nl_hpo.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_dutchitchannel.nl_hpo', ['https://www.dutchitchannel.nl/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['NL'],
-});
