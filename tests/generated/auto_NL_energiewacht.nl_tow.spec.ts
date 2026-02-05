93000c596ddcafc2d1b3b2b7555d9fde42e6206b
Robert Anderson <randerson@duckduckgo.com>
2026-02-05 19:51:08 +1100

    Revert "Add autoconsent rules for https://www.energiewacht.nl/"
    This reverts commit 1ad7de8a7b71173b598c931ba012ab900aa60372.

--- tests/generated/auto_NL_energiewacht.nl_tow.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_energiewacht.nl_tow', ['https://www.energiewacht.nl/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['NL'],
-});
