e940eca495def67d21499833fe8c81f2511474b8
Robert Anderson <randerson@duckduckgo.com>
2026-02-05 19:51:09 +1100

    Revert "Add autoconsent rules for https://www.groningenbereikbaar.nl/"
    This reverts commit 361bbb7e4004567d680c0fd0ae236e5c0b95a62c.

--- tests/generated/auto_NL_groningenbereikbaar.nl_mvo.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NL_groningenbereikbaar.nl_mvo', ['https://www.groningenbereikbaar.nl/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['NL'],
-});
