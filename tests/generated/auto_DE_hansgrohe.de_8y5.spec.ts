92fb7e057e522dbf909f7d44f1c578fe326e539b
Robert Anderson <randerson@duckduckgo.com>
2026-02-05 19:51:07 +1100

    Revert "Add autoconsent rules for https://www.hansgrohe.de/"
    This reverts commit ead38c95cb50f0a8f53c68b1337382f57c111a15.

--- tests/generated/auto_DE_hansgrohe.de_8y5.spec.ts
+++
@@ -1,2 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_hansgrohe.de_8y5', ['https://www.hansgrohe.de/'], { testOptIn: false, testSelfTest: true, onlyRegions: ['DE'] });
