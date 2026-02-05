68d6d180f363cbcb932ac4e14741615c17f5ca29
Robert Anderson <randerson@duckduckgo.com>
2026-02-05 19:51:11 +1100

    Revert "Add autoconsent rules for https://www.die-bewerbungsschreiber.ch/"
    This reverts commit 9876c4743c7c3f25b746e7e1c0a7f6a6be237907.

--- tests/generated/auto_CH_die-bewerbungsschreiber.ch_8oq.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_die-bewerbungsschreiber.ch_8oq', ['https://www.die-bewerbungsschreiber.ch/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['CH'],
-});
