62138793d812797c91df2073468af6c5f59541a6
Robert Anderson <randerson@duckduckgo.com>
2026-02-05 19:51:07 +1100

    Revert "Add autoconsent rules for https://www.gravelpassion.fr/"
    This reverts commit f343a1aba112086b7adf4660826eb38df2590c51.

--- tests/generated/auto_FR_gravelpassion.fr_010.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_FR_gravelpassion.fr_010', ['https://www.gravelpassion.fr/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['FR'],
-});
