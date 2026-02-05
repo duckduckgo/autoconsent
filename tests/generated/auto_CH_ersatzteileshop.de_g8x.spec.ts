acc3529c675bf4e1263cc953becee03fc8bfc62c
Robert Anderson <randerson@duckduckgo.com>
2026-02-05 19:51:08 +1100

    Revert "Add autoconsent rules for https://www.ersatzteileshop.de/"
    This reverts commit 1af1c69cace132cbb612715e49b135a39ffc5468.

--- tests/generated/auto_CH_ersatzteileshop.de_g8x.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_ersatzteileshop.de_g8x', ['https://www.ersatzteileshop.de/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['CH'],
-});
