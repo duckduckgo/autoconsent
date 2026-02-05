96f10e1ae259517ec5b128ef6cf1c11a8fbb5779
Robert Anderson <randerson@duckduckgo.com>
2026-02-05 19:51:07 +1100

    Revert "Add autoconsent rules for https://www.conflingo.com/"
    This reverts commit d580a5d069b74d87785cfce005b4a9e7c71aa660.

--- tests/generated/auto_NO_conflingo.com_tht.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_NO_conflingo.com_tht', ['https://www.conflingo.com/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['NO'],
-});
