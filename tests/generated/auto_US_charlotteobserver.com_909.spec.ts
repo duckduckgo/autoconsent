6177a92b414139a9306756aa99abf7e1e4a1cd87
Robert Anderson <randerson@duckduckgo.com>
2026-02-05 19:51:12 +1100

    Revert "Add autoconsent rules for https://www.charlotteobserver.com/"
    This reverts commit c77aa4b4e1c518db3edb9c8fb001f938a547c924.

--- tests/generated/auto_US_charlotteobserver.com_909.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_US_charlotteobserver.com_909', ['https://www.charlotteobserver.com/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['US'],
-});
