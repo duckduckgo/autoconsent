1aa1b3670a210e4ef8f2f3936940662177f54051
Maxim Tsoy <maks.tsoy@gmail.com>
2026-02-19 20:02:52 +0100

    Lint

--- tests/ensighten.spec.ts
+++ tests/ensighten.spec.ts
@@ -2,12 +2,7 @@ import generateCMPTests from '../playwright/runner';
 
 generateCMPTests(
     'Ensighten',
-    [
-        'https://www.audi.ca/en/',
-        'https://www.volkswagen.de/',
-        'https://de.rs-online.com/',
-        'https://www.easyjet.com/',
-    ],
+    ['https://www.audi.ca/en/', 'https://www.volkswagen.de/', 'https://de.rs-online.com/', 'https://www.easyjet.com/'],
     {
         skipRegions: ['US'],
     },
