d6132ded1321daf80c586c5307966abc78ac1a08
Dax the Deployer <dax@duckduckgo.com>
2026-03-08 18:45:40 -0400

    Add autoconsent rules for https://communityforums.atmeta.com/

--- tests/generated/auto_AU_communityforums.atmeta.com_kez.spec.ts
+++ tests/generated/auto_AU_communityforums.atmeta.com_kez.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../../playwright/runner';
 generateCMPTests('auto_AU_communityforums.atmeta.com_kez', ['https://communityforums.atmeta.com/'], {
     testOptIn: false,
-    testSelfTest: false,
+    testSelfTest: true,
     onlyRegions: ['AU'],
 });
