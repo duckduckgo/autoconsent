c01f136b81e907e1c1cdb4d7a3634c5a6dc7fa6b
Dax the Deployer <dax@duckduckgo.com>
2026-03-08 18:42:20 -0400

    Add autoconsent rules for https://bafoeg-digital.de/

--- tests/generated/auto_DE_bafoeg-digital.de_99m.spec.ts
+++ tests/generated/auto_DE_bafoeg-digital.de_99m.spec.ts
@@ -1,5 +1,5 @@
 import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_DE_bafoeg-digital.de_99m', ['https://www.bafoeg-digital.de/ams/BAFOEG'], {
+generateCMPTests('auto_DE_bafoeg-digital.de_99m', ['https://bafoeg-digital.de/'], {
     testOptIn: false,
     testSelfTest: true,
     onlyRegions: ['DE'],
