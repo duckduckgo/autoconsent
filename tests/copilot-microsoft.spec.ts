5095367c733abcfa2983d7bd4de919f4cf194928
Sam Macbeth <smacbeth@duckduckgo.com>
2026-03-10 15:30:46 +0100

    Reduce testing regions.

--- tests/copilot-microsoft.spec.ts
+++ tests/copilot-microsoft.spec.ts
@@ -1,5 +1,5 @@
 import generateCMPTests from '../playwright/runner';
 
 generateCMPTests('copilot-microsoft', ['https://copilot.microsoft.com/'], {
-    skipRegions: ['US'],
+    onlyRegions: ['UK', 'DE'],
 });
