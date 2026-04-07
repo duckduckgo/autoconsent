43878a5f4aac8be8196c05642627a399c1cd12a2
Maxim Tsoy <maks.tsoy@gmail.com>
2026-04-07 15:00:22 +0200

    fix didomi rule

--- tests/didomi.spec.ts
+++ tests/didomi.spec.ts
@@ -8,9 +8,10 @@ generateCMPTests(
         'http://www.allocine.fr/',
         'https://www.boursorama.com/',
         'https://www.theoriginalshotels.com/en/hotels/la-villa-vicha',
+        'https://support.didomi.io/set-up-the-absence-of-interaction-as-a-cookie-denial',
     ],
     {
-        testOptIn: false,
+        testOptIn: true,
         testSelfTest: false,
         skipRegions: ['US'],
     },
