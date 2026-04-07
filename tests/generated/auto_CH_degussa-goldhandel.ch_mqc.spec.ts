62a8ceafe5e6a52d5f1553f7c411065a3b97a118
Maxim Tsoy <maks.tsoy@gmail.com>
2026-04-07 16:02:58 +0200

    update real-cookie-banner

--- tests/generated/auto_CH_degussa-goldhandel.ch_mqc.spec.ts
+++
@@ -1,6 +0,0 @@
-import generateCMPTests from '../../playwright/runner';
-generateCMPTests('auto_CH_degussa-goldhandel.ch_mqc', ['https://www.degussa-goldhandel.ch/'], {
-    testOptIn: false,
-    testSelfTest: true,
-    onlyRegions: ['CH'],
-});
