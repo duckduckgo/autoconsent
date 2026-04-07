ad51110efcc2f46b3eae0ce52a1acc3362a717a4
Maxim Tsoy <maks.tsoy@gmail.com>
2026-04-07 18:34:50 +0200

    Fix consentmo

--- tests/consentmo.spec.ts
+++ tests/consentmo.spec.ts
@@ -2,7 +2,7 @@ import generateCMPTests from '../playwright/runner';
 
 generateCMPTests(
     'consentmo',
-    ['https://filofax.com/', 'https://www.consentmo.com/', 'https://mgemi.com/products/the-felize-saddle-suede'],
+    ['https://filofax.com/', 'https://www.consentmo.com/', 'https://mgemi.com/products/the-felize-saddle-suede', 'https://eu.blackdiamondequipment.com/', 'https://www.directdoors.com/'],
     {
         skipRegions: ['US'],
     },
