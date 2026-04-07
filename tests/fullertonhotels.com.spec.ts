fff75f75b9507d1d8ce19186d1518a3d02c07d6b
Maxim Tsoy <maks.tsoy@gmail.com>
2026-04-07 14:07:19 +0200

    fix test

--- tests/fullertonhotels.com.spec.ts
+++ tests/fullertonhotels.com.spec.ts
@@ -1,4 +1,4 @@
-import generateCMPTests from '../../playwright/runner';
+import generateCMPTests from '../playwright/runner';
 generateCMPTests('fullertonhotels.com', ['https://www.fullertonhotels.com/', 'https://marriottsalalahresort.com/'], {
     testOptIn: false,
     testSelfTest: false,
