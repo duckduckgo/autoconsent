892a2acf2597ea959b11ab2d1f09ce2c9f17f534
Maxim Tsoy <maks.tsoy@gmail.com>
2026-04-07 18:43:00 +0200

    Lint fix

--- tests/consentmo.spec.ts
+++ tests/consentmo.spec.ts
@@ -2,7 +2,13 @@ import generateCMPTests from '../playwright/runner';
 
 generateCMPTests(
     'consentmo',
-    ['https://filofax.com/', 'https://www.consentmo.com/', 'https://mgemi.com/products/the-felize-saddle-suede', 'https://eu.blackdiamondequipment.com/', 'https://www.directdoors.com/'],
+    [
+        'https://filofax.com/',
+        'https://www.consentmo.com/',
+        'https://mgemi.com/products/the-felize-saddle-suede',
+        'https://eu.blackdiamondequipment.com/',
+        'https://www.directdoors.com/',
+    ],
     {
         skipRegions: ['US'],
     },
