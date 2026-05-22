3fc89d6947bd6ae6e6341187b87283337eb3a253
Sam Macbeth <sammacbeth@users.noreply.github.com>
2026-05-22 12:42:10 +0200

    Update sostereo.spec.ts

--- tests/sostereo.spec.ts
+++ tests/sostereo.spec.ts
@@ -1,7 +1,3 @@
 import generateCMPTests from '../playwright/runner';
 
-generateCMPTests('sostereo.com', [
-    'https://sostereo.com/spaces/pitch/b58c388677?slide=1',
-    'https://sostereo.com/',
-    'https://sostereo.com/about',
-]);
+generateCMPTests('sostereo.com', ['https://sostereo.com/']);
