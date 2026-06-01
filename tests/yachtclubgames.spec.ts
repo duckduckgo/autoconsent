a719dd84f4114767bd81982343ee66b0a9bc6c9a
Sam Macbeth <sammacbeth@users.noreply.github.com>
2026-06-01 16:08:09 +0200

    Update yachtclubgames.spec.ts

--- tests/yachtclubgames.spec.ts
+++ tests/yachtclubgames.spec.ts
@@ -1,6 +1,6 @@
 import generateCMPTests from '../playwright/runner';
 
-const urls = ['https://www.yachtclubgames.com/games/mina-the-hollower/', 'https://www.yachtclubgames.com/'];
+const urls = ['https://www.yachtclubgames.com/'];
 
 generateCMPTests('yachtclubgames.com', urls);
 
