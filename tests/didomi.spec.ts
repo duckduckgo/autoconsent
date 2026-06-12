632bfe5e8cde0bd86161967ed1636d70b5cc4bd3
Sam Macbeth <smacbeth@duckduckgo.com>
2026-06-12 16:47:18 +0200

    Tighten test cases

--- tests/didomi.spec.ts
+++ tests/didomi.spec.ts
@@ -4,19 +4,17 @@ generateCMPTests(
     'didomi',
     [
         'https://nothing.tech/',
-        'https://www.planet.fr/',
         'http://www.allocine.fr/',
         'https://www.boursorama.com/',
         'https://www.theoriginalshotels.com/en/hotels/la-villa-vicha',
         'https://support.didomi.io/set-up-the-absence-of-interaction-as-a-cookie-denial',
-        'https://www.leboncoin.fr/',
     ],
     {
         testOptIn: true,
         // Self-test is disabled because some sites (e.g. planet.fr) navigate away
         // after clicking the disagree button, which destroys the Didomi state.
         testSelfTest: false,
-        skipRegions: ['US'],
+        onlyRegions: ['US', 'FR'],
     },
 );
 
